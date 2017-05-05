caf0d033b3b00174d0dc57a0e4d75db30f00b990
Ezequiel Jadib <ezequiel.jadib@southworks.net>
2017-05-05 16:33:28 -0700

    Add dependency to Microsoft.Recognizers.Text.Number on NumberWithUnit nuspec. Add cmds files to build nuget packages

---
+++ CreateAllPackages.cmd
@@ -0,0 +1,6 @@
+pushd Microsoft.Recognizers.Text.Number
+call CreatePackage.cmd
+popd
+pushd Microsoft.Recognizers.Text.NumberWithUnit
+call CreatePackage.cmd
+popd
---
+++ Microsoft.Recognizers.Text.Number/CreatePackage.cmd
@@ -0,0 +1,12 @@
+﻿@echo off
+echo *** Building Microsoft.Recognizers.Text.Number
+setlocal
+setlocal enabledelayedexpansion
+setlocal enableextensions
+set errorlevel=0
+mkdir ..\nuget
+erase /s ..\nuget\Microsoft.Recognizers.Text.Number*.nupkg
+msbuild /property:Configuration=release Microsoft.Recognizers.Text.Number.csproj
+for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Recognizers.Text.Number.dll).FileVersionInfo.FileVersion"') do set numberVersion=%%v
+..\packages\NuGet.CommandLine.3.5.0\tools\NuGet.exe pack Microsoft.Recognizers.Text.Number.nuspec -symbols -properties version=%numberVersion% -OutputDirectory ..\nuget
+echo *** Finished building Microsoft.Recognizers.Text.Number
