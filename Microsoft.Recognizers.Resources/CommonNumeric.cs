454879bdda9a3f2e22e513b45d2c0750669413f7
JuanAr <juan.arguello@southworks.com>
2017-08-09 17:42:41 -0300

    Auto-generate CommonNumeric and extracted constants for BaseNumberExtractor

--- Microsoft.Recognizers.Resources/CommonNumeric.cs
+++ Microsoft.Recognizers.Resources/CommonNumeric.cs
@@ -1,8 +1,13 @@
 ﻿namespace Microsoft.Recognizers.Resources
 {
-    public class CommonNumeric
-    {
-        // Percentage Regex
-        public const string NumberReplaceToken = "@builtin.num";
-    }
+	using System;
+	using System.Collections.Generic;
+
+	public static class CommonNumeric
+	{
+		public const string NumberReplaceToken = "@builtin.num";
+		public const string IntegerTemplateRegex = "(((?<!\\d+\\s*)-\\s*)|((?<=\\b)(?<!(\\d+\\.|\\d+,))))\\d{{1,3}}({0}\\d{{3}})+";
+		public const string DoubleTemplateRegex = "(((?<!\\d+\\s*)-\\s*)|((?<=\\b)(?<!\\d+\\.|\\d+,)))\\d{{1,3}}({0}\\d{{3}})+{1}\\d+";
+		public const string PlaceHolderDefault = "\\D|\\b";
+	}
 }
