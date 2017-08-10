86cda9647353392454b7ddc4756603d09400faed
Enzo Cano <enzo.cano@southworks.com>
2017-08-10 12:40:07 -0300

    Changed Resources folders structure

---
+++ Microsoft.Recognizers.Resources/BaseNumbers.cs
@@ -0,0 +1,13 @@
+﻿namespace Microsoft.Recognizers.Resources
+{
+	using System;
+	using System.Collections.Generic;
+
+	public static class CommonNumeric
+	{
+		public const string NumberReplaceToken = "@builtin.num";
+		public const string IntegerTemplateRegex = "(((?<!\\d+\\s*)-\\s*)|((?<=\\b)(?<!(\\d+\\.|\\d+,))))\\d{{1,3}}({0}\\d{{3}})+";
+		public const string DoubleTemplateRegex = "(((?<!\\d+\\s*)-\\s*)|((?<=\\b)(?<!\\d+\\.|\\d+,)))\\d{{1,3}}({0}\\d{{3}})+{1}\\d+";
+		public const string PlaceHolderDefault = "\\D|\\b";
+	}
+}
