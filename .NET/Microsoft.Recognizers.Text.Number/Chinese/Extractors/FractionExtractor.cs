dde25cb11f8466a576b92cd809c86802fa136c41
JuanAr <juan.arguello@southworks.com>
2017-08-11 19:35:19 -0300

    Extracted Chinese Numbers constants to YAML

--- .NET/Microsoft.Recognizers.Text.Number/Chinese/Extractors/FractionExtractor.cs
+++ .NET/Microsoft.Recognizers.Text.Number/Chinese/Extractors/FractionExtractor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Text.RegularExpressions;
+using Microsoft.Recognizers.Definitions.Chinese;
 
 namespace Microsoft.Recognizers.Text.Number.Chinese
 {
@@ -8,44 +9,27 @@ namespace Microsoft.Recognizers.Text.Number.Chinese
     {
         internal sealed override ImmutableDictionary<Regex, string> Regexes { get; }
         protected sealed override string ExtractType { get; } = Constants.SYS_NUM_FRACTION;
-
-        public static string AllFractionNumber
-            =>
-                $@"{IntegerExtractor.SignSymbolRegexChs}?(({IntegerExtractor.ZeroToNineChsFullHalfRegexChs}+|{IntegerExtractor
-                    .AllIntRegexChs})\s*又\s*)?{IntegerExtractor.SignSymbolRegexChs}?({IntegerExtractor
-                        .ZeroToNineChsFullHalfRegexChs}+|{IntegerExtractor.AllIntRegexChs})\s*分\s*之\s*{IntegerExtractor
-                            .SignSymbolRegexChs}?({IntegerExtractor.ZeroToNineChsFullHalfRegexChs}+|{IntegerExtractor
-                                .AllIntRegexChs})";
-
+        
         public FractionExtractor()
         {
             var regexes = new Dictionary<Regex, string>()
             {
                 {
                     // -4 5/2,       ４ ６／３
-                    new Regex(
-                        $@"({IntegerExtractor.SignSymbolRegexNum}\s*)?{IntegerExtractor
-                            .ZeroToNineChsFullHalfRegexChs}+\s+{IntegerExtractor
-                                .ZeroToNineChsFullHalfRegexChs}+[/／]{IntegerExtractor.ZeroToNineChsFullHalfRegexChs}+",
-                        RegexOptions.IgnoreCase | RegexOptions.Singleline)
+                    new Regex(NumbersDefinitions.FractionNotationSpecialsCharsRegex, RegexOptions.IgnoreCase | RegexOptions.Singleline)
                     , "FracNum"
                 },
+                // 8/3 
                 {
-                    // 8/3
-                    new Regex(
-                        $@"({IntegerExtractor.SignSymbolRegexNum}\s*)?{IntegerExtractor
-                            .ZeroToNineChsFullHalfRegexChs}+[/／]{IntegerExtractor
-                                .ZeroToNineChsFullHalfRegexChs}+",
-                        RegexOptions.IgnoreCase | RegexOptions.Singleline)
+                    new Regex(NumbersDefinitions.FractionNotationRegex, RegexOptions.IgnoreCase | RegexOptions.Singleline)
                     , "FracNum"
                 },
+                //四分之六十五
                 {
-                    //四分之六十五
-                    new Regex($@"{AllFractionNumber}", RegexOptions.Singleline)
+                    new Regex(NumbersDefinitions.AllFractionNumber, RegexOptions.Singleline)
                     , "FracChs"
                 }
             };
-
             Regexes = regexes.ToImmutableDictionary();
         }
     }
--- .NET/Microsoft.Recognizers.Text.Number/Chinese/Extractors/OrdinalExtractor.cs
+++ .NET/Microsoft.Recognizers.Text.Number/Chinese/Extractors/OrdinalExtractor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Text.RegularExpressions;
+using Microsoft.Recognizers.Definitions.Chinese;
 
 namespace Microsoft.Recognizers.Text.Number.Chinese
 {
@@ -8,26 +9,22 @@ namespace Microsoft.Recognizers.Text.Number.Chinese
     {
         internal sealed override ImmutableDictionary<Regex, string> Regexes { get; }
         protected sealed override string ExtractType { get; } = Constants.SYS_NUM_ORDINAL;
-
-        public static string OrdinalRegexChs => $@"第{IntegerExtractor.AllIntRegexChs}";
-
+        
         public OrdinalExtractor()
         {
             var regexes = new Dictionary<Regex, string>
             {
-                
+                //第一百五十四
                 {
-                    //第一百五十四
-                    new Regex(OrdinalRegexChs, RegexOptions.Singleline)
+                    new Regex(NumbersDefinitions.OrdinalRegexChs, RegexOptions.Singleline)
                     , "OrdinalChs"
                 },
+                //第２５６５,  第1234
                 {
-                    //第２５６５,  第1234
-                    new Regex($@"第{IntegerExtractor.ZeroToNineChsFullHalfRegexChs}+",RegexOptions.Singleline)
+                    new Regex(NumbersDefinitions.OrdinalNumbersRegex, RegexOptions.Singleline)
                     , "OrdinalChs"
                 }
             };
-
             Regexes = regexes.ToImmutableDictionary();
         }
     }
