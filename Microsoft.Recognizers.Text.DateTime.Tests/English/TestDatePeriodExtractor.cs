5330a80abd0335ff8d2e90b8f47f69a6ef3aa3d6
borjekar <borjekar@microsoft.com>
2017-08-21 16:43:32 +0800

    Fixing bug where substrings of a word where recognized as a datetime.

--- Microsoft.Recognizers.Text.DateTime.Tests/English/TestDatePeriodExtractor.cs
+++ Microsoft.Recognizers.Text.DateTime.Tests/English/TestDatePeriodExtractor.cs
@@ -3,44 +3,64 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Microsoft.Recognizers.Text.DateTime.Tests.English
 {
-    [TestClass]
-    public class TestDatePeriodExtractor
-    {
-        private readonly BaseDatePeriodExtractor extractor = new BaseDatePeriodExtractor(new EnglishDatePeriodExtractorConfiguration());
 
-        public void BasicTest(string text, int start, int length)
+    [TestClass]
+    public class TestDatePeriodExtractor {
+
+        private readonly BaseDatePeriodExtractor extractor =
+            new BaseDatePeriodExtractor(new EnglishDatePeriodExtractorConfiguration());
+
+        public void BasicTest(string text, int start, int length, int expected = 1)
         {
+
             var results = extractor.Extract(text);
-            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual(expected, results.Count);
+
+            if (expected < 1) {
+                return;
+            }
+
             Assert.AreEqual(start, results[0].Start);
             Assert.AreEqual(length, results[0].Length);
             Assert.AreEqual(Constants.SYS_DATETIME_DATEPERIOD, results[0].Type);
         }
 
-        private string[] shortMonths = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Sept", "Oct", "Nov", "Dec" };
-        private string[] fullMonths = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+        public void BasicNegativeTest(string text)
+        {
+            BasicTest(text, -1, -1, 0);
+        }
 
+        private readonly string[] shortMonths = {
+            "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Sept", "Oct", "Nov", "Dec"
+        };
+
+        private readonly string[] fullMonths = {
+            "January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+            "November", "December"
+        };
 
         [TestMethod]
-        public void TestDatePeriodExtract()
+        public void TestDatePeriodExtract() {
+
+            foreach (var month in shortMonths) {
+                BasicTest($"I'll be out in {month}", 15, month.Length);
+                BasicTest($"I'll be out this {month}", 12, 5 + month.Length);
+                BasicTest($"I was missing {month} 2001", 14, 5 + month.Length);
+                BasicTest($"I was missing {month}, 2001", 14, 6 + month.Length);
+            }
+
+            foreach (var month in fullMonths) {
+                BasicTest($"I'll be out in {month}", 15, month.Length);
+                BasicTest($"I'll be out this {month}", 12, 5 + month.Length);
+                BasicTest($"I was missing {month} 2001", 14, 5 + month.Length);
+                BasicTest($"I was missing {month}, 2001", 14, 6 + month.Length);
+            }
+
+        }
+
+        [TestMethod]
+        public void TestDatePeriodExtractBasicCases()
         {
-            foreach (var month in shortMonths)
-            {
-                BasicTest($"I'll be out in {month}", 15, month.Length);
-                BasicTest($"I'll be out this {month}", 12, 5 + month.Length);
-                BasicTest($"I was missing {month} 2001", 14, 5 + month.Length);
-                BasicTest($"I was missing {month}, 2001", 14, 6 + month.Length);
-            }
-
-            foreach (var month in fullMonths)
-            {
-                BasicTest($"I'll be out in {month}", 15, month.Length);
-                BasicTest($"I'll be out this {month}", 12, 5 + month.Length);
-                BasicTest($"I was missing {month} 2001", 14, 5 + month.Length);
-                BasicTest($"I was missing {month}, 2001", 14, 6 + month.Length);
-            }
-
-            // test basic cases
             BasicTest("I'll be out from 4 to 22 this month", 12, 23);
             BasicTest("I'll be out from 4-23 in next month", 12, 23);
             BasicTest("I'll be out from 3 until 12 of Sept hahaha", 12, 23);
@@ -70,7 +90,11 @@ namespace Microsoft.Recognizers.Text.DateTime.Tests.English
             BasicTest("I'll be out past 3 weeks", 12, 12);
             BasicTest("I'll be out last 3year", 12, 10);
             BasicTest("I'll be out previous 3 weeks", 12, 16);
+        }
 
+        [TestMethod]
+        public void TestDatePeriodExtractMErgingTwoTimepoints()
+        {
             // test merging two time points
             BasicTest("I'll be out Oct. 2 to October 22", 12, 20);
             BasicTest("I'll be out January 12, 2016 - 02/22/2016", 12, 29);
@@ -102,22 +126,34 @@ namespace Microsoft.Recognizers.Text.DateTime.Tests.English
 
             BasicTest("I'll be out the third week of 2027", 12, 22);
             BasicTest("I'll be out the third week next year", 12, 24);
+        }
 
+        [TestMethod]
+        public void TestDatePeriodExtractSeason()
+        {
             BasicTest("I'll leave this summer", 11, 11);
             BasicTest("I'll leave next spring", 11, 11);
             BasicTest("I'll leave the summer", 15, 6);
             BasicTest("I'll leave summer", 11, 6);
             BasicTest("I'll leave summer 2016", 11, 11);
             BasicTest("I'll leave summer of 2016", 11, 14);
+        }
 
-            //test week of and month of
-            BasicTest("week of september.15th", 0, 22);
-            BasicTest("month of september.15th", 0, 23);
-
+        [TestMethod]
+        public void TestDatePeriodExtractNextAndUpcoming() {
             //next and upcoming
             BasicTest("upcoming month holidays", 0, 14);
             BasicTest("next month holidays", 0, 10);
-
         }
+
+        [TestMethod]
+        public void TestDatePeriodExtractWeekOf()
+        {
+            //test week of and month of
+            BasicTest("week of september.15th", 0, 22);
+            BasicTest("month of september.15th", 0, 23);
+        }
+
     }
+
 }
