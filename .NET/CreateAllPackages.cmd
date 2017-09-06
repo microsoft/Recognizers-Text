119e8330175f99ed2b16a9595617f6eb90c0ceab
borjekar <borjekar@microsoft.com>
2017-09-06 18:48:56 +0800

    Fixing build scripts after code move to .NET dir

--- .NET/CreateAllPackages.cmd
+++ .NET/CreateAllPackages.cmd
@@ -1,4 +1,17 @@
 @echo off
+set dir=%1
+
+rem Enter dir specified as 1st argument (e.g. .NET)
+IF NOT [%dir%] == [] IF NOT EXIST %dir%\NUL GOTO NODIR
+
+IF [%dir%] == [] IF NOT EXIST .\Microsoft.Recognizers.Text\NUL GOTO NOSPECDIR
+
+echo .%dir%.
+
+:DIROK
+pushd .\%dir%
+
+rem Go through Sub-packages
 pushd Microsoft.Recognizers.Text
 call CreatePackage.cmd
 popd
@@ -11,3 +24,25 @@ popd
 pushd Microsoft.Recognizers.Text.DateTime
 call CreatePackage.cmd
 popd
+rem Exit .NET dir
+popd
+
+GOTO END
+
+:NOSPECDIR
+echo "Please specify a directory where the package directories containing .cmd scripts should be found. Abort."
+
+GOTO BADEND
+
+:NODIR
+echo "Dir %dir% not found. Abort."
+
+rem exit 1
+GOTO BADEND
+
+:BADEND
+exit /b 1
+
+GOTO END
+
+:END
