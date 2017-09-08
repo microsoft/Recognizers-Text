08485deeeea80797efaa23027d7bde706d6eced2
Enzo Cano <enzo.cano@southworks.com>
2017-09-08 16:59:16 -0300

    sanitize Test and Class name

--- .NET/Microsoft.Recognizers.Text.Tests/TestWriter.cs
+++ .NET/Microsoft.Recognizers.Text.Tests/TestWriter.cs
@@ -8,15 +8,20 @@ namespace Microsoft.Recognizers.Text
 {
     public static class TestWriter
     {
-        private const string separator = "|";
+        private const string separator = "\t";
         public static void Write(IEnumerable<string> values)
         {
             Trace.WriteLine(string.Join(separator, values));
         }
 
-        public static string getFileName(string path)
+        private static string getFileName(string path)
         {
-            return Path.GetFileNameWithoutExtension(path).Replace("Chs", "");
+            return Path.GetFileNameWithoutExtension(path).Replace("Chs", "").Replace("Test", "");
+        }
+        
+        private static string getName(object obj)
+        {
+            return obj.GetType().Name.Replace("Chs", "");
         }
 
         private static string getJson<T>(T obj)
@@ -31,63 +36,63 @@ namespace Microsoft.Recognizers.Text
         public static void Write(string lang, IModel model, string source, ModelResult result, [CallerFilePath] string callerFilePath = "", [CallerMemberName] string callerMemberName = "")
         {
             var fileName = getFileName(callerFilePath);
-            var modelStr = model.GetType().ToString();
+            var modelStr = getName(model);
             var resultStr = getJson(result);
-            Write(new string[] { lang, fileName, callerMemberName, modelStr, source, resultStr });
+            Write(new string[] { lang, fileName, callerMemberName, modelStr, source, "1", resultStr });
         }
 
         public static void Write(string lang, IModel model, string source, int count, [CallerFilePath] string callerFilePath = "", [CallerMemberName] string callerMemberName = "")
         {
             var fileName = getFileName(callerFilePath);
-            var modelStr = model.GetType().ToString();
+            var modelStr = getName(model);
             Write(new string[] { lang, fileName, callerMemberName, modelStr, source, count.ToString() });
         }
 
         public static void Write(string lang, IModel model, string source, ModelResult result, int count, [CallerFilePath] string callerFilePath = "", [CallerMemberName] string callerMemberName = "")
         {
             var fileName = getFileName(callerFilePath);
-            var modelStr = model.GetType().ToString();
+            var modelStr = getName(model);
             var resultStr = getJson(result);
-            Write(new string[] { lang, fileName, callerMemberName, modelStr, source, resultStr, count.ToString() });
+            Write(new string[] { lang, fileName, callerMemberName, modelStr, source, count.ToString(), resultStr });
         }
 
         public static void Write(string lang, IModel model, string source, IEnumerable<ModelResult> results, int count, [CallerFilePath] string callerFilePath = "", [CallerMemberName] string callerMemberName = "")
         {
             var fileName = getFileName(callerFilePath);
-            var modelStr = model.GetType().ToString();
+            var modelStr = getName(model);
             var resultStr = getJson(results);
-            Write(new string[] { lang, fileName, callerMemberName, modelStr, source, resultStr, count.ToString() });
+            Write(new string[] { lang, fileName, callerMemberName, modelStr, source, count.ToString(), resultStr });
         }
 
         public static void Write(string lang, IExtractor extractor, string source, ExtractResult result, [CallerFilePath] string callerFilePath = "", [CallerMemberName] string callerMemberName = "")
         {
             var fileName = getFileName(callerFilePath);
-            var modelStr = extractor.GetType().ToString();
+            var modelStr = getName(extractor);
             var resultStr = getJson(result);
-            Write(new string[] { lang, fileName, callerMemberName, modelStr, source, resultStr });
+            Write(new string[] { lang, fileName, callerMemberName, modelStr, source, "1", resultStr });
         }
 
         public static void Write(string lang, IExtractor extractor, string source, IEnumerable<ExtractResult> results, int count, [CallerFilePath] string callerFilePath = "", [CallerMemberName] string callerMemberName = "")
         {
             var fileName = getFileName(callerFilePath);
-            var modelStr = extractor.GetType().ToString();
+            var modelStr = getName(extractor);
             var resultStr = getJson(results);
-            Write(new string[] { lang, fileName, callerMemberName, modelStr, source, resultStr, count.ToString() });
+            Write(new string[] { lang, fileName, callerMemberName, modelStr, source, count.ToString(), resultStr });
         }
 
         public static void Write(string lang, IExtractor extractor, string source, [CallerFilePath] string callerFilePath = "", [CallerMemberName] string callerMemberName = "")
         {
             var fileName = getFileName(callerFilePath);
-            var modelStr = extractor.GetType().ToString();
+            var modelStr = getName(extractor);
             Write(new string[] { lang, fileName, callerMemberName, modelStr, source, "0"});
         }
 
         public static void Write(string lang, IParser parser, string source, ParseResult result, [CallerFilePath] string callerFilePath = "", [CallerMemberName] string callerMemberName = "")
         {
             var fileName = getFileName(callerFilePath);
-            var modelStr = parser.GetType().ToString();
+            var modelStr = getName(parser);
             var resultStr = getJson(result);
-            Write(new string[] { lang, fileName, callerMemberName, modelStr, source, resultStr });
+            Write(new string[] { lang, fileName, callerMemberName, modelStr, source, "1", resultStr });
         }
     }
 }
