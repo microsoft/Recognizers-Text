29b9654c537b88579b448d58aa691c8ce9e3eb06
lmarambio <lucas.marambio@southworks.com>
2017-10-04 16:37:55 -0300

    Update Recognition sample to clean the code and make the output prettier

--- .NET/Samples/SimpleConsole/Program.cs
+++ .NET/Samples/SimpleConsole/Program.cs
@@ -11,14 +11,12 @@ namespace SimpleConsole
 {
     class Program
     {
-        private const string defaultCulture = "en-us";
+        // Use English for the Recognizers culture
+        private const string defaultCulture = Culture.English;
 
         static void Main(string[] args)
         {
-            // Show introduction
-            Console.WriteLine("Welcome to the Recognizer's Sample console application!");
-            Console.WriteLine("To try the recognizers enter a phrase and let us show you the different outputs for each recognizer or just type 'exit' to leave the application.");
-            Console.WriteLine();
+            ShowIntro();
 
             while (true)
             {
@@ -27,10 +25,9 @@ namespace SimpleConsole
                 string input = Console.ReadLine().Trim();
                 Console.WriteLine();
 
-                // Close application if user types "exit"
                 if (input.ToLower() == "exit")
                 {
-                    Console.WriteLine("Goodbye!");
+                    // Close application if user types "exit"
                     break;
                 }
                 else
@@ -38,35 +35,70 @@ namespace SimpleConsole
                     // Validate input 
                     if (input.Length > 0)
                     {
-                        // Parse the input text and show the result of each recognizer as JSON.
-                        GetParsers().Select(r =>
-                            $"{r.GetType().Name}\t: {JsonConvert.SerializeObject(r.Parse(input))}")
-                            .ToList()
-                            .ForEach(x => Console.WriteLine(x.ToString()));
-                        
-                        //Leave a blank line
+                        // Retrieve all the parsers and call 'Parse' to recognize all the values from the user input
+                        var results = GetParsers()
+                            .Select(parser => parser.Parse(input))
+                            .SelectMany(a => a);
+
+                        // Write output
+                        Console.WriteLine(results.Count() > 0 ? (string.Format("I found the following entities ({0:d}):", results.Count())) : "I found no entities.");
+                        results.ToList().ForEach(result => Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented)));
                         Console.WriteLine();
                     }
                 }
             }
         }
-
+        
+        /// <summary>
+        /// Introduction
+        /// </summary>
+        private static void ShowIntro()
+        {
+            Console.WriteLine("Welcome to the Recognizer's Sample console application!");
+            Console.WriteLine("To try the recognizers enter a phrase and let us show you the different outputs for each recognizer or just type 'exit' to leave the application.");
+            Console.WriteLine();
+        }
+        
         /// <summary>
         /// Get all recognizers model instances.
         /// </summary>
         /// <returns>A list of all the existing recognizer's models</returns>
         private static IEnumerable<IModel> GetParsers()
         {
-            List<IModel> modelList = new List<IModel>();
-            modelList.Add(NumberRecognizer.Instance.GetNumberModel(defaultCulture));
-            modelList.Add(NumberRecognizer.Instance.GetOrdinalModel(defaultCulture));
-            modelList.Add(NumberRecognizer.Instance.GetPercentageModel(defaultCulture));
-            modelList.Add(NumberWithUnitRecognizer.Instance.GetAgeModel(defaultCulture));
-            modelList.Add(NumberWithUnitRecognizer.Instance.GetCurrencyModel(defaultCulture));
-            modelList.Add(NumberWithUnitRecognizer.Instance.GetDimensionModel(defaultCulture));
-            modelList.Add(NumberWithUnitRecognizer.Instance.GetTemperatureModel(defaultCulture));
-            modelList.Add(DateTimeRecognizer.GetInstance().GetDateTimeModel(defaultCulture));
-            return modelList;
+            return new IModel[]
+            {
+                // Add Number recognizer - This recognizer will find any number from the input
+                // E.g "I have two apples" will return "2".
+                NumberRecognizer.Instance.GetNumberModel(defaultCulture),
+                
+                // Add Ordinal number recognizer - This recognizer will find any ordinal number
+                // E.g "eleventh" will return "11".
+                NumberRecognizer.Instance.GetOrdinalModel(defaultCulture),
+
+                // Add Percentage recognizer - This recognizer will find any number presented as percentage
+                // E.g "one hundred percents" will return "100%"
+                NumberRecognizer.Instance.GetPercentageModel(defaultCulture),
+
+                // Add Age recognizer - This recognizer will find any age number presented
+                // E.g "After ninety five years of age, perspectives change" will return "95 Year"
+                NumberWithUnitRecognizer.Instance.GetAgeModel(defaultCulture),
+
+                // Add Currency recognizer - This recognizer will find any currency presented
+                // E.g "Interest expense in the 1988 third quarter was $ 75.3 million" will return "75300000 Dollar"
+                NumberWithUnitRecognizer.Instance.GetCurrencyModel(defaultCulture),
+
+                // Add Dimension recognizer - This recognizer will find any dimension presented
+                // E.g "The six-mile trip to my airport hotel that had taken 20 minutes earlier in the day took more than three hours." will return "6 Mile"
+                NumberWithUnitRecognizer.Instance.GetDimensionModel(defaultCulture),
+
+                // Add Temperature recognizer - This recognizer will find any temperature presented
+                // E.g "Set the temperature to 30 degrees celsius" will return "30 C"
+                NumberWithUnitRecognizer.Instance.GetTemperatureModel(defaultCulture),
+
+                // Add Datetime recognizer - This model will find any Date even if its write in coloquial language - 
+                // E.g "I'll go back 8pm today" will return "2017-10-04 20:00:00"
+                DateTimeRecognizer.GetInstance().GetDateTimeModel(defaultCulture)
+            };
         }
     }
 }
