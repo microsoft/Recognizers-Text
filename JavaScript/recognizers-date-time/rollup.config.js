b0257bd578d77e8f8460fec2b25432618c43f191
Enzo Cano <enzo.cano@southworks.com>
2017-10-04 16:38:45 -0300

    Rename module folders

--- JavaScript/recognizers-date-time/rollup.config.js
+++ JavaScript/recognizers-date-time/rollup.config.js
@@ -4,7 +4,7 @@ import sourceMaps from 'rollup-plugin-sourcemaps'
 const pkg = require('./package.json')
 const camelCase = require('lodash.camelcase')
 
-const libraryName = 'recognizers-text-datetime'
+const libraryName = 'recognizers-text-date-time'
 
 export default {
   entry: `compiled/${libraryName}.js`,
