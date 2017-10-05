dbf14bc8bc190cb1cacba0377dc1aa1fd004d3a1
Enzo Cano <enzo.cano@southworks.com>
2017-10-05 14:40:17 -0300

    Update console-app

--- console-app/compiled/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const recognizers_text_number_1 = require("recognizers-text-number");
-let numberRecognizer = recognizers_text_number_1.NumberRecognizer.instance.getNumberModel(recognizers_text_number_1.Culture.English);
-let data = numberRecognizer.parse("I have twenty apples");
-console.log(JSON.stringify(data));
-//# sourceMappingURL=index.js.map
