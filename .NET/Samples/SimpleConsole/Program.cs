4a9730f01d453208907bc94e32e8ae04506de185
lmarambio <lucas.marambio@southworks.com>
2017-10-05 11:53:44 -0300

    Update introduction to SimpleConsole sample and rename "GetParsers" to "GetModels"

--- .NET/Samples/SimpleConsole/Program.cs
+++ .NET/Samples/SimpleConsole/Program.cs
@@ -36,7 +36,7 @@ namespace SimpleConsole
                     if (input.Length > 0)
                     {
                         // Retrieve all the parsers and call 'Parse' to recognize all the values from the user input
-                        var results = GetParsers()
+                        var results = GetModels()
                             .Select(parser => parser.Parse(input))
                             .SelectMany(a => a);
 
@@ -57,13 +57,23 @@ namespace SimpleConsole
             Console.WriteLine("Welcome to the Recognizer's Sample console application!");
             Console.WriteLine("To try the recognizers enter a phrase and let us show you the different outputs for each recognizer or just type 'exit' to leave the application.");
             Console.WriteLine();
+            Console.WriteLine("Here are some examples you could try:");
+            Console.WriteLine();
+            Console.WriteLine("\" I want twenty meters of cable for tomorrow \"");
+            Console.WriteLine("\" I'll be available tomorrow from 11am to 2pm to receive up to 5kg of sugar\"");
+            Console.WriteLine("\" I'll be out between 4 and 22 this month \"");
+            Console.WriteLine("\" I was the fifth person to finish the 10 km race \"");
+            Console.WriteLine("\" The temperature this night will be of 40 deg celsius \"");
+            Console.WriteLine("\" The american stock exchange said a seat was sold for down $ 5,000 from the previous sale last friday \"");
+            Console.WriteLine("\" It happened when the baby was only ten months old \"");
+            Console.WriteLine();
         }
         
         /// <summary>
         /// Get all recognizers model instances.
         /// </summary>
         /// <returns>A list of all the existing recognizer's models</returns>
-        private static IEnumerable<IModel> GetParsers()
+        private static IEnumerable<IModel> GetModels()
         {
             return new IModel[]
             {
