a171fe3f521e2f5c30b5a3a649fbf28b0a4e94da
Pablo Costantini <pablo.costantini@southworks.com>
2017-10-17 16:17:46 -0300

    Parse date and ranges of dates (and times)

--- JavaScript/samples/botbuilder/index.js
+++ JavaScript/samples/botbuilder/index.js
@@ -6,6 +6,7 @@ var dateModel = Recognizers.DateTimeRecognizer.instance.getDateTimeModel(Recogni
 // This loads the environment variables from the .env file
 require('dotenv-extended').load();
 
+var _ = require('lodash');
 var builder = require('botbuilder');
 var restify = require('restify');
 var helpers = require('./helpers');
@@ -25,7 +26,6 @@ server.post('/api/messages', connector.listen());
 
 var bot = new builder.UniversalBot(connector, [
     function (session) {
-
         // Welcome message
         var welcomeCard = new builder.HeroCard(session)
             .title('Welcome to Contoso Roses')
@@ -56,17 +56,26 @@ var bot = new builder.UniversalBot(connector, [
 
         // store amount
         session.dialogData.amount = amount;
-
         // Prompt for delivery date
         var promptMessage = [
-            'When do you want to delivery to be sent?',
+            'When do you want the delivery to be sent?',
             'Some valid options are:',
-            ' - ASAP',
             ' - Tomorrow morning',
             ' - 12/30/2017',
-            ' - 9PM Tomorrow'];
-
+            ' - 9PM Tomorrow',
+            ' - Five hours from now',
+            ' - From 9AM to 10AM tomorrow'];
         session.beginDialog('ask-date', { prompt: promptMessage.join('\n\n') });
+    },
+
+    function (session, results) {
+        var momentOrRange = results.response;
+        var amount = session.dialogData.amount;
+        var nRoses = session.ngettext(`just one rose`, `${amount} roses`, amount);
+        session.send(`Thank you! I'll deliver ${nRoses} ${momentOrRangeToString(momentOrRange)}.`);
+
+        // TODO: Continue to checkout
+        session.send('Have a nice day!');
     }
 ]);
 
@@ -75,7 +84,7 @@ var bot = new builder.UniversalBot(connector, [
 bot.dialog('ask-amount', new builder.Prompt().onRecognize((context, callback) => {
     var input = context.message.text || '';
     var results = numberModel.parse(input);
-    console.log('numberModel parse results', results);
+    console.log('numberModel parse results: ', results);
 
     if (results.length && results[0].typeName === 'number') {
         var first = results[0];
@@ -94,35 +103,24 @@ bot.dialog('ask-amount', new builder.Prompt().onRecognize((context, callback) =>
     callback(null, 0);
 }));
 
+var DateValidationErros = {
+    'past_value': 'I\'m sorry, but I need at least an hour to deliver.\n\n $moment$ is no good for me.\n\nWhat other moment suits you best?',
+    'default': 'I\'m sorry, that doesn\'t seem to be a valid delivery date and time'
+};
+
 // Ask for delivery date and validate input
 bot.dialog('ask-date', new builder.Prompt().onRecognize((context, callback) => {
-    var input = context.message.text || '';
-    var results = dateModel.parse(input);
-    console.log('dateModel parser results: ', results.length);
-    results.forEach((r, ix) => {
-        console.log(`result[${ix}]:`, r);
-        if (r.resolution && r.resolution.get) {
-            console.log(`result[${ix}].resolution:`, r.resolution.get('values').map(helpers.toObject))
-        }
-    });
+    var result = validateAndExtract(context.message.text || '');
+    console.log('ask-date-result:', result);
 
-    if (results.length && results[0].typeName.startsWith('datetimeV2')) {
-        // The DateTime model can return several resolution types (https://github.com/Microsoft/Recognizers-Text/blob/master/JavaScript/recognizers-date-time/src/dateTime/constants.ts#L2-L9)
-        // We only care for those with a date, date and time, or date time period:
-        // date, daterange, datetime, datetimerange
-        var first = results[0];
-        if(first.typeName.split('.')[1].includes('date')) {
-            console.log("*******************************", first.resolution.get("values"));
-            // callback(null, 1, resolution)
-            // return;
-
-        }
+    if (result.valid) {
+        return callback(null, 1, result.value);
     }
 
-    context.dialogData.options.prompt = 'I\'m sorry, that doesn\'t seem to be a valid delivery date and time';
-    context.dialogData.options.prompt = 'I\'m sorry, this is not yet implemented';
+    // Set error message and re-prompt;
+    var errorTemplate = DateValidationErros[result.error] || DateValidationErros.default;
+    context.dialogData.options.prompt = errorTemplate.replace('$moment$', momentOrRangeToString(result.value));
     callback(null, 0);
-
 }));
 
 // Send welcome when conversation with bot is started, by initiating the root dialog
@@ -140,3 +138,97 @@ bot.on('conversationUpdate', function (message) {
 bot.on('error', function (e) {
     console.log('And error ocurred', e);
 });
+
+// Date Helpers
+function validateAndExtract(input) {
+    var results = dateModel.parse(input);
+
+    // Log de results
+    results.forEach((r, ix) => {
+        console.log(`DateModel.result[${ix}]:`, r);
+        if (r.resolution && r.resolution.get) {
+            console.log(`DateModel.result[${ix}].resolution:`, r.resolution.get('values').map(helpers.toObject))
+        }
+    });
+
+    // Check there are valid results
+    if (results.length && results[0].typeName.startsWith('datetimeV2')) {
+        // The DateTime model can return several resolution types (https://github.com/Microsoft/Recognizers-Text/blob/master/JavaScript/recognizers-date-time/src/dateTime/constants.ts#L2-L9)
+        // We only care for those with a date, date and time, or date time period:
+        // date, daterange, datetime, datetimerange
+
+        var first = results[0];
+        var subType = first.typeName.split('.')[1];
+        var resolutionValues = first.resolution && first.resolution.get("values");
+
+        if(!resolutionValues) {
+            // no resolution values
+            return {
+                valid: false
+            }
+        }
+
+        if (subType.includes('date') && !subType.includes('range')) {
+            // a single date (or date & time)
+            var moment = new Date(resolutionValues[0].get("value"));
+            if (!isNaN(moment.getTime())) {
+                // a valid date
+                if (isFutureMoment(moment)) {
+                    // a future moment, valid!
+                    return {
+                        valid: true,
+                        value: moment
+                    };
+                }
+
+                // a past moment
+                return {
+                    valid: false,
+                    error: 'past_value',
+                    value: moment,
+                }
+            }
+        } else if (subType.includes('date') && subType.includes('range')) {
+            // range
+            var from = new Date(resolutionValues[0].get('start'));
+            var to = new Date(resolutionValues[0].get('end'));
+            if (!isNaN(from.getTime()) && !isNaN(to.getTime())) {
+                if (isFutureMoment(from) && isFutureMoment(to)) {
+                    // future
+                    return {
+                        valid: true,
+                        value: [from, to]
+                    };
+                }
+
+                // past
+                return {
+                    valid: false,
+                    error: 'past_value',
+                    value: [from, to]
+                };
+            }
+        }
+    }
+
+    return {
+        valid: false
+    };
+}
+
+function isFutureMoment(date) {
+    // at least one hour
+    var anHour = 1000 * 60 * 60;
+    return date.getTime() > (Date.now() + anHour);
+}
+
+function momentOrRangeToString(moment, prefix) {
+    prefix = prefix !== undefined ? prefix : 'on ';
+    if (_.isDate(moment)) {
+        return prefix + moment.toLocaleString('en-US');
+    } else if (_.isArray(moment)) {
+        return 'from ' + moment.map(m => momentOrRangeToString(m, '')).join(' to ');
+    }
+
+    return 'not supported';
+}
