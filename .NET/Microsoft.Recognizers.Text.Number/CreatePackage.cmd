8812c232c9793b25594fb836b6b2921b9aaaa9e4
borjekar <borjekar@microsoft.com>
2017-10-24 10:56:45 +0800

    - Updating French definition .cs files according to latest YAML; - Upgrading nuget CLI dependency; - Standardizing on .NET 4.6.2 as previously sub-projects where all over the place; - Adding dependency to vswhere nuget package to avoid build not finding msbuild.

--- .NET/Microsoft.Recognizers.Text.Number/CreatePackage.cmd
+++ .NET/Microsoft.Recognizers.Text.Number/CreatePackage.cmd
@@ -3,11 +3,16 @@ echo *** Building Microsoft.Recognizers.Text.Number
 setlocal
 setlocal enabledelayedexpansion
 setlocal enableextensions
-set errorlevel=0
+rem set errorlevel=0
+
+for /f "usebackq tokens=*" %%i in (`..\packages\vswhere.2.2.7\tools\vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+  set MSBuildDir=%%i\MSBuild\15.0\Bin\
+)
+
 mkdir ..\nuget
 erase /s ..\nuget\Microsoft.Recognizers.Text.Number*.nupkg
-msbuild /property:Configuration=release Microsoft.Recognizers.Text.Number.csproj
+"%MSBuildDir%\MSBuild\15.0\Bin\MSBuild.exe" /property:Configuration=release Microsoft.Recognizers.Text.Number.csproj
 for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Recognizers.Text.dll).FileVersionInfo.FileVersion"') do set basic=%%v
 for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Recognizers.Text.Number.dll).FileVersionInfo.FileVersion"') do set numberVersion=%%v
-..\packages\NuGet.CommandLine.3.5.0\tools\NuGet.exe pack Microsoft.Recognizers.Text.Number.nuspec -symbols -properties version=%numberVersion%;basic=%basic% -OutputDirectory ..\nuget
+..\packages\NuGet.CommandLine.4.3.0\tools\NuGet.exe pack Microsoft.Recognizers.Text.Number.nuspec -symbols -properties version=%numberVersion%;basic=%basic% -OutputDirectory ..\nuget
 echo *** Finished building Microsoft.Recognizers.Text.Number
--- .NET/Microsoft.Recognizers.Text.NumberWithUnit/CreatePackage.cmd
+++ .NET/Microsoft.Recognizers.Text.NumberWithUnit/CreatePackage.cmd
@@ -3,12 +3,17 @@ echo *** Building Microsoft.Recognizers.Text.NumberWithUnit
 setlocal
 setlocal enabledelayedexpansion
 setlocal enableextensions
-set errorlevel=0
+rem set errorlevel=0
+
+for /f "usebackq tokens=*" %%i in (`..\packages\vswhere.2.2.7\tools\vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+  set MSBuildDir=%%i\MSBuild\15.0\Bin\
+)
+
 mkdir ..\nuget
 erase /s ..\nuget\Microsoft.Recognizers.Text.NumberWithUnit*nupkg
-msbuild /property:Configuration=release Microsoft.Recognizers.Text.NumberWithUnit.csproj
+"%MSBuildDir%\MSBuild\15.0\Bin\MSBuild.exe" /property:Configuration=release Microsoft.Recognizers.Text.NumberWithUnit.csproj
 for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Recognizers.Text.dll).FileVersionInfo.FileVersion"') do set basic=%%v
 for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Recognizers.Text.Number.dll).FileVersionInfo.FileVersion"') do set number=%%v
 for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Recognizers.Text.NumberWithUnit.dll).FileVersionInfo.FileVersion"') do set version=%%v
-..\packages\NuGet.CommandLine.3.5.0\tools\NuGet.exe pack Microsoft.Recognizers.Text.NumberWithUnit.nuspec -symbols -properties version=%version%;basic=%basic%;number=%number% -OutputDirectory ..\nuget
+..\packages\NuGet.CommandLine.4.3.0\tools\NuGet.exe pack Microsoft.Recognizers.Text.NumberWithUnit.nuspec -symbols -properties version=%version%;basic=%basic%;number=%number% -OutputDirectory ..\nuget
 echo *** Finished building Microsoft.Recognizers.Text.NumberWithUnit
