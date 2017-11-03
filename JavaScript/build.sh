3ccbdbecf75852b24fa3bbd620b2db663a7528f2
Pablo Costantini <sixdemons@gmail.com>
2017-11-02 21:22:20 -0300

    build.sh for *nix systems

---
+++ JavaScript/build.sh
@@ -0,0 +1,20 @@
+echo // Building Javacript platform
+
+echo // Building recognizers number module
+npm run prebuild-number
+
+echo // Building recognizers number-with-unit module
+npm run prebuild-number-with-unit
+
+echo // Building recognizers date-time module
+npm run prebuild-date-time
+
+REM Build main module
+echo // Installing dependencies - npm install
+npm i
+
+echo // Building - npm run build
+npm run build
+
+echo // Running test - npm run test
+npm run test
