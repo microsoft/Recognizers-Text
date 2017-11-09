a79e817ad3dc9e4635643116db575ba93f15c4fb
Yiyong Lin <yiyolin@microsoft.com>
2017-11-08 20:31:40 -0600

    Add  multi-framework dlls to nuget packages (#180)
    * Add  muti-framework dll to nuget package
    
    * upload artifacts
    
    * Only upload Microsoft.Recognizers packages
    
    * Build should fail because of packages
    
    * Fix the failure to call create packages
    
    * Update newtonsoft to keep the same version as other project
    
    * Add createPackage
    
    * Fix by comments
    
    * Add vswhere and NuGet.CommandLine dependency to DataDrivenTests project to get these packages to the package folder
    
    * Revert "Update newtonsoft to keep the same version as other project"
    
    This reverts commit a364adcd568e13e04a37890dfd96a541a8d865d1.

--- .NET/Microsoft.Recognizers.Text.Number/CreatePackage.cmd
+++ .NET/Microsoft.Recognizers.Text.Number/CreatePackage.cmd
@@ -11,8 +11,8 @@ for /f "usebackq tokens=*" %%i in (`..\packages\vswhere.2.2.7\tools\vswhere -lat
 if not exist ..\nuget mkdir ..\nuget
 if exist ..\nuget\Microsoft.Recognizers.Text.Number*.nupkg erase /s ..\nuget\Microsoft.Recognizers.Text.Number*.nupkg
 "%MSBuildDir%\MSBuild\15.0\Bin\MSBuild.exe" /property:Configuration=release Microsoft.Recognizers.Text.Number.csproj
-for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Recognizers.Text.dll).FileVersionInfo.FileVersion"') do set basic=%%v
-for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Recognizers.Text.Number.dll).FileVersionInfo.FileVersion"') do set numberVersion=%%v
+for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\net462\Microsoft.Recognizers.Text.dll).FileVersionInfo.FileVersion"') do set basic=%%v
+for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\net462\Microsoft.Recognizers.Text.Number.dll).FileVersionInfo.FileVersion"') do set numberVersion=%%v
 ..\packages\NuGet.CommandLine.4.3.0\tools\NuGet.exe pack Microsoft.Recognizers.Text.Number.nuspec -symbols -properties version=%numberVersion%;basic=%basic% -OutputDirectory ..\nuget
 
 set error=%errorlevel%
