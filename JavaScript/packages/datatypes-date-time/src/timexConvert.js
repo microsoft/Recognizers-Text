67cdef4d54c781397725f8805b549280894e0fd0
John Taylor <johtaylo@microsoft.com>
2017-11-29 12:01:53 -0800

    add TIMEX datatype support

---
+++ JavaScript/packages/datatypes-date-time/src/timexConvert.js
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+const en = require('./en/timexConvert.js');
+
+module.exports = {
+    convertTimexToString: en.convertTimexToString,
+    convertTimexSetToString: en.convertTimexSetToString
+};
