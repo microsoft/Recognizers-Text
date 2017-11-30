ca6a4efa9eb5dee0dcf7a5278dedab166f49adac
johnataylor <johtaylo@microsoft.com>
2017-11-29 19:34:10 -0800

    add TIMEX datatype support (#224)
    * add TIMEX datatype support

---
+++ JavaScript/packages/datatypes-date-time/src/timexResolver.js
@@ -0,0 +1,189 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+const Timex = require('./timex.js').Timex;
+const timexValue = require('./timexValue.js');
+const timexInference = require('./timexInference.js');
+const timexHelpers = require('./timexHelpers.js');
+
+const dateOfLastDay = require('./timexDateHelpers.js').dateOfLastDay;
+const dateOfNextDay = require('./timexDateHelpers.js').dateOfNextDay;
+
+const resolveDefiniteTime = function (timex, date) {
+    return [{ timex: timex.timex, type: 'datetime', value: `${timexValue.dateValue(timex)} ${timexValue.timeValue(timex)}` }];
+};
+
+const resolveDefinite = function (timex, date) {
+    return [{ timex: timex.timex, type: 'date', value: timexValue.dateValue(timex) }];
+};
+
+const lastDateValue = function (timex, date) {
+    if (timex.month !== undefined && timex.dayOfMonth !== undefined) {
+        return timexValue.dateValue({ year: date.getFullYear() - 1, month: timex.month, dayOfMonth: timex.dayOfMonth });
+    }
+    if (timex.dayOfWeek !== undefined) {
+        const day = timex.dayOfWeek === 7 ? 0 : timex.dayOfWeek;
+        const result = dateOfLastDay(day, date);
+        return timexValue.dateValue({ year: result.getFullYear(), month: result.getMonth() + 1, dayOfMonth: result.getDate() });
+    }
+};
+
+const nextDateValue = function (timex, date) {
+    if (timex.month !== undefined && timex.dayOfMonth !== undefined) {
+        return timexValue.dateValue({ year: date.getFullYear(), month: timex.month, dayOfMonth: timex.dayOfMonth });
+    }
+    if (timex.dayOfWeek !== undefined) {
+        const day = timex.dayOfWeek === 7 ? 0 : timex.dayOfWeek;
+        const result = dateOfNextDay(day, date);
+        return timexValue.dateValue({ year: result.getFullYear(), month: result.getMonth() + 1, dayOfMonth: result.getDate() });
+    }
+};
+
+const resolveDate = function (timex, date) {
+    return [
+        { timex: timex.timex, type: 'date', value: lastDateValue(timex, date) },
+        { timex: timex.timex, type: 'date', value: nextDateValue(timex, date) }
+    ];
+};
+
+const resolveTime = function (timex) {
+    return [ { timex: timex.timex, type: 'time', value: timexValue.timeValue(timex) } ];
+};
+
+const resolveDuration = function (timex) {
+    return [ { timex: timex.timex, type: 'duration', value: timexValue.durationValue(timex) }];
+};
+
+const monthDateRange = function (year, month) {
+    return {
+        start: timexValue.dateValue({ year: year, month: month, dayOfMonth: 1 }),
+        end: timexValue.dateValue({ year: year, month: month + 1, dayOfMonth: 1 })
+    };
+};
+
+const resolveDateRange = function (timex, date) {
+    if ('season' in timex) {
+        return [{ timex: timex.timex, type: 'daterange', value: 'not resolved' }];
+    }
+    else {
+        if (timex.year !== undefined && timex.month !== undefined) {
+            const dateRange = monthDateRange(timex.year, timex.month);
+            return [{ timex: timex.timex, type: 'daterange', start: dateRange.start, end: dateRange.end }];
+        }
+        if (timex.month !== undefined) {
+            const y = date.getFullYear();
+            const lastYearDateRange = monthDateRange(y - 1, timex.month);
+            const thisYearDateRange = monthDateRange(y, timex.month);
+            
+            return [
+                { timex: timex.timex, type: 'daterange', start: lastYearDateRange.start, end: lastYearDateRange.end },
+                { timex: timex.timex, type: 'daterange', start: thisYearDateRange.start, end: thisYearDateRange.end }
+            ];
+        }
+        return [];
+    }
+};
+
+const partOfDayTimeRange = function (timex) {
+    switch (timex.partOfDay) {
+        case 'MO': return { start: '08:00:00', end: '12:00:00' };
+        case 'AF': return { start: '12:00:00', end: '16:00:00' };
+        case 'EV': return { start: '16:00:00', end: '20:00:00' };
+    }
+    return { start: 'not resolved', end: 'not resolved' };
+};
+
+const resolveTimeRange = function (timex, date) {
+    if ('partOfDay' in timex) {
+        const range = partOfDayTimeRange(timex);
+        return [{ timex: timex.timex, type: 'timerange', start: range.start, end: range.end }];
+    }
+    else {
+        const range = timexHelpers.expandTimeRange(timex);
+        return [{
+            timex: timex.timex,
+            type: 'timerange',
+            start: timexValue.timeValue(range.start),
+            end: timexValue.timeValue(range.end)
+        }];
+    }
+};
+
+const resolveDateTime = function (timex, date) {
+    const resolvedDates = resolveDate(timex, date);
+    for (const resolved of resolvedDates) {
+        resolved.type = 'datetime';
+        resolved.value = `${resolved.value} ${timexValue.timeValue(timex)}`; 
+
+    }
+    return resolvedDates;
+};
+
+const resolveDateTimeRange = function (timex) {
+    if ('partOfDay' in timex) {
+        const date = timexValue.dateValue(timex);
+        const timeRange = partOfDayTimeRange(timex);
+        return [{
+            timex: timex.timex,
+            type: 'datetimerange',
+            start: `${date} ${timeRange.start}`,
+            end: `${date} ${timeRange.end}`
+        }];
+    }
+    else {
+        const range = timexHelpers.expandDateTimeRange(timex);
+        return [{ 
+            timex: timex.timex,
+            type: 'datetimerange', 
+            start: `${timexValue.dateValue(range.start)} ${timexValue.timeValue(range.start)}`,
+            end: `${timexValue.dateValue(range.end)} ${timexValue.timeValue(range.end)}`
+        }];
+    }
+};
+
+const resolveTimex = function (timex, date) {
+
+    const types = ('types' in timex) ? timex.types : timexInference.infer(timex);
+
+    if (types.has('datetimerange')) {
+        return resolveDateTimeRange(timex);
+    }
+    if (types.has('definite') && types.has('time')) {
+        return resolveDefiniteTime(timex, date);
+    }
+    if (types.has('definite')) {
+        return resolveDefinite(timex, date);
+    }
+    if (types.has('daterange')) {
+        return resolveDateRange(timex, date);
+    }
+    if (types.has('timerange')) {
+        return resolveTimeRange(timex);
+    }
+    if (types.has('datetime')) {
+        return resolveDateTime(timex, date);
+    }
+    if (types.has('duration')) {
+        return resolveDuration(timex);
+    }
+    if (types.has('date')) {
+        return resolveDate(timex, date);
+    }
+    if (types.has('time')) {
+        return resolveTime(timex);
+    }
+    return [];
+};
+
+const resolve = function (timexArray, date) {
+    const resolution = { values: [] };
+    for (const timex of timexArray) {
+        const t = new Timex(timex);
+        const r = resolveTimex(t, date);
+        Array.prototype.push.apply(resolution.values, r);        
+    }
+    return resolution;
+};
+
+module.exports = {
+    resolve: resolve
+};
---
+++ JavaScript/packages/datatypes-date-time/test/timexIntegrated.spec.js
@@ -0,0 +1,38 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+const chai = require('chai');
+
+const { Timex, resolver, creator } = require('../index.js');
+
+const assert = chai.assert;
+const expect = chai.expect;
+chai.should();
+
+describe('No Network', () => {
+    describe('Datatypes', () => {
+        describe('Timex integrate resolution with constants and conversion', () => {
+            describe('with constants', () => {
+                const today = new Date(2017, 9, 5);
+                it('day of week constrained by next week in the evening', () => {
+                    const constraints = [ creator.weekFromToday(today), creator.evening ];
+                    const resolutions = resolver.evaluate(['XXXX-WXX-3T04', 'XXXX-WXX-3T16'], constraints);
+                    resolutions.map(t => t.timex).should.be.an('array').lengthOf(1).members(['2017-10-11T16']);
+                    const text = resolutions[0].toNaturalLanguage(today);
+                    text.should.equal('next Wednesday 4PM');
+                });
+            });
+        });
+        describe('Timex integrate resolution with incremental updates', () => {
+            describe('date + year (daterange) + time (time)', () => {
+                const today = new Date(2017, 9, 5);
+                it('day of week constrained by next year and adding a specific time', () => {
+                    const constraints = [ new Timex('T17:00:00'), new Timex('2018') ];
+                    const resolutions = resolver.evaluate(['XXXX-03-15'], constraints);
+                    resolutions.map(t => t.timex).should.be.an('array').lengthOf(1).members(['2018-03-15T17']);
+                    const text = resolutions[0].toNaturalLanguage(today);
+                    text.should.equal('15th March 2018 5PM');
+                });
+            });
+        });
+    });
+});
