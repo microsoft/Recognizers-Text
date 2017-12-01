ef3c954ec5e572dae2519648630fd3d5ee09a80c
Enzo Cano <enzo.cano@southworks.com>
2017-12-01 16:11:57 -0300

    Simplify boolean regex definitions

--- JavaScript/packages/recognizers-options/src/resources/englishOptions.ts
+++ JavaScript/packages/recognizers-options/src/resources/englishOptions.ts
@@ -9,6 +9,6 @@
 export namespace EnglishOptions {
 	export const LangMarker = 'Eng';
 	export const TokenizerRegex = `[^\\w\\d]`;
-	export const TrueRegex = `\\b(y(e[sp])?|sure|ok|\\uD83D\\uDC4D|\\uD83D\\uDC4C)\\b`;
-	export const FalseRegex = `\\b(n(o(pe)?)?|\\uD83D\\uDC4E|\\u270B|\\uD83D\\uDD90)\\b`;
+	export const TrueRegex = `\\b(true|yes|yep|y|sure|ok|\\uD83D\\uDC4D|\\uD83D\\uDC4C)\\b`;
+	export const FalseRegex = `\\b(false|nope|nop|no|\\uD83D\\uDC4E|\\u270B|\\uD83D\\uDD90)\\b`;
 }
