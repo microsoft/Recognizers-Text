d3859d0517c5a3f742a5e86152188afed6c5d2fe
Enzo Cano <enzo.cano@southworks.com>
2017-12-18 18:01:16 -0300

    Update resources with latest changes

--- JavaScript/packages/recognizers-number/src/resources/portugueseNumeric.ts
+++ JavaScript/packages/recognizers-number/src/resources/portugueseNumeric.ts
@@ -43,7 +43,7 @@ export namespace PortugueseNumeric {
 	export const SuffixRoundOrdinalRegex = `((${AllIntRegex})(${SimpleRoundOrdinalRegex}))`;
 	export const ComplexRoundOrdinalRegex = `(((${SuffixRoundOrdinalRegex}(\\s)?)?${ComplexOrdinalRegex})|${SuffixRoundOrdinalRegex})`;
 	export const AllOrdinalRegex = `${ComplexOrdinalRegex}|${SimpleRoundOrdinalRegex}|${ComplexRoundOrdinalRegex}`;
-	export const OrdinalSuffixRegex = `(?<=\\b)(\\d*(1[oa]|2[oa]|3[oa]|4[oa]|5[oa]|6[oa]|7[oa]|8[oa]|9[oa]|0[oa]))(?=\\b)`;
+	export const OrdinalSuffixRegex = `(?<=\\b)(\\d*(1[oaº]|2[oaº]|3[oaº]|4[oaº]|5[oaº]|6[oaº]|7[oaº]|8[oaº]|9[oaº]|0[oaº]|1.º|2.º|3.º|4.º|5.º|6.º|7.º|8.º|9.º))(?=\\b)`;
 	export const OrdinalEnglishRegex = `(?<=\\b)${AllOrdinalRegex}(?=\\b)`;
 	export const FractionNotationRegex = `(((?<=\\W|^)-\\s*)|(?<=\\b))\\d+[/]\\d+(?=(\\b[^/]|$))`;
 	export const FractionNotationWithSpacesRegex = `(((?<=\\W|^)-\\s*)|(?<=\\b))\\d+\\s+\\d+[/]\\d+(?=(\\b[^/]|$))`;
