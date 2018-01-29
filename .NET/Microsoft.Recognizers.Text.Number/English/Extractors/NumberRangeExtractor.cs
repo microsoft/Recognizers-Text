6d8a9deb60bcfbc2acae2ab1cceb2fe459f2915f
Luxin Zhang <v-luxzha@microsoft.com>
2018-01-30 02:28:09 +0800

    Support Number Range in English

---
+++ .NET/Microsoft.Recognizers.Text.Number/English/Extractors/NumberRangeExtractor.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Recognizers.Definitions.English;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+
+namespace Microsoft.Recognizers.Text.Number.English
+{
+    public class NumberRangeExtractor : BaseNumberRangeExtractor
+    {
+        internal sealed override ImmutableDictionary<Regex, string> Regexes { get; }
+
+        protected sealed override string ExtractType { get; } = Constants.SYS_NUMRANGE;
+
+        public NumberRangeExtractor() : base(NumberExtractor.GetInstance(), OrdinalExtractor.GetInstance())
+        {
+            var regexes = new Dictionary<Regex, string>()
+            {
+                {
+                    // between...and...
+                    new Regex(NumbersDefinitions.TwoNumberRangeRegex1, RegexOptions.IgnoreCase | RegexOptions.Singleline)
+                    , Constants.TWONUMBETWEEN
+                },
+                {
+                    // more than ... less than ...
+                    new Regex(NumbersDefinitions.TwoNumberRangeRegex2, RegexOptions.IgnoreCase | RegexOptions.Singleline)
+                    , Constants.TWONUM
+                },
+                {
+                    // less than ... more than ...
+                    new Regex(NumbersDefinitions.TwoNumberRangeRegex3, RegexOptions.IgnoreCase | RegexOptions.Singleline)
+                    , Constants.TWONUM
+                },
+                {
+                    // from ... to/~/- ...
+                    new Regex(NumbersDefinitions.TwoNumberRangeRegex4, RegexOptions.IgnoreCase | RegexOptions.Singleline)
+                    , Constants.TWONUMTILL
+                },
+                {
+                    // more/greater/higher than ...
+                    new Regex(NumbersDefinitions.OneNumberRangeMoreRegex1, RegexOptions.IgnoreCase | RegexOptions.Singleline)
+                    , Constants.MORE
+                },
+                {
+                    // 30 and greater/higher
+                    new Regex(NumbersDefinitions.OneNumberRangeMoreRegex2, RegexOptions.IgnoreCase | RegexOptions.Singleline)
+                    , Constants.MORE
+                },
+                {
+                    // less/smaller/lower than ...
+                    new Regex(NumbersDefinitions.OneNumberRangeLessRegex1, RegexOptions.IgnoreCase | RegexOptions.Singleline)
+                    , Constants.LESS
+                },
+                {
+                    // 30 and less/smaller/lower
+                    new Regex(NumbersDefinitions.OneNumberRangeLessRegex2, RegexOptions.IgnoreCase | RegexOptions.Singleline)
+                    , Constants.LESS
+                },
+                {
+                    // equal to ...
+                    new Regex(NumbersDefinitions.OneNumberRangeEqualRegex, RegexOptions.IgnoreCase | RegexOptions.Singleline)
+                    , Constants.EQUAL
+                }
+            };
+
+            Regexes = regexes.ToImmutableDictionary();
+        }
+    }
+}
---
+++ .NET/Microsoft.Recognizers.Text.Number/English/Parsers/EnglishNumberRangeParserConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Recognizers.Definitions.English;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Microsoft.Recognizers.Text.Number.English
+{
+    public class EnglishNumberRangeParserConfiguration : INumberRangeParserConfiguration
+    {
+        public CultureInfo CultureInfo { get; private set; }
+
+        public IExtractor NumberExtractor { get; private set; }
+
+        public IExtractor OrdinalExtractor { get; private set; }
+
+        public IParser NumberParser { get; private set; }
+
+        public Regex MoreOrEqual { get; private set; }
+
+        public Regex LessOrEqual { get; private set; }
+
+        public EnglishNumberRangeParserConfiguration() : this(new CultureInfo(Culture.English))
+        {
+        }
+
+        public EnglishNumberRangeParserConfiguration(CultureInfo ci)
+        {
+            CultureInfo = ci;
+
+            NumberExtractor = English.NumberExtractor.GetInstance();
+            OrdinalExtractor = English.OrdinalExtractor.GetInstance();
+            NumberParser = new BaseNumberParser(new EnglishNumberParserConfiguration());
+            MoreOrEqual = new Regex(NumbersDefinitions.MoreOrEqual, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            LessOrEqual = new Regex(NumbersDefinitions.LessOrEqual, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+        }
+    }
+}
