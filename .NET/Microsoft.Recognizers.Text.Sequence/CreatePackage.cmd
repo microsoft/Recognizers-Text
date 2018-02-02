e7fd3d3e96287b673fa69cb2fd0e7960986eb47c
Börje F. Karlsson <borjekar@microsoft.com>
2018-02-02 15:25:08 +0800

    Fixing package def.

--- .NET/Microsoft.Recognizers.Text.Sequence/CreatePackage.cmd
+++ .NET/Microsoft.Recognizers.Text.Sequence/CreatePackage.cmd
@@ -1,5 +1,5 @@
 @echo off
-echo *** Building Microsoft.Recognizers.Text.Number
+echo *** Building Microsoft.Recognizers.Text.Sequence
 setlocal
 setlocal enabledelayedexpansion
 setlocal enableextensions
@@ -9,14 +9,14 @@ for /f "usebackq tokens=*" %%i in (`..\packages\vswhere.2.2.7\tools\vswhere -lat
 )
 
 if not exist ..\nuget mkdir ..\nuget
-if exist ..\nuget\Microsoft.Recognizers.Text.Number*.nupkg erase /s ..\nuget\Microsoft.Recognizers.Text.Number*.nupkg
-"%MSBuildDir%\MSBuild\15.0\Bin\MSBuild.exe" /property:Configuration=release Microsoft.Recognizers.Text.Number.csproj
+if exist ..\nuget\Microsoft.Recognizers.Text.Sequence*.nupkg erase /s ..\nuget\Microsoft.Recognizers.Text.Sequence*.nupkg
+"%MSBuildDir%\MSBuild\15.0\Bin\MSBuild.exe" /property:Configuration=release Microsoft.Recognizers.Text.Sequence.csproj
 for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\net462\Microsoft.Recognizers.Text.dll).FileVersionInfo.FileVersion"') do set basic=%%v
-for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\net462\Microsoft.Recognizers.Text.Number.dll).FileVersionInfo.FileVersion"') do set numberVersion=%%v
-..\packages\NuGet.CommandLine.4.3.0\tools\NuGet.exe pack Microsoft.Recognizers.Text.Number.nuspec -symbols -properties version=%numberVersion%;basic=%basic% -OutputDirectory ..\nuget
+for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\net462\Microsoft.Recognizers.Text.Sequence.dll).FileVersionInfo.FileVersion"') do set numberVersion=%%v
+..\packages\NuGet.CommandLine.4.3.0\tools\NuGet.exe pack Microsoft.Recognizers.Text.Sequence.nuspec -symbols -properties version=%numberVersion%;basic=%basic% -OutputDirectory ..\nuget
 
 set error=%errorlevel%
-set packageName=Microsoft.Recognizers.Text.Number
+set packageName=Microsoft.Recognizers.Text.Sequence
 if %error% NEQ 0 (
 	echo *** Failed to build %packageName%
 	exit /b %error%
