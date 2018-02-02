b78f9767a6ecf0fcc61033ab217242f984abad1a
Börje F. Karlsson <borjekar@microsoft.com>
2018-02-02 18:34:44 +0800

    Adding missing AssemblyInfo.

--- .NET/Microsoft.Recognizers.Text.Sequence/Properties/AssemblyInfo.cs
+++ .NET/Microsoft.Recognizers.Text.Sequence/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.Recognizers.Text")]
+[assembly: AssemblyTitle("Microsoft.Recognizers.Text.Sequence")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Microsoft.Recognizers.Text")]
+[assembly: AssemblyProduct("Microsoft.Recognizers.Text.Sequence")]
 [assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("59755149-E656-4EF7-87A5-76EEE3FD5146")]
+[assembly: Guid("59755149-E656-4EF7-87A5-76EEE3FD5144")]
 
 // Version information for an assembly consists of the following four values:
 //
