b5e0900854c86218f4aca78e9becef34792390ea
Börje F. Karlsson <borjekar@microsoft.com>
2018-02-02 15:16:47 +0800

    Basic structure for new Sequence recognizer.

---
+++ .NET/Microsoft.Recognizers.Text.Sequence/CreatePackage.cmd
@@ -0,0 +1,25 @@
+@echo off
+echo *** Building Microsoft.Recognizers.Text.Number
+setlocal
+setlocal enabledelayedexpansion
+setlocal enableextensions
+
+for /f "usebackq tokens=*" %%i in (`..\packages\vswhere.2.2.7\tools\vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+  set MSBuildDir=%%i\MSBuild\15.0\Bin\
+)
+
+if not exist ..\nuget mkdir ..\nuget
+if exist ..\nuget\Microsoft.Recognizers.Text.Number*.nupkg erase /s ..\nuget\Microsoft.Recognizers.Text.Number*.nupkg
+"%MSBuildDir%\MSBuild\15.0\Bin\MSBuild.exe" /property:Configuration=release Microsoft.Recognizers.Text.Number.csproj
+for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\net462\Microsoft.Recognizers.Text.dll).FileVersionInfo.FileVersion"') do set basic=%%v
+for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\net462\Microsoft.Recognizers.Text.Number.dll).FileVersionInfo.FileVersion"') do set numberVersion=%%v
+..\packages\NuGet.CommandLine.4.3.0\tools\NuGet.exe pack Microsoft.Recognizers.Text.Number.nuspec -symbols -properties version=%numberVersion%;basic=%basic% -OutputDirectory ..\nuget
+
+set error=%errorlevel%
+set packageName=Microsoft.Recognizers.Text.Number
+if %error% NEQ 0 (
+	echo *** Failed to build %packageName%
+	exit /b %error%
+) else (
+	echo *** Succeeded to build %packageName%
+)
---
+++ .NET/Microsoft.Recognizers.Text.Sequence/Models/AbstractSequenceModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Recognizers.Text.Sequence
+{
+    public abstract class AbstractSequenceModel : IModel
+    {
+        protected AbstractSequenceModel(IParser parser, IExtractor extractor)
+        {
+            this.Parser = parser;
+            this.Extractor = extractor;
+        }
+
+        public abstract string ModelTypeName { get; }
+
+        protected IExtractor Extractor { get; private set; }
+
+        protected IParser Parser { get; private set; }
+
+        public List<ModelResult> Parse(string query)
+        {
+            throw new NotImplementedException();
+
+        }
+    }
+}
