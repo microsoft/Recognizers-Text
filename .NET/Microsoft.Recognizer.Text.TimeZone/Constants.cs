f25c8b2e225a9150c2680bbf6809893cb017df9c
Jingyue Gao (MSR Student-FA Talent) <v-jingyg@microsoft.com>
2018-02-02 17:09:57 +0800

    delete useless project

--- .NET/Microsoft.Recognizer.Text.TimeZone/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Microsoft.Recognizers.Text.TimeZone
-{
-    class Constants
-    {
-        public const string SYS_TIMEZONE_UTC = "builtin.timezone.utc";
-        public const string SYS_TIMEZONE = "builtin.timezone";
-    }
-}
