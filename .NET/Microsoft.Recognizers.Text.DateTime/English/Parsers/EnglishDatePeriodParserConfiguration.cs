07772b43a6f5cd66d4999d5aab1a26e168452f13
Luxin Zhang <zhangluxin@pku.edu.cn>
2018-02-02 12:31:16 +0800

    Improve datetime extraction (#304)
    * Extend support for datetime extraction
    * Add more test cases
    * Revise code. Move tests to the correct spec

--- .NET/Microsoft.Recognizers.Text.DateTime/English/Parsers/EnglishDatePeriodParserConfiguration.cs
+++ .NET/Microsoft.Recognizers.Text.DateTime/English/Parsers/EnglishDatePeriodParserConfiguration.cs
@@ -72,6 +72,10 @@ namespace Microsoft.Recognizers.Text.DateTime.English
             new Regex(
                 DateTimeDefinitions.ThisPrefixRegex,
                 RegexOptions.IgnoreCase | RegexOptions.Singleline);
+        public static readonly Regex AfterNextSuffixRegex =
+            new Regex(
+                DateTimeDefinitions.AfterNextSuffixRegex,
+                RegexOptions.IgnoreCase | RegexOptions.Singleline);
 
         Regex IDatePeriodParserConfiguration.NextPrefixRegex => NextPrefixRegex;
         Regex IDatePeriodParserConfiguration.PastPrefixRegex => PastPrefixRegex;
@@ -150,7 +154,11 @@ namespace Microsoft.Recognizers.Text.DateTime.English
         {
             var trimedText = text.Trim().ToLowerInvariant();
             var swift = 0;
-            if (NextPrefixRegex.IsMatch(trimedText))
+            if (AfterNextSuffixRegex.IsMatch(trimedText))
+            {
+                swift = 2;
+            }
+            else if (NextPrefixRegex.IsMatch(trimedText))
             {
                 swift = 1;
             }
@@ -165,7 +173,11 @@ namespace Microsoft.Recognizers.Text.DateTime.English
         {
             var trimedText = text.Trim().ToLowerInvariant();
             var swift = -10;
-            if (NextPrefixRegex.IsMatch(trimedText))
+            if (AfterNextSuffixRegex.IsMatch(trimedText))
+            {
+                swift = 2;
+            }
+            else if (NextPrefixRegex.IsMatch(trimedText))
             {
                 swift = 1;
             }
@@ -195,7 +207,7 @@ namespace Microsoft.Recognizers.Text.DateTime.English
         public bool IsMonthOnly(string text)
         {
             var trimedText = text.Trim().ToLowerInvariant();
-            return trimedText.EndsWith("month");
+            return trimedText.EndsWith("month") || trimedText.Contains(" month ") && AfterNextSuffixRegex.IsMatch(trimedText);
         }
 
         public bool IsMonthToDate(string text)
@@ -207,19 +219,19 @@ namespace Microsoft.Recognizers.Text.DateTime.English
         public bool IsWeekend(string text)
         {
             var trimedText = text.Trim().ToLowerInvariant();
-            return trimedText.EndsWith("weekend");
+            return trimedText.EndsWith("weekend") || trimedText.Contains(" weekend ") && AfterNextSuffixRegex.IsMatch(trimedText);
         }
 
         public bool IsWeekOnly(string text)
         {
             var trimedText = text.Trim().ToLowerInvariant();
-            return trimedText.EndsWith("week");
+            return trimedText.EndsWith("week") || trimedText.Contains(" week ") && AfterNextSuffixRegex.IsMatch(trimedText);
         }
 
         public bool IsYearOnly(string text)
         {
             var trimedText = text.Trim().ToLowerInvariant();
-            return trimedText.EndsWith("year");
+            return trimedText.EndsWith("year") || trimedText.Contains(" year ") && AfterNextSuffixRegex.IsMatch(trimedText);
         }
 
         public bool IsYearToDate(string text)
