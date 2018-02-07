4ec64edc8ac3d659270cea128dd50513693908c2
Sanxing Chen <v-sanc@microsoft.com>
2018-02-07 14:16:43 +0800

    Replace phone number extractor's logic.

--- .NET/Microsoft.Recognizers.Text.Sequence/English/Extractors/PhoneNumberExtractor.cs
+++ .NET/Microsoft.Recognizers.Text.Sequence/English/Extractors/PhoneNumberExtractor.cs
@@ -1,37 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Text.RegularExpressions;
-
-using Microsoft.Recognizers.Definitions;
-
-namespace Microsoft.Recognizers.Text.Sequence.English
+﻿namespace Microsoft.Recognizers.Text.Sequence.English
 {
-    class PhoneNumberExtractor : BaseSequenceExtractor
+    class PhoneNumberExtractor : BasePhoneNumberExtractor
     {
-        internal override ImmutableDictionary<Regex, string> Regexes { get; }
-
-        protected sealed override string ExtractType { get; } = Constants.MODEL_PHONE_NUMBER;
-
-        public PhoneNumberExtractor()
-        {
-            var regexes = new Dictionary<Regex, string>
-            {
-                {
-                    new Regex(BasePhoneNumbers.BrazilPhoneNumberRegex), Constants.PHONE_NUMBER_REGEX_BRAZIL
-                },
-                {
-                    new Regex(BasePhoneNumbers.GeneralPhoneNumberRegex), Constants.PHONE_NUMBER_REGEX_GENERAL
-                },
-                {
-                    new Regex(BasePhoneNumbers.UkPhoneNumberRegex), Constants.PHONE_NUMBER_REGEX_UK
-                },
-                {
-                    new Regex(BasePhoneNumbers.GermanyPhoneNumberRegex), Constants.PHONE_NUMBER_REGEX_GERMANY
-                }
-
-            };
-
-            Regexes = regexes.ToImmutableDictionary();
-        }
+       
     }
 }
