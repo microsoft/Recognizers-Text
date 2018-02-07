d8bdd8768ccf920c5c46d2f4a146f4e03fbd5043
Luxin Zhang <v-luxzha@microsoft.com>
2018-02-07 15:22:26 +0800

    Fix decade and number range extraction.

--- .NET/Microsoft.Recognizers.Definitions/English/DateTimeDefinitions.cs
+++ .NET/Microsoft.Recognizers.Definitions/English/DateTimeDefinitions.cs
@@ -206,7 +206,7 @@ namespace Microsoft.Recognizers.Definitions.English
 		public static readonly string TimeBeforeAfterRegex = $@"\b(((?<=\b(before|no later than|by|after)\s+)({EngTimeRegex}|{HourNumRegex}|{BaseDateTime.HourRegex}|{MidTimeRegex}))|{MidTimeRegex})\b";
 		public const string DateNumberConnectorRegex = @"^\s*(?<connector>\s+at)\s*$";
 		public const string DecadeRegex = @"(?<decade>noughties|twenties|thirties|forties|fifties|sixties|seventies|eighties|nineties|two thousands)";
-		public static readonly string DecadeWithCenturyRegex = $@"the\s+(((?<century>\d|1\d|2\d)?(')?(?<decade>\d0)(')?s)|(({CenturyRegex}(\s+|-)(and\s+)?)?{DecadeRegex})|({CenturyRegex}(\s+|-)(and\s+)?(?<decade>tens|hundreds)))";
+		public static readonly string DecadeWithCenturyRegex = $@"(the\s+)?(((?<century>\d|1\d|2\d)?(')?(?<decade>\d0)(')?s)|(({CenturyRegex}(\s+|-)(and\s+)?)?{DecadeRegex})|({CenturyRegex}(\s+|-)(and\s+)?(?<decade>tens|hundreds)))";
 		public const string YearAfterRegex = @"\b(or\s+(above|after))\b";
 		public static readonly string YearPeriodRegex = $@"(((from|during|in|between)\s+)?{YearRegex}\s*{TillRegex}\s*{YearRegex})";
 		public static readonly Dictionary<string, string> UnitMap = new Dictionary<string, string>
