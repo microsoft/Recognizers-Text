636d093af13adbb0dadb83b53d65e794c2c40e03
JuanAr <juan.arguello@southworks.com>
2018-02-28 17:59:13 -0300

    Added choiceRecognizer static method test

--- JavaScript/test/index.js
+++ JavaScript/test/index.js
@@ -4,9 +4,11 @@ var specs = require('./specs');
 var numberRecognizerEnglish = require('./numberRecognizer-english');
 var dateTimeRecognizerEnglish = require('./dateTimeRecognizer-english');
 var dateTimeRecognizerInitialization = require('./dateTimeRecognizer-initialization');
+var choiceRecognizerEnglish = require('./choiceRecognizer-english');
 
 // run
 SpecRunner(describe, specs.readAll());
 numberRecognizerEnglish(describe);
 dateTimeRecognizerEnglish(describe);
 dateTimeRecognizerInitialization(describe);
+choiceRecognizerEnglish(describe);
