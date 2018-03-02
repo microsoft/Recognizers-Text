86a78c75a767baed77476ce4f8041700a3bb5a82
Enzo Cano <enzo.cano@southworks.com>
2018-03-02 20:17:05 -0300

    Add JavaScript cache tests

--- JavaScript/test/initialization-tests/index.js
+++ JavaScript/test/initialization-tests/index.js
@@ -2,6 +2,7 @@ var numberRecognizerInitialization = require('./numberRecognizer');
 var numberWithUnitRecognizerInitialization = require('./numberWithUnitRecognizer');
 var dateTimeRecognizerInitialization = require('./dateTimeRecognizer');
 var choiceRecognizerInitialization = require('./choiceRecognizer');
+var sequenceRecognizerInitialization = require('./sequenceRecognizer');
 
 //run
 module.exports = function (describe) {
@@ -9,4 +10,5 @@ module.exports = function (describe) {
     numberWithUnitRecognizerInitialization(describe);
     dateTimeRecognizerInitialization(describe);
     choiceRecognizerInitialization(describe);
+    sequenceRecognizerInitialization(describe);
 }
