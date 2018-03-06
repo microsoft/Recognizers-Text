27bdce0408e8b3460dd0e99550fbadbb67b0ea3b
JuanAr <juan.arguello@southworks.com>
2018-03-06 20:04:29 -0300

    Manually convert TS resources to PY

---
+++ Python/libraries/recognizers-number/recognizers_number/resources/__init__.py
@@ -0,0 +1,6 @@
+from .base_numbers import BaseNumbers
+from .chinese_numeric import ChineseNumeric
+from .english_numeric import EnglishNumeric
+from .french_numeric import FrenchNumeric
+from .portuguese_numeric import PortugueseNumeric
+from .spanish_numeric import SpanishNumeric
