6c56fd03eec4e1498f37d867a120fe1be90c4fb8
John Taylor <johtaylo@microsoft.com>
2018-03-05 17:51:19 -0800

    TIMEX client library in .NET (a direct port of the JavaScript)

---
+++ .NET/Microsoft.Recognizers.DataTypes.DateTime/TimexRelativeConvert.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Recognizers.DataTypes.DateTime
+{
+    public static class TimexRelativeConvert
+    {
+        public static string ConvertTimexToStringRelative(Timex timex, System.DateTime referenceDate)
+        {
+            return TimexRelativeConvertEn.ConvertTimexToStringRelative(timex, referenceDate);
+        }
+    }
+}
