be003adccd0506ffa8a8632d3da7976a34032804
Sanxing Chen <mestayinloves@gmail.com>
2018-03-06 11:31:36 +0800

    Support year's quarters in format Q[1-4] (#376)
    * Support year's quarters in format Q[1-4] (#363)
    * Comments fixed
    * Add tests and support for patterns like "Q3 2016"

--- .NET/Microsoft.Recognizers.Definitions/English/DateTimeDefinitions.cs
+++ .NET/Microsoft.Recognizers.Definitions/English/DateTimeDefinitions.cs
@@ -52,8 +52,10 @@ namespace Microsoft.Recognizers.Definitions.English
 		public static readonly string WeekOfYearRegex = $@"(?<woy>(the\s+)?(?<cardinal>first|1st|second|2nd|third|3rd|fourth|4th|fifth|5th|last)\s+week(\s+of)?\s+({YearRegex}|{RelativeRegex}\s+year))";
 		public static readonly string FollowedDateUnit = $@"^\s*{DateUnitRegex}";
 		public static readonly string NumberCombinedWithDateUnit = $@"\b(?<num>\d+(\.\d*)?){DateUnitRegex}";
-		public static readonly string QuarterRegex = $@"(the\s+)?(?<cardinal>first|1st|second|2nd|third|3rd|fourth|4th)\s+quarter(\s+of|\s*,\s*)?\s+({YearRegex}|{RelativeRegex}\s+year)";
-		public static readonly string QuarterRegexYearFront = $@"({YearRegex}|{RelativeRegex}\s+year)\s+(the\s+)?(?<cardinal>first|1st|second|2nd|third|3rd|fourth|4th)\s+quarter";
+		public const string QuarterTermRegex = @"(((?<cardinal>first|1st|second|2nd|third|3rd|fourth|4th)\s+quarter)|(Q(?<number>[1-4])))";
+		public static readonly string QuarterRegex = $@"(the\s+)?{QuarterTermRegex}((\s+of|\s*,\s*)?\s+({YearRegex}|{RelativeRegex}\s+year))?";
+		public static readonly string QuarterRegexYearFront = $@"({YearRegex}|{RelativeRegex}\s+year)\s+(the\s+)?{QuarterTermRegex}";
+		public static readonly string QuarterRegexYearBack = $@"(the\s+)?{QuarterTermRegex}\s+({YearRegex}|{RelativeRegex}\s+year)";
 		public const string EarlyPrefixRegex = @"?<EarlyPrefix>early|beginning of|start of";
 		public const string MidPrefixRegex = @"?<MidPrefix>mid|middle of";
 		public const string LaterPrefixRegex = @"?<LatePrefix>late|later|end of";
