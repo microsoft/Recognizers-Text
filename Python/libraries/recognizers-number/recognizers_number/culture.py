69845c5fbb40529c0dbfca3301533fe591d0dd31
Amit Stein <amit.stein@southworks.com>
2018-03-13 17:34:48 -0300

    include culture into format number

--- Python/libraries/recognizers-number/recognizers_number/culture.py
+++ Python/libraries/recognizers-number/recognizers_number/culture.py
@@ -1,5 +1,14 @@
-from recognizers_text.culture import BaseCultureInfo
+from recognizers_text.culture import BaseCultureInfo, Culture
+from .number import LongFormatMode
 from decimal import *
+
+
+supported_cultures = {
+    Culture.English: LongFormatMode.DOUBLE_COMMA_DOT,
+    Culture.Chinese: None,
+    Culture.Spanish: LongFormatMode.DOUBLE_DOT_COMMA,
+    Culture.French: LongFormatMode.DOUBLE_DOT_COMMA,
+}
 
 class CultureInfo(BaseCultureInfo):
     def format(self, value: object) -> str:
@@ -14,4 +23,7 @@ class CultureInfo(BaseCultureInfo):
             p = result.split('E+')
             p[0] = p[0].rstrip('0')
             result = 'E+'.join(p)
+        long_format = supported_cultures.get(self.code)
+        if long_format:
+            result = long_format.thousands_mark.join(list(map(lambda x: x.replace(".", long_format.decimals_mark),  result.split(","))))
         return result
