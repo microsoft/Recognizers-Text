5dadd3e170abbb07a3b22d7282f03f85f7055257
JuanAr <juan.arguello@southworks.com>
2018-03-15 15:01:52 -0300

    Fix Publication+Installation issues after testing PyPI upload

--- Python/libraries/recognizers-number/setup.py
+++ Python/libraries/recognizers-number/setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 NAME = "recognizers-text-number"
-VERSION = "1.0.0"
+VERSION = "1.0.0.a0"
 REQUIRES = ['recognizers-text', 'regex']
 
 setup(
--- Python/libraries/recognizers-text/setup.py
+++ Python/libraries/recognizers-text/setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 NAME = "recognizers_text"
-VERSION = "1.0.0"
+VERSION = "1.0.0.a0"
 REQUIRES = []
 
 setup(
