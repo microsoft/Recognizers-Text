9f3ddab179eb08d327d9c686177e177e4e4ea7f6
Amit Stein <amit.stein@southworks.com>
2018-03-20 12:07:20 -0300

    cleanup code from test

--- Python/tests/test_runner_number.py
+++ Python/tests/test_runner_number.py
@@ -1,21 +1,21 @@
 import pytest
-from runner import *
+from runner import get_specs
 from recognizers_number.number import NumberRecognizer
 
-modelFunction = {
+MODELFUNCTION = {
     'NumberModel': NumberRecognizer.recognize_number,
     'OrdinalModel': NumberRecognizer.recognize_ordinal,
     'PercentModel': NumberRecognizer.recognize_percentage,
 }
 
 def get_results(culture, model, source):
-    return modelFunction[model](source, culture)
+    return MODELFUNCTION[model](source, culture)
 
-@pytest.mark.parametrize("culture, model, source, expected_results", get_specs("Number"))
+@pytest.mark.parametrize('culture, model, source, expected_results', get_specs('Number'))
 def test_number_recognizer(culture, model, source, expected_results):
     results = get_results(culture, model, source)
     assert len(results) == len(expected_results)
     for expected, actual in zip(expected_results, results):
-        assert expected["TypeName"] == actual.type_name
-        assert expected["Text"] == actual.text
-        assert expected["Resolution"]["value"] == actual.resolution["value"]
+        assert expected['TypeName'] == actual.type_name
+        assert expected['Text'] == actual.text
+        assert expected['Resolution']['value'] == actual.resolution['value']
--- Python/tests/test_runner_number_with_unit.py
+++ Python/tests/test_runner_number_with_unit.py
@@ -1,8 +1,8 @@
 import pytest
-from runner import *
+from runner import get_specs
 from recognizers_number_with_unit.number_with_unit import NumberWithUnitRecognizer
 
-modelFunction = {
+MODELFUNCTION = {
     'AgeModel': NumberWithUnitRecognizer.recognize_age,
     'CurrencyModel': NumberWithUnitRecognizer.recognize_currency,
     'TemperatureModel': NumberWithUnitRecognizer.recognize_temperature,
@@ -10,18 +10,18 @@ modelFunction = {
 }
 
 def get_results(culture, model, source):
-    return modelFunction[model](source, culture)
+    return MODELFUNCTION[model](source, culture)
 
-@pytest.mark.parametrize("culture, model, source, expected_results", get_specs("NumberWithUnit"))
+@pytest.mark.parametrize('culture, model, source, expected_results', get_specs('NumberWithUnit'))
 def test_number_with_unit_recognizer(culture, model, source, expected_results):
     results = get_results(culture, model, source)
     assert len(results) == len(expected_results)
     for expected, actual in zip(expected_results, results):
-        assert expected["Text"] == actual.text
-        assert expected["TypeName"] == actual.type_name
-        if expected["Resolution"] is None:
+        assert expected['Text'] == actual.text
+        assert expected['TypeName'] == actual.type_name
+        if expected['Resolution'] is None:
             assert actual.resolution is None
         else:
-            expected_resolution = expected["Resolution"]
-            assert expected_resolution["value"] == actual.resolution.value
-            assert expected_resolution["unit"] == actual.resolution.unit
+            expected_resolution = expected['Resolution']
+            assert expected_resolution['value'] == actual.resolution.value
+            assert expected_resolution['unit'] == actual.resolution.unit
