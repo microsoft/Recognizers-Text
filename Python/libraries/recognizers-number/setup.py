de69ef25d1cef4c8c0afc534fac1b752ab5b9efd
JuanAr <juan.arguello@southworks.com>
2018-03-20 18:24:46 -0300

    Restore VERSION = "1.0.0.a0" in setup.py

--- Python/libraries/recognizers-number/setup.py
+++ Python/libraries/recognizers-number/setup.py
@@ -8,7 +8,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 NAME = "recognizers-text-number"
-VERSION = "1.0.0.6"
+VERSION = "1.0.0.a0"
 REQUIRES = ['recognizers-text', 'regex']
 
 setup(
