0e7ed3447e46abd188c05c96e9b9da81b3f9a6dd
Enzo Cano <enzo.cano@southworks.com>
2018-03-22 14:43:39 -0300

    Add FormatUtil

--- Python/libraries/recognizers-date-time/recognizers_date_time/date_time/utilities.py
+++ Python/libraries/recognizers-date-time/recognizers_date_time/date_time/utilities.py
@@ -1,7 +1,10 @@
 from abc import ABC, abstractmethod
-from typing import List, Dict, Pattern
+from typing import List, Dict, Pattern, Union
+from datetime import datetime
+import regex
 
 from recognizers_text.extractor import ExtractResult
+from recognizers_text.utilities import RegExpUtility
 
 class Token:
     def __init__(self, start: int, end: int):
@@ -88,3 +91,72 @@ class DateTimeUtilityConfiguration(ABC):
     @abstractmethod
     def am_pm_desc_regex(self) -> Pattern:
         raise NotImplementedError
+
+class FormatUtil:
+    HourTimeRegex = RegExpUtility.get_safe_reg_exp(r'(?<!P)T\d{2}')
+
+    @staticmethod
+    def to_str(num: Union[int, float], size: int) -> str:
+        format_ = f'{{0:0{size}d}}'
+        return str.format(format_, num)
+
+    @staticmethod
+    def luis_date(year: int, month: int, day: int) -> str:
+        if year == -1:
+            if month == -1:
+                return f'XXXX-XX-{day:02d}'
+            return f'XXXX-{month:02d}-{day:02d}'
+        return f'{year:04d}-{month:02d}-{day:02d}'
+
+    @staticmethod
+    def luis_date_from_datetime(date: datetime) -> str:
+        return FormatUtil.luis_date(date.year, date.month, date.day)
+
+    @staticmethod
+    def luis_time(hour: int, min: int, second: int) -> str:
+        return f'{hour:02d}:{min:02d}:{second:02d}'
+
+    @staticmethod
+    def luis_time_from_datetime(time: datetime) -> str:
+        return FormatUtil.luis_time(time.hour, time.minute, time.second)
+
+    @staticmethod
+    def format_date(date: datetime) -> str:
+        return f'{date.year:04d}-{date.month:02d}-{date.day:02d}'
+
+    @staticmethod
+    def format_time(time: datetime) -> str:
+        return f'{time.hour:02d}:{time.minute:02d}-{time.second:02d}'
+
+    @staticmethod
+    def format_date_time(date_time: datetime) -> str:
+        return FormatUtil.format_date(date_time) + FormatUtil.format_time(date_time)
+
+    @staticmethod
+    def all_str_to_pm(source: str) -> str:
+        matches = list(regex.finditer(FormatUtil.HourTimeRegex, source))
+        split: List[str] = list()
+        last_position = 0
+        for match in matches:
+            if last_position == match.start():
+                split.append(source[last_position:match.start()])
+            split.append(source[match.start():match.end()])
+            last_position = match.end()
+        if source[:last_position]:
+            split.append(source[:last_position])
+        for index, value in enumerate(split):
+            if regex.search(FormatUtil.HourTimeRegex, value):
+                split[index] = FormatUtil.to_pm(value)
+        return ''.join(split)
+
+    @staticmethod
+    def to_pm(source: str) -> str:
+        result = ''
+        if source.startwith('T'):
+            result = 'T'
+            source = source[1:]
+        split = source.split(':')
+        hour = int(split[0])
+        hour = 0 if hour == 12 else hour + 12
+        split[0] = f'{hour:02d}'
+        return result + ':'.join(split)
