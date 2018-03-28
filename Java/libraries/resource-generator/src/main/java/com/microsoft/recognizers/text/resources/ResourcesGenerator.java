425bc5c237a7935b83d2d5b33c02a244c74ae462
Pablo Costantini <sixdemons@gmail.com>
2018-03-27 23:57:35 -0300

    Resources YAML Parsing - WIP

---
+++ Java/libraries/resource-generator/src/main/java/com/microsoft/recognizers/text/resources/ResourcesGenerator.java
@@ -0,0 +1,42 @@
+package com.microsoft.recognizers.text.resources;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+
+public class ResourcesGenerator {
+
+    private static final String ResourcesPath = "../../../Patterns";
+
+    public static void main(String[] args) throws IOException {
+        String resourceDefinitionFile = args[0];
+        ResourceDefinitions definition = Parse(resourceDefinitionFile);
+        Path output = FileSystems.getDefault().getPath(definition.outputPath);
+        definition.configFiles.forEach(config -> {
+            Path inputPath = FileSystems.getDefault().getPath(ResourcesPath, String.join(File.pathSeparator, config.input) + ".yaml");
+            Path outputPath = FileSystems.getDefault().getPath(definition.outputPath, config.output + ".java");
+            System.out.println(String.format("%s => %s", inputPath.toString(), outputPath.toString()));
+
+            String header = String.join(System.lineSeparator(), config.header);
+            String footer = String.join(System.lineSeparator(), config.footer);
+
+            try {
+                ResourceCodeGenerator.Generate(inputPath, outputPath, header, footer);
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        });
+    }
+
+    private static ResourceDefinitions Parse(String resourceDefinitionFile) throws IOException {
+        Path path = FileSystems.getDefault().getPath(resourceDefinitionFile);
+        String json = new String(Files.readAllBytes(path));
+
+        return new ObjectMapper().readValue(json, ResourceDefinitions.class);
+    }
+}
