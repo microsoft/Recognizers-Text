199c276e2bdd1367f86370efbe36e5ae08e21215
JuanAr <juan.arguello@southworks.com>
2018-03-28 15:55:46 -0300

    Implement SetExtractorConfiguration

--- Python/libraries/recognizers-date-time/recognizers_date_time/date_time/base_set.py
+++ Python/libraries/recognizers-date-time/recognizers_date_time/date_time/base_set.py
@@ -1,4 +1,5 @@
-from typing import List, Optional
+from abc import ABC, abstractmethod
+from typing import List, Optional, Pattern
 from datetime import datetime
 
 from recognizers_text.extractor import ExtractResult
@@ -6,8 +7,81 @@ from .constants import Constants
 from .extractors import DateTimeExtractor
 from .parsers import DateTimeParser, DateTimeParseResult
 
-class SetExtractorConfiguration:
-    pass
+class SetExtractorConfiguration(ABC):
+    @property
+    @abstractmethod
+    def last_regex(self) -> Pattern:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def each_prefix_regex(self) -> Pattern:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def periodic_regex(self) -> Pattern:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def each_unit_regex(self) -> Pattern:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def each_day_regex(self) -> Pattern:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def before_each_day_regex(self) -> Pattern:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def set_week_day_regex(self) -> Pattern:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def set_each_regex(self) -> Pattern:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def duration_extractor(self) -> DateTimeExtractor:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def time_extractor(self) -> DateTimeExtractor:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def date_extractor(self) -> DateTimeExtractor:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def date_time_extractor(self) -> DateTimeExtractor:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def date_period_extractor(self) -> DateTimeExtractor:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def time_period_extractor(self) -> DateTimeExtractor:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def date_time_period_extractor(self) -> DateTimeExtractor:
+        raise NotImplementedError
 
 class BaseSetExtractor(DateTimeExtractor):
     @property
