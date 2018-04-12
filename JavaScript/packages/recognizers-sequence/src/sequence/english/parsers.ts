34a9b840f9e8130ab4b686ce9789c650d2c96580
FAREAST\v-zelzha <v-zelzha@microsoft.com>
2018-04-12 16:53:09 +0800

    Add sequence recognizers to JS

--- JavaScript/packages/recognizers-sequence/src/sequence/english/parsers.ts
+++ JavaScript/packages/recognizers-sequence/src/sequence/english/parsers.ts
@@ -6,4 +6,20 @@ export class PhoneNumberParser extends BaseSequenceParser {
 
 export class IpParser extends BaseIpParser {
     
+}
+
+export class MentionParser extends BaseSequenceParser {
+    
 } 
+
+export class HashtagParser extends BaseSequenceParser {
+    
+}
+
+export class EmailParser extends BaseSequenceParser {
+    
+}
+
+export class URLParser extends BaseSequenceParser {
+    
+}
