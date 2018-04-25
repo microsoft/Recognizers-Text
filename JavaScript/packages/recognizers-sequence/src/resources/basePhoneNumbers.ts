3fb83e6d590493882eb0920458c5fdbb6f071124
Sanxing Chen <v-sanc@microsoft.com>
2018-04-25 16:45:26 +0800

    Better support phone number extraction

--- JavaScript/packages/recognizers-sequence/src/resources/basePhoneNumbers.ts
+++ JavaScript/packages/recognizers-sequence/src/resources/basePhoneNumbers.ts
@@ -11,7 +11,7 @@ export namespace BasePhoneNumbers {
 	export const GeneralPhoneNumberRegex = `(\\b((\\d[\\s]?){7,15}\\d)\\b)`;
 	export const BrazilPhoneNumberRegex = `(((\\B\\(\\s?))\\d{2,3}(\\s?\\))|(\\b\\d{2,3}))\\s?\\d{4,5}-?\\d{3,5}\\b`;
 	export const UkPhoneNumberRegex = `(((\\b(00)|\\B\\+)\\s?)?\\b\\d{2}\\s?(\\(0\\))?\\d{3,5}[/-]?\\s?(\\d{7,8}|\\d{3}\\s?\\d{3})\\b)`;
-	export const GermanyPhoneNumberRegex = `(\\b(\\d{4}\\s?[-/]?(\\s?\\d){7,8})\\b)`;
+	export const GermanyPhoneNumberRegex = `(\\b(\\d{3,4}\\s?[-/]?(\\s?\\d){7,8})\\b)`;
 	export const USPhoneNumberRegex = `((((\\B\\+)|\\b)1(\\s|-)?)|\\b)?(\\B\\(|\\b)\\d{3}\\)?\\s?-?\\s?\\d{3}\\s?-?\\s?\\d{4}\\b`;
-	export const SpecialPhoneNumberRegex = `\\b(\\d{4}[/-]\\d[/-]\\d{3,4})\\b`;
+	export const SpecialPhoneNumberRegex = `\\b(\\d{3,4}[/-]\\d{1,4}[/-]\\d{3,4})\\b`;
 }
