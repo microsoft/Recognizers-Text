87f62bc7967cc67a9ae6756feb08f8745522b1d2
Sanxing Chen <mestayinloves@gmail.com>
2018-04-27 12:24:15 +0800

    Integrate datatypes-date-time into build/packaging process (#496)
    * Integrate datatypes-date-time into build process
    * Add recognizers-text-data-types-timex-expression to main package's deps
    * Bumping npm package version of data-types-timex

---
+++ JavaScript/packages/datatypes-date-time/rollup.config.js
@@ -0,0 +1,26 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import pkg from './package.json';
+import camelCase from 'lodash.camelcase';
+
+export default {
+  input: `index.js`,
+  output: [
+    { file: pkg.module, format: 'es' },
+    { file: pkg.main, name: camelCase(pkg.name), format: 'umd', exports: 'named'  },
+    { file: pkg.browser, format: 'iife', name: camelCase(pkg.name), exports: 'named' }
+  ],
+  exports: 'named',
+  sourcemap: true,
+  plugins: [
+    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+    commonjs(),
+    // Allow node_modules resolution, so you can use 'external' to control
+    // which external modules to include in the bundle
+    // https://github.com/rollup/rollup-plugin-node-resolve#usage
+    resolve(),
+    // Resolve source maps to the original source
+    sourceMaps()
+  ]
+};
