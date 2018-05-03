69d008b7a58e482fe550c17d4c0d74409adca3dd
Sanxing Chen <v-sanc@microsoft.com>
2018-05-03 10:24:57 +0800

    fix comments

--- .NET/Microsoft.Recognizers.Text.DateTime/Constants.cs
+++ .NET/Microsoft.Recognizers.Text.DateTime/Constants.cs
@@ -57,8 +57,8 @@ namespace Microsoft.Recognizers.Text.DateTime
         public const string MID_MOD = "mid";
         public const string LATE_MOD = "end";
 
-        public const string MORE_THAN_MOD = "more than";
-        public const string LESS_THAN_MOD = "less than";
+        public const string MORE_THAN_MOD = "more";
+        public const string LESS_THAN_MOD = "less";
 
         // Invalid year
         public const int InvalidYear = int.MinValue;
