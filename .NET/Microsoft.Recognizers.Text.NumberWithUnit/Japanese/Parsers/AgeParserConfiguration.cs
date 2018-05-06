3bc26d84f5957d6ff7bb3b63e5927d1db2a33a1f
anichikage <anichikage@163.com>
2018-05-07 01:44:04 +0800

    Add Japanese NumberWithUnit(Age)

---
+++ .NET/Microsoft.Recognizers.Text.NumberWithUnit/Japanese/Parsers/AgeParserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using Microsoft.Recognizers.Definitions.Japanese;
+
+namespace Microsoft.Recognizers.Text.NumberWithUnit.Japanese
+{
+    public class AgeParserConfiguration : JapaneseNumberWithUnitParserConfiguration
+    {
+        public AgeParserConfiguration() : this(new CultureInfo(Culture.Chinese)) { }
+
+        public AgeParserConfiguration(CultureInfo ci) : base(ci)
+        {
+            this.BindDictionary(NumbersWithUnitDefinitions.AgeSuffixList);
+        }
+    }
+}
