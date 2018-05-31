2e61c2bbdab665373ba7f0c272c9ba388bce2373
Mu Guo <muguo@microsoft.com>
2018-05-31 13:13:15 +0800

    Support cases like "two weeks from / after / before today"

--- JavaScript/packages/recognizers-date-time/src/resources/englishDateTime.ts
+++ JavaScript/packages/recognizers-date-time/src/resources/englishDateTime.ts
@@ -25,8 +25,8 @@ export namespace EnglishDateTime {
 	export const FullTextYearRegex = `\\b((?<firsttwoyearnum>${CenturyRegex})\\s+(?<lasttwoyearnum>((zero|twenty|thirty|forty|fourty|fifty|sixty|seventy|eighty|ninety)\\s+${WrittenNumRegex})|${WrittenNumRegex}))\\b|\\b(?<firsttwoyearnum>${CenturyRegex})\\b`;
 	export const YearNumRegex = `\\b(?<year>((1[5-9]|20)\\d{2})|2100)\\b`;
 	export const YearRegex = `(${YearNumRegex}|${FullTextYearRegex})`;
-	export const WeekDayRegex = `\\b(in\\s+the\\s+day\\s+)?(?<weekday>Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Mon|Tues|Tue|Wedn|Weds|Wed|Thurs|Thur|Thu|Fri|Sat|Sun)s?\\b`;
-	export const SingleWeekDayRegex = `\\b(in\\s+the\\s+day\\s+)?(?<weekday>Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Mon|Tue|Tues|Wedn|Weds|Wed|Thurs|Thur|Thu|Fri|((?<=on)\\s+(Sat|Sun)))\\b`;
+	export const WeekDayRegex = `\\b(?<weekday>Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Mon|Tues|Tue|Wedn|Weds|Wed|Thurs|Thur|Thu|Fri|Sat|Sun)s?\\b`;
+	export const SingleWeekDayRegex = `\\b(?<weekday>Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Mon|Tue|Tues|Wedn|Weds|Wed|Thurs|Thur|Thu|Fri|((?<=on)\\s+(Sat|Sun)))\\b`;
 	export const RelativeMonthRegex = `(?<relmonth>(of\\s+)?${RelativeRegex}\\s+month)\\b`;
 	export const WrittenMonthRegex = `(((the\\s+)?month of\\s+)?(?<month>April|Apr|August|Aug|December|Dec|February|Feb|January|Jan|July|Jul|June|Jun|March|Mar|May|November|Nov|October|Oct|September|Sept|Sep))`;
 	export const MonthSuffixRegex = `(?<msuf>(in\\s+|of\\s+|on\\s+)?(${RelativeMonthRegex}|${WrittenMonthRegex}))`;
@@ -54,6 +54,7 @@ export namespace EnglishDateTime {
 	export const MidPrefixRegex = `(?<MidPrefix>mid|middle of)`;
 	export const LaterPrefixRegex = `(?<LatePrefix>late|later(\\s+in)?|end of)`;
 	export const PrefixPeriodRegex = `(${EarlyPrefixRegex}|${MidPrefixRegex}|${LaterPrefixRegex})`;
+	export const PrefixDayRegex = `((?<EarlyPrefix>early)|(?<MidPrefix>mid|middle)|(?<LatePrefix>late|later))(\\s+in)?(\\s+the\\s+day)?$`;
 	export const SeasonDescRegex = `(?<seas>spring|summer|fall|autumn|winter)`;
 	export const SeasonRegex = `\\b(?<season>(${PrefixPeriodRegex}\\s+)?(${RelativeRegex}\\s+)?${SeasonDescRegex}((\\s+of|\\s*,\\s*)?\\s+(${YearRegex}|${RelativeRegex}\\s+year))?)\\b`;
 	export const WhichWeekRegex = `(week)(\\s*)(?<number>\\d\\d|\\d|0\\d)`;
@@ -180,8 +181,8 @@ export namespace EnglishDateTime {
 	export const BeforeRegex = `\\b${InclusiveModPrepositions}?(before|in advance of|prior to|(no later|earlier|sooner) than|ending (with|on)|by|till|til|until)${InclusiveModPrepositions}?\\b\\s*`;
 	export const AfterRegex = `\\b${InclusiveModPrepositions}?(after(?!\\s+or equal to)|(?<!no\\s+)later than)${InclusiveModPrepositions}?\\b\\s*`;
 	export const SinceRegex = `\\b(since|after or equal to|starting (from|on|with))\\b\\s*`;
-	export const AgoRegex = `\\b(ago)\\b`;
-	export const LaterRegex = `\\b(later|from now)\\b`;
+	export const AgoRegex = `\\b(ago|before\\s+(?<day>yesterday|tomorrow|tmr|today))\\b`;
+	export const LaterRegex = `\\b(later|from\\s+now|(from|after)\\s+(?<day>yesterday|tomorrow|tmr|today))\\b`;
 	export const InConnectorRegex = `\\b(in)\\b`;
 	export const WithinNextPrefixRegex = `\\b(within(\\s+the)?(\\s+${NextPrefixRegex})?)\\b`;
 	export const AmPmDescRegex = `(ampm)`;
