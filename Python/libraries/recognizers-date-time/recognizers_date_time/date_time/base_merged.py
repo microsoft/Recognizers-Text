1cfc41ac94f19bf1722084506043d1b042455397
Enzo Cano <enzo.cano@southworks.com>
2018-06-07 16:34:21 -0300

    MergedExtractorConfiguration

--- Python/libraries/recognizers-date-time/recognizers_date_time/date_time/base_merged.py
+++ Python/libraries/recognizers-date-time/recognizers_date_time/date_time/base_merged.py
@@ -1,14 +1,105 @@
-from typing import List, Optional
+from abc import abstractmethod
+from typing import List, Optional, Pattern
 from datetime import datetime
+import regex
 
-from recognizers_text.extractor import ExtractResult
+from recognizers_text.extractor import Extractor, ExtractResult
 from .constants import Constants
 from .extractors import DateTimeExtractor
 from .parsers import DateTimeParser, DateTimeParseResult
 from .date_time_recognizer import DateTimeOptions
+from .utilities import Token, merge_all_tokens, RegExpUtility
 
 class MergedExtractorConfiguration:
-    pass
+    @property
+    @abstractmethod
+    def date_extractor(self) -> DateTimeExtractor:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def time_extractor(self) -> DateTimeExtractor:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def date_time_extractor(self) -> DateTimeExtractor:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def date_period_extractor(self) -> DateTimeExtractor:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def time_period_extractor(self) -> DateTimeExtractor:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def date_time_period_extractor(self) -> DateTimeExtractor:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def holiday_extractor(self) -> DateTimeExtractor:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def duration_extractor(self) -> DateTimeExtractor:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def set_extractor(self) -> DateTimeExtractor:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def integer_extractor(self) -> Extractor:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def after_regex(self) -> Pattern:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def before_regex(self) -> Pattern:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def since_regex(self) -> Pattern:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def from_to_regex(self) -> Pattern:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def single_ambiguous_month_regex(self) -> Pattern:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def preposition_suffix_regex(self) -> Pattern:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def number_ending_pattern(self) -> Pattern:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def filter_word_regex_list(self) -> List[Pattern]:
+        raise NotImplementedError
 
 class BaseMergedExtractor(DateTimeExtractor):
     @property
@@ -20,7 +111,43 @@ class BaseMergedExtractor(DateTimeExtractor):
         self.options = options
 
     def extract(self, source: str, reference: datetime = None) -> List[ExtractResult]:
-        #TODO: code
+        if reference is None:
+            reference = datetime.now()
+
+        result:List[ExtractResult] = list()
+        result = self.add_to(result, self.config.date_extractor.extract(source, reference), source)
+        result = self.add_to(result, self.config.time_extractor.extract(source, reference), source)
+        result = self.add_to(result, self.config.duration_extractor.extract(source, reference), source)
+        result = self.add_to(result, self.config.date_period_extractor.extract(source, reference), source)
+        result = self.add_to(result, self.config.date_time_extractor.extract(source, reference), source)
+        result = self.add_to(result, self.config.time_period_extractor.extract(source, reference), source)
+        result = self.add_to(result, self.config.date_time_period_extractor.extract(source, reference), source)
+        result = self.add_to(result, self.config.set_extractor.extract(source, reference), source)
+        result = self.add_to(result, self.config.holiday_extractor.extract(source, reference), source)
+
+        # this should be at the end since if need the extractor to determine the previous text contains time or not
+        result = self.add_to(result, self.number_ending_regex_match(source, result), source)
+
+        result = self.add_mod(result, source)
+
+        # filtering
+        if self.options & DateTimeOptions.CALENDAR:
+            result = self.check_calendar_filter_list(result, source)
+
+        result = sorted(result, key=lambda x: x.start)
+        
+        return result
+
+    def add_to(self, destination: List[ExtractResult], source: List[ExtractResult], text: str) -> List[ExtractResult]:
+        return destination
+
+    def number_ending_regex_match(self, source: str, extract_results: List[ExtractResult]) -> List[ExtractResult]:
+        pass
+
+    def add_mod(self, ers: List[ExtractResult], source: str) -> List[ExtractResult]:
+        pass
+
+    def check_calendar_filter_list(self, ers: List[ExtractResult], source: str) -> List[ExtractResult]:
         pass
 
 class MergedParserConfiguration:
