4a7311e18f955145743eb5da20e938207cd85b5b
Zhishuai Han <anichikage@163.com>
2018-06-15 18:29:48 +0800

    Fix はい！ not returned correct and miss そうです (#627)

--- JavaScript/packages/recognizers-choice/src/resources/japaneseChoice.ts
+++ JavaScript/packages/recognizers-choice/src/resources/japaneseChoice.ts
@@ -9,6 +9,6 @@
 export namespace JapaneseChoice {
 	export const LangMarker = 'Jpn';
 	export const TokenizerRegex = `[^\\w\\d\\u3040-\\u309f\\u30a0-\\u30ff\\uff00-\\uff9f\\u4e00-\\u9faf\\u3400-\\u4dbf]`;
-	export const TrueRegex = `(はい|そうです|そう|よい)|(\\uD83D\\uDC4D|\\uD83D\\uDC4C)`;
+	export const TrueRegex = `(はい(！)*|そうです|よい(です)*)|(\\uD83D\\uDC4D|\\uD83D\\uDC4C)`;
 	export const FalseRegex = `(いいえ|ではありません|ではない|じゃない|じゃありません)|(\\uD83D\\uDC4E|\\u270B|\\uD83D\\uDD90)`;
 }
