7c209794b73b90ca700c347355ef0930d9038d50
Pablo Costantini <sixdemons@gmail.com>
2018-06-28 12:19:27 -0300

    BaseCJKNumberParser

--- Java/libraries/recognizers-text/src/main/java/com/microsoft/recognizers/text/utilities/StringUtility.java
+++ Java/libraries/recognizers-text/src/main/java/com/microsoft/recognizers/text/utilities/StringUtility.java
@@ -4,4 +4,8 @@ public abstract class StringUtility {
     public static boolean isNullOrEmpty(String source) {
         return source == null || source.equals("");
     }
+
+    public static boolean isNullOrWhiteSpace(String source) {
+        return source == null || source.trim().equals("");
+    }
 }
