36204111c7bc09b8047bd1540d44d3b637185c7f
Amit Stein <amit.stein@southworks.com>
2018-07-03 18:25:08 -0300

    Port Date Period extractor in French

--- Python/libraries/recognizers-date-time/recognizers_date_time/date_time/base_dateperiod.py
+++ Python/libraries/recognizers-date-time/recognizers_date_time/date_time/base_dateperiod.py
@@ -281,7 +281,7 @@ class BaseDatePeriodExtractor(DateTimeExtractor):
         return tokens
 
     def __match_regex_in_prefix(self, source: str, match: Match) -> bool:
-        return match and source[match.end()]
+        return match and not source[match.end():].strip()
 
     def __get_token_for_regex_matching(self, source: str, regexp: Pattern, er: ExtractResult) -> List[Token]:
         tokens = []
