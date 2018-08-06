a274c9002551f05fd7326ef34b8b8521c48dcb1b
Hiroyuki Hasebe <crystalline407@gmail.com>
2018-08-06 16:14:56 +0900

    - Japanese Currency model for JavaScript (#770)

---
+++ JavaScript/packages/recognizers-number-with-unit/src/numberWithUnit/japanese/currency.ts
@@ -0,0 +1,39 @@
+import { CultureInfo, Culture } from "@microsoft/recognizers-text-number";
+import { Constants } from "../constants";
+import { JapaneseNumberWithUnitExtractorConfiguration, JapaneseNumberWithUnitParserConfiguration } from "./base";
+import { JapaneseNumericWithUnit } from "../../resources/japaneseNumericWithUnit";
+
+export class JapaneseCurrencyExtractorConfiguration extends JapaneseNumberWithUnitExtractorConfiguration {
+    readonly suffixList: ReadonlyMap<string, string>;
+    readonly prefixList: ReadonlyMap<string, string>;
+    readonly ambiguousUnitList: ReadonlyArray<string>;
+    readonly extractType: string;
+
+    constructor(ci?: CultureInfo) {
+        if (!ci) {
+            ci = new CultureInfo(Culture.Japanese);
+        }
+
+        super(ci);
+
+        this.extractType = Constants.SYS_UNIT_CURRENCY;
+
+        // Reference Source: https:// en.wikipedia.org/wiki/List_of_circulating_currencies
+        this.suffixList = JapaneseNumericWithUnit.CurrencySuffixList;
+        this.prefixList = JapaneseNumericWithUnit.CurrencyPrefixList;
+        this.ambiguousUnitList = JapaneseNumericWithUnit.CurrencyAmbiguousValues;;
+    }
+}
+
+export class JapaneseCurrencyParserConfiguration extends JapaneseNumberWithUnitParserConfiguration {
+    constructor(ci?: CultureInfo) {
+        if(!ci) {
+            ci = new CultureInfo(Culture.Japanese);
+        }
+
+        super(ci);
+
+        this.BindDictionary(JapaneseNumericWithUnit.CurrencySuffixList);
+        this.BindDictionary(JapaneseNumericWithUnit.CurrencyPrefixList);
+    }
+}
