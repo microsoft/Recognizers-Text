1f714cbaf696b4bb4836f6401bb838f277d41c62
Hiroyuki Hasebe <crystalline407@gmail.com>
2018-08-07 20:12:28 +0900

    [JavaScript] Add Japanese age model

---
+++ JavaScript/packages/recognizers-number-with-unit/src/numberWithUnit/japanese/age.ts
@@ -0,0 +1,37 @@
+import { CultureInfo, Culture } from "@microsoft/recognizers-text-number";
+import { Constants } from "../constants";
+import { JapaneseNumberWithUnitExtractorConfiguration, JapaneseNumberWithUnitParserConfiguration } from "./base";
+import { JapaneseNumericWithUnit } from "../../resources/japaneseNumericWithUnit";
+
+export class JapaneseAgeExtractorConfiguration extends JapaneseNumberWithUnitExtractorConfiguration {
+    readonly suffixList: ReadonlyMap<string, string>;
+    readonly prefixList: ReadonlyMap<string, string>;
+    readonly ambiguousUnitList: ReadonlyArray<string>;
+    readonly extractType: string;
+
+    constructor(ci?: CultureInfo) {
+        if (!ci) {
+            ci = new CultureInfo(Culture.Japanese);
+        }
+
+        super(ci);
+
+        this.extractType = Constants.SYS_UNIT_AGE;
+
+        this.suffixList = JapaneseNumericWithUnit.AgeSuffixList;
+        this.prefixList = new Map<string, string>();
+        this.ambiguousUnitList = JapaneseNumericWithUnit.AgeAmbiguousValues;
+    }
+}
+
+export class JapaneseAgeParserConfiguration extends JapaneseNumberWithUnitParserConfiguration {
+    constructor(ci?: CultureInfo) {
+        if(!ci) {
+            ci = new CultureInfo(Culture.Japanese);
+        }
+
+        super(ci);
+
+        this.BindDictionary(JapaneseNumericWithUnit.AgeSuffixList);
+    }
+}
