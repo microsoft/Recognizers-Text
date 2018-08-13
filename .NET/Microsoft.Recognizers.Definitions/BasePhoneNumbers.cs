b99789e942f1b0644f2dc85699fd6c3f0a5133fb
Yifan Sun <v-yifsu@microsoft.com>
2018-08-13 16:18:33 +0800

    Complement test cases and remove some improper cases; adjust phone number rules

--- .NET/Microsoft.Recognizers.Definitions/BasePhoneNumbers.cs
+++ .NET/Microsoft.Recognizers.Definitions/BasePhoneNumbers.cs
@@ -24,9 +24,9 @@ namespace Microsoft.Recognizers.Definitions
 		public const string DEPhoneNumberRegex = @"(\b(\d{3,4}\s?[-/]?(\s?\d){7,8})(?!-)\b)";
 		public const string USPhoneNumberRegex = @"((((\B\+)|\b)1(\s|-)?)|\b)?(\(\d{3}\)|\b\d{3})\s?-?\s?\d{3}\s?-?\s?\d{4}(?!-)\b";
 		public const string CNPhoneNumberRegex = @"((\b00\s?)?\+?86\s?-?\s?)?((\d{2,5}|\(\d{2,5}\))\s?-?\s?\d{4}\s?-?\s?\d{4}(\s?-?\s?\d{4})?|\d{3}\s?-?\s?\d{4}\s?-?\s?\d{4})(?!-)\b";
-		public const string DKPhoneNumberRegex = @"((\(\s?((?<!\d)\+\s?|00)45\s?\)(\s|-)?)|(((?<!\d)\+\s?|\b00\s?|(?<![\+\*])\b)45(\s|-)?)|(?<![\+\*])\b)((\d{8})|(\d{4}\s?-?\s?\d{4})|((\d{2}[\s-]){3}\d{2})|(\d{2}\s?-?\s?\d{3}\s?-?\s?\d{3}))(?!-)\b";
-		public const string ITPhoneNumberRegex = @"((\(\s?((?<!\d)\+\s?|00)39\s?\)(\s|-)?)|(((?<!\d)\+\s?|\b00\s?|(?<![\+\*])\b)39(\s|-)?)|(?<![\+\*])\b)((0[\d\s-]{4,10}\d)|(3[\d\s-]{7,9}\d))(?!-)\b";
-		public const string NLPhoneNumberRegex = @"(((\(\s?((?<!\d)\+\s?|00)31\s?\)(\s|-)?)|(((?<!\d)\+\s?|\b00\s?|(?<![\+\*])\b)31(\s|-)?)|(?<![\+\*])\b)(((0?\d{1,3})|(\(\s?0?\d{0,3}\s?\)))\s?-?\s?[\d\s]{5,11}\d)|\d{10,12})(?!-)\b";
+		public const string DKPhoneNumberRegex = @"((\(\s?((?<!\d)\+\s?|00)45\s?\)\s?)|(((?<!\d)\+\s?|\b00|(?<![\+\*])\b)45\s?)|(?<![\+\*])\b)((\d{8})|(\d{4}\s?-?\s?\d{4})|((\d{2}[\s-]){3}\d{2})|(\d{2}\s?-?\s?\d{3}\s?-?\s?\d{3}))(?!-)\b";
+		public const string ITPhoneNumberRegex = @"((\(\s?((?<!\d)\+\s?|00)39\s?\)\s?)|(((?<!\d)\+\s?|\b00|(?<![\+\*])\b)39\s?)|(?<![\+\*])\b)((0[\d\s-]{4,10}\d)|(3[\d\s-]{7,9}\d))(?!-)\b";
+		public const string NLPhoneNumberRegex = @"((((\(\s?((?<!\d)\+\s?|00)31\s?\)\s?)|(((?<!\d)\+\s?|\b00|(?<![\+\*])\b)31\s?)|(?<![\+\*])\b)(((0?\d{1,3})|(\(\s?0?\d{0,3}\s?\)))\s?-?\s?[\d\s]{5,11}\d))|\b\d{10,12})(?!-)\b";
 		public const string SpecialPhoneNumberRegex = @"\b(\d{3,4}[/-]\d{1,4}[/-]\d{3,4})\b";
 		public static readonly IList<char> SeparatorCharList = new List<char>
 		{
