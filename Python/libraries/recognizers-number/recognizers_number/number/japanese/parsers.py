024a5f6e53dc18ed727558999c4e0c746c8d5aee
Hiroyuki Hasebe <crystalline407@gmail.com>
2018-08-13 14:25:07 +0900

    [Python] Japanese Number model (#782)
    * Japanese Number model for Python
    
    * Add CJK number parser for Python

--- Python/libraries/recognizers-number/recognizers_number/number/japanese/parsers.py
+++ Python/libraries/recognizers-number/recognizers_number/number/japanese/parsers.py
@@ -8,14 +8,14 @@ from recognizers_text.utilities import RegExpUtility
 from recognizers_text.culture import Culture
 from recognizers_text.extractor import ExtractResult
 from recognizers_text.parser import ParseResult
-from recognizers_number.resources.chinese_numeric import ChineseNumeric
-from recognizers_number.number.cjk_parsers import CJKNumberParser
+from recognizers_number.resources.japanese_numeric import JapaneseNumeric
+from recognizers_number.number.cjk_parsers import CJKNumberParserConfiguration
 from recognizers_number.number.parsers import BaseNumberParser, NumberParserConfiguration
 from recognizers_number.culture import CultureInfo
 
 getcontext().prec = 15
 
-class ChineseNumberParserConfiguration(NumberParserConfiguration):
+class JapaneseNumberParserConfiguration(CJKNumberParserConfiguration):
     @property
     def cardinal_number_map(self) -> Dict[str, int]:
         return dict()
@@ -91,27 +91,27 @@ class ChineseNumberParserConfiguration(NumberParserConfiguration):
     @property
     def round_number_map_char(self) -> Dict[str, int]:
         return self._round_number_map_char
-
+    
     @property
     def full_to_half_map(self) -> Dict[str, int]:
         return self._full_to_half_map
-
+    
     @property
     def trato_sim_map(self) -> Dict[str, int]:
-        return self._trato_sim_map
-
+        return None
+    
     @property
     def unit_map(self) -> Dict[str, int]:
         return self._unit_map
-
+    
     @property
     def round_direct_list(self) -> List[str]:
         return self._round_direct_list
-
+    
     @property
     def digit_num_regex(self) -> Pattern:
         return self._digit_num_regex
-
+    
     @property
     def dozen_regex(self) -> Pattern:
         return self._dozen_regex
@@ -119,11 +119,11 @@ class ChineseNumberParserConfiguration(NumberParserConfiguration):
     @property
     def percentage_regex(self) -> Pattern:
         return self._percentage_regex
-
+    
     @property
     def double_and_round_regex(self) -> Pattern:
         return self._double_and_round_regex
-
+    
     @property
     def frac_split_regex(self) -> Pattern:
         return self._frac_split_regex
@@ -135,7 +135,7 @@ class ChineseNumberParserConfiguration(NumberParserConfiguration):
     @property
     def spe_get_number_regex(self) -> Pattern:
         return self._spe_get_number_regex
-
+    
     @property
     def pair_regex(self) -> Pattern:
         return self._pair_regex
@@ -146,35 +146,35 @@ class ChineseNumberParserConfiguration(NumberParserConfiguration):
 
     def __init__(self, culture_info=None):
         if culture_info is None:
-            culture_info = CultureInfo(Culture.Chinese)
+            culture_info = CultureInfo(Culture.Japanese)
 
         self._culture_info = culture_info
-        self._lang_marker = ChineseNumeric.LangMarker
-        self._decimal_separator_char = ChineseNumeric.DecimalSeparatorChar
-        self._fraction_marker_token = ChineseNumeric.FractionMarkerToken
-        self._non_decimal_separator_char = ChineseNumeric.NonDecimalSeparatorChar
-        self._half_a_dozen_text = ChineseNumeric.HalfADozenText
-        self._word_separator_token = ChineseNumeric.WordSeparatorToken
 
-        self._round_number_map = ChineseNumeric.RoundNumberMap
-        self._digital_number_regex = RegExpUtility.get_safe_reg_exp(ChineseNumeric.DigitalNumberRegex)
+        self._lang_marker = JapaneseNumeric.LangMarker
+        self._decimal_separator_char = JapaneseNumeric.DecimalSeparatorChar
+        self._fraction_marker_token = JapaneseNumeric.FractionMarkerToken
+        self._non_decimal_separator_char = JapaneseNumeric.NonDecimalSeparatorChar
+        self._half_a_dozen_text = JapaneseNumeric.HalfADozenText
+        self._word_separator_token = JapaneseNumeric.WordSeparatorToken
 
-        self._zero_to_nine_map = ChineseNumeric.ZeroToNineMap
-        self._round_number_map_char = ChineseNumeric.RoundNumberMapChar
-        self._full_to_half_map = ChineseNumeric.FullToHalfMap
-        self._trato_sim_map = ChineseNumeric.TratoSimMap
-        self._unit_map = ChineseNumeric.UnitMap
-        self._round_direct_list = ChineseNumeric.RoundDirectList
+        self._round_number_map = JapaneseNumeric.RoundNumberMap
+        self._digital_number_regex = RegExpUtility.get_safe_reg_exp(JapaneseNumeric.DigitalNumberRegex)
 
-        self._digit_num_regex = ChineseNumeric.DigitNumRegex
-        self._dozen_regex = ChineseNumeric.DozenRegex
-        self._percentage_regex = ChineseNumeric.PercentageRegex
-        self._double_and_round_regex = RegExpUtility.get_safe_reg_exp(ChineseNumeric.DoubleAndRoundRegex)
-        self._frac_split_regex = RegExpUtility.get_safe_reg_exp(ChineseNumeric.FracSplitRegex)
-        self._negative_number_sign_regex = RegExpUtility.get_safe_reg_exp(ChineseNumeric.NegativeNumberSignRegex)
-        self._point_regex = ChineseNumeric.PointRegex
-        self._spe_get_number_regex = RegExpUtility.get_safe_reg_exp(ChineseNumeric.SpeGetNumberRegex)
-        self._pair_regex = RegExpUtility.get_safe_reg_exp(ChineseNumeric.PairRegex)
+        self._zero_to_nine_map = JapaneseNumeric.ZeroToNineMap
+        self._round_number_map_char = JapaneseNumeric.RoundNumberMapChar
+        self._full_to_half_map = JapaneseNumeric.FullToHalfMap
+        self._unit_map = JapaneseNumeric.UnitMap
+        self._round_direct_list = JapaneseNumeric.RoundDirectList
+        self._digit_num_regex = JapaneseNumeric.DigitNumRegex
+        self._dozen_regex = JapaneseNumeric.DozenRegex
+        self._percentage_regex = JapaneseNumeric.PercentageRegex
+        self._double_and_round_regex = RegExpUtility.get_safe_reg_exp(JapaneseNumeric.DoubleAndRoundRegex)
+        self._frac_split_regex = RegExpUtility.get_safe_reg_exp(JapaneseNumeric.FracSplitRegex)
+        self._negative_number_sign_regex = RegExpUtility.get_safe_reg_exp(JapaneseNumeric.NegativeNumberSignRegex)
+        self._point_regex = JapaneseNumeric.PointRegex
+        self._spe_get_number_regex = RegExpUtility.get_safe_reg_exp(JapaneseNumeric.SpeGetNumberRegex)
+        self._pair_regex = RegExpUtility.get_safe_reg_exp(JapaneseNumeric.PairRegex)
+        self._round_number_integer_regex = RegExpUtility.get_safe_reg_exp(JapaneseNumeric.RoundNumberIntegerRegex)
 
     def normalize_token_set(self, tokens: List[str], context: ParseResult) -> List[str]:
         return tokens
