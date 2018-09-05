a3e0709ac147addf1a6bb914babdd06521aac9a6
matthewshim-ms <shimm@oregonstate.edu>
2018-09-04 19:29:23 -0700

    moved file to proper location

--- .NET/Microsoft.Recognizers.Text.Choice/French/Extractors/FrenchBooleanExtractorConfiguration.cs
+++ .NET/Microsoft.Recognizers.Text.Choice/French/Extractors/FrenchBooleanExtractorConfiguration.cs
@@ -1,10 +1,46 @@
-﻿using System;
+﻿using Microsoft.Recognizers.Definitions.Chinese;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Microsoft.Recognizers.Text.Choice.French.Extractors
 {
-    class FrenchBooleanExtractorConfiguration
+    class FrenchBooleanExtractorConfiguration : IBooleanExtractorConfiguration
     {
+        public static readonly Regex TrueRegex =
+            new Regex(ChoiceDefinitions.TrueRegex, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+        public static readonly Regex FalseRegex =
+            new Regex(ChoiceDefinitions.FalseRegex, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+        public static readonly Regex TokenRegex =
+            new Regex(ChoiceDefinitions.TokenizerRegex, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+        public static readonly IDictionary<Regex, string> MapRegexes = new Dictionary<Regex, string>()
+        {
+            {TrueRegex, Constants.SYS_BOOLEAN_TRUE },
+            {FalseRegex, Constants.SYS_BOOLEAN_FALSE }
+        };
+
+        public FrenchBooleanExtractorConfiguration(bool onlyTopMatch = true)
+        {
+            this.OnlyTopMatch = onlyTopMatch;
+        }
+
+        Regex IBooleanExtractorConfiguration.TrueRegex => TrueRegex;
+
+        Regex IBooleanExtractorConfiguration.FalseRegex => FalseRegex;
+
+        IDictionary<Regex, string> IChoiceExtractorConfiguration.MapRegexes => MapRegexes;
+
+        Regex IChoiceExtractorConfiguration.TokenRegex => TokenRegex;
+
+        public bool AllowPartialMatch => false;
+
+        public int MaxDistance => 2;
+
+        public bool OnlyTopMatch { get; }
+
     }
 }
