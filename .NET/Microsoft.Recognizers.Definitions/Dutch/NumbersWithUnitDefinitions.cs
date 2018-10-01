37631ec893a0c88917fa5b0dff7069328cb9f902
Mick Vleeshouwer <mick@imick.nl>
2018-10-01 18:03:27 +0200

    Add support for lowercase units

--- .NET/Microsoft.Recognizers.Definitions/Dutch/NumbersWithUnitDefinitions.cs
+++ .NET/Microsoft.Recognizers.Definitions/Dutch/NumbersWithUnitDefinitions.cs
@@ -44,16 +44,16 @@ namespace Microsoft.Recognizers.Definitions.Dutch
 		{
 			{ "Bit", "-bit|bit|bits" },
 			{ "Kilobit", "kilobit|kilobits|kb|kbit" },
-			{ "Megabit", "megabit|megabits|Mb|mb|Mbit" },
-			{ "Gigabit", "gigabit|gigabits|Gb|gb|Gbit" },
-			{ "Terabit", "terabit|terabits|Tb|tb|Tbit" },
-			{ "Petabit", "petabit|petabits|Pb|pb|Pbit" },
+			{ "Megabit", "megabit|megabits|Mb|Mbit" },
+			{ "Gigabit", "gigabit|gigabits|Gb|Gbit" },
+			{ "Terabit", "terabit|terabits|Tb|Tbit" },
+			{ "Petabit", "petabit|petabits|Pb|Pbit" },
 			{ "Byte", "-byte|byte|bytes" },
 			{ "Kilobyte", "-kilobyte|-kilobytes|kilobyte|kB|KB|kilobytes|kilo byte|kilo bytes|kByte" },
-			{ "Megabyte", "-megabyte|-megabytes|megabyte|mB|MB|megabytes|mega byte|mega bytes|MByte" },
-			{ "Gigabyte", "-gigabyte|-gigabytes|gigabyte|gB|GB|gigabytes|giga byte|giga bytes|GByte" },
-			{ "Terabyte", "-terabyte|-terabytes|terabyte|tB|TB|terabytes|tera byte|tera bytes|TByte" },
-			{ "Petabyte", "-petabyte|-petabytes|petabyte|pB|PB|petabytes|peta byte|peta bytes|PByte" }
+			{ "Megabyte", "-megabyte|-megabytes|megabyte|mB|MB|mb|megabytes|mega byte|mega bytes|MByte" },
+			{ "Gigabyte", "-gigabyte|-gigabytes|gigabyte|gB|GB|gb|gigabytes|giga byte|giga bytes|GByte" },
+			{ "Terabyte", "-terabyte|-terabytes|terabyte|tB|TB|tb|terabytes|tera byte|tera bytes|TByte" },
+			{ "Petabyte", "-petabyte|-petabytes|petabyte|pB|PB|pb|petabytes|peta byte|peta bytes|PByte" }
 		};
 		public static readonly IList<string> AmbiguousDimensionUnitList = new List<string>
 		{
