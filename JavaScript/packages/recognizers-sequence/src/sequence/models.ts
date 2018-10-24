2e1951ea72451a2751707db0bfc38006949cfc22
Yifan Sun <v-yifsu@microsoft.com>
2018-10-24 20:00:15 +0800

    Support more GUID formats and add score function

--- JavaScript/packages/recognizers-sequence/src/sequence/models.ts
+++ JavaScript/packages/recognizers-sequence/src/sequence/models.ts
@@ -89,4 +89,22 @@ export class URLModel extends AbstractSequenceModel {
 
 export class GUIDModel extends AbstractSequenceModel {
     public modelTypeName: string = "guid";
+
+    parse(query: string): ModelResult[] {
+        let extractResults = this.extractor.extract(query);
+        let parseResults = extractResults.map(r => this.parser.parse(r));
+
+        return parseResults
+            .map(o => o as ParseResult)
+            .map(o => ({
+                start: o.start,
+                end: o.start + o.length - 1,
+                resolution: {
+                    "value": o.resolutionStr,
+                    "score": o.value.toString()
+                },
+                text: o.text,
+                typeName: this.modelTypeName
+            }));
+    }
 }
