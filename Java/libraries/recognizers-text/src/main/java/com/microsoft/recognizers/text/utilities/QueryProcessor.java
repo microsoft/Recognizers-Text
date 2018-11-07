42db0bde97aacb4c30d30c82d9de770faf8765aa
Börje F. Karlsson <borjekar@microsoft.com>
2018-11-07 16:29:54 +0800

    Lowercase except sensitive terms

--- Java/libraries/recognizers-text/src/main/java/com/microsoft/recognizers/text/utilities/QueryProcessor.java
+++ Java/libraries/recognizers-text/src/main/java/com/microsoft/recognizers/text/utilities/QueryProcessor.java
@@ -2,6 +2,7 @@ package com.microsoft.recognizers.text.utilities;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -47,9 +48,34 @@ public class QueryProcessor {
 
         if (!caseSensitive) {
             query = query.toLowerCase();
+        } else {
+            query = toLowerTermSensitive(query);
         }
 
         return query;
+    }
+
+    private static String tokens = "(kB|K[Bb]|K|M[Bb]|M|G[Bb]|G|B)";
+    private static String expression = "(?<=(\\s|\\d))" + tokens + "\\b";
+    private static Pattern special_tokens_regex = Pattern.compile(expression, Pattern.UNICODE_CHARACTER_CLASS);
+
+    private static String toLowerTermSensitive(String input) {
+
+        char[] inputChars = input.toLowerCase(Locale.ROOT).toCharArray();
+
+        Match[] matches = RegExpUtility.getMatches(special_tokens_regex, input);
+        for (Match match : matches) {
+            QueryProcessor.applyReverse(match.index, inputChars, match.value);
+        }
+
+        return new String(inputChars);
+    }
+
+    private static void applyReverse(int index, char[] inputChars, String value) {
+        for (int i = 0; i < value.length(); ++i)
+        {
+            inputChars[index + i] = value.charAt(i);
+        }
     }
 
     public static String trimEnd(String input) {
