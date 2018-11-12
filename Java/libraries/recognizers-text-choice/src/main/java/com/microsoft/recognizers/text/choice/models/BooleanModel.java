ba4816056485fbfb8e3315df4ac1490275531ee5
Joe <quesjose1@gmail.com>
2018-11-12 00:28:30 -0300

    [Java - Choice] Base classes porting (#957)
    * WIP for base classes port
    
    * Fixed implements and base classes
    
    * Modified Resource-Generator POM to include Choice resources

---
+++ Java/libraries/recognizers-text-choice/src/main/java/com/microsoft/recognizers/text/choice/models/BooleanModel.java
@@ -0,0 +1,24 @@
+package com.microsoft.recognizers.text.choice.models;
+
+import java.util.Map;
+
+import com.microsoft.recognizers.text.IExtractor;
+import com.microsoft.recognizers.text.IParser;
+import com.microsoft.recognizers.text.ParseResult;
+import com.microsoft.recognizers.text.choice.Constants;
+
+public class BooleanModel extends ChoiceModel {
+
+	public BooleanModel(IParser parser, IExtractor extractor) {
+		super(parser, extractor);
+		throw new UnsupportedOperationException();
+	}
+
+	public String getModelTypeName() {
+		return Constants.MODEL_BOOLEAN;
+	}
+
+	protected Map<String, Object> GetResolution(ParseResult parseResult) {
+		throw new UnsupportedOperationException();
+	}
+}
