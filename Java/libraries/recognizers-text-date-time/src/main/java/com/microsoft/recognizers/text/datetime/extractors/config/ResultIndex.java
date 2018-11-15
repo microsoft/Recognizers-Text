9f0939167be052d4c71f19c0b5706c4ce0f7c470
JuanAr <juan.arguello@southworks.com>
2018-11-15 00:33:23 -0300

    [Java][Datetime] Port BaseDatePeriodExtractor from C# to Java (#959)
    * Port BaseDatePeriodExtractor from C# to Java
    
    * Disable unsupported test case for Java

---
+++ Java/libraries/recognizers-text-date-time/src/main/java/com/microsoft/recognizers/text/datetime/extractors/config/ResultIndex.java
@@ -0,0 +1,11 @@
+package com.microsoft.recognizers.text.datetime.extractors.config;
+
+public class ResultIndex {
+  public final boolean result;
+  public final int index;
+
+  public ResultIndex(boolean result, int index) {
+    this.result = result;
+    this.index = index;
+  }
+}
