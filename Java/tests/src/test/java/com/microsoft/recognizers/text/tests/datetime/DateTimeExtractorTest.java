193f67c599079278054087a2fee9c8c976ee0405
Börje Karlsson <tellarin@gmail.com>
2018-11-19 11:19:24 +0800

    Update DateTimeExtractorTest.java

--- Java/tests/src/test/java/com/microsoft/recognizers/text/tests/datetime/DateTimeExtractorTest.java
+++ Java/tests/src/test/java/com/microsoft/recognizers/text/tests/datetime/DateTimeExtractorTest.java
@@ -89,18 +89,18 @@ public class DateTimeExtractorTest extends AbstractTest {
                 return new BaseDatePeriodExtractor(new EnglishDatePeriodExtractorConfiguration());
             case "DateExtractor":
                 return new BaseDateExtractor(new EnglishDateExtractorConfiguration());
+            case "DateTimePeriodExtractor":
+                return new BaseDateTimePeriodExtractor(new EnglishDateTimePeriodExtractorConfiguration());
             case "DurationExtractor":
                 return new BaseDurationExtractor(new EnglishDurationExtractorConfiguration());
-			case "SetExtractor":
-				return new BaseSetExtractor(new EnglishSetExtractorConfiguration());
             case "DateTimeExtractor":
                 return new BaseDateTimeExtractor(new EnglishDateTimeExtractorConfiguration());
             case "HolidayExtractor":
                 return new BaseHolidayExtractor(new EnglishHolidayExtractorConfiguration());
             case "TimeExtractor":
                 return new BaseTimeExtractor(new EnglishTimeExtractorConfiguration());
-            case "DateTimePeriodExtractor":
-                return new BaseDateTimePeriodExtractor(new EnglishDateTimePeriodExtractorConfiguration());
+            case "SetExtractor":
+                return new BaseSetExtractor(new EnglishSetExtractorConfiguration());
             case "TimeZoneExtractor":
                 return new BaseTimeZoneExtractor(new EnglishTimeZoneExtractorConfiguration(DateTimeOptions.EnablePreview));
             case "TimePeriodExtractor":
