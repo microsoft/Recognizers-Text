8cfc779feb97119fa0f2a58a944831c8e26dfaa1
Mick Vleeshouwer <mick@imick.nl>
2018-11-22 22:48:43 +0100

    Add missing culture string

--- .NET/Samples/SimpleConsole/Program.cs
+++ .NET/Samples/SimpleConsole/Program.cs
@@ -15,6 +15,7 @@ namespace SimpleConsole
     class Program
     {
         // Use English for the Recognizers culture
+        private const string DefaultCulture = Culture.English;
 
         static void Main(string[] args)
         {
