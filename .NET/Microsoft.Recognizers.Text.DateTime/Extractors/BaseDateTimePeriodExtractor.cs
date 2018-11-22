62ff2aa62f5892e7945a93e90bfd5aa46ac8fce1
Mu Guo <muguo@microsoft.com>
2018-11-22 12:34:03 +0800

    Fix logic and regex issues

--- .NET/Microsoft.Recognizers.Text.DateTime/Extractors/BaseDateTimePeriodExtractor.cs
+++ .NET/Microsoft.Recognizers.Text.DateTime/Extractors/BaseDateTimePeriodExtractor.cs
@@ -176,7 +176,7 @@ namespace Microsoft.Recognizers.Text.DateTime
                             var begin = er.Start ?? 0;
 
                             var middleStr = beforeStr.Substring(begin + (er.Length ?? 0)).Trim().ToLower();
-                            if (string.IsNullOrEmpty(middleStr) || this.config.PrepositionRegex.IsMatch(middleStr))
+                            if (string.IsNullOrEmpty(middleStr) || this.config.PrepositionRegex.IsExactMatch(middleStr))
                             {
                                 ret.Add(new Token(begin, match.Index + match.Length));
                                 hasBeforeDate = true;
@@ -194,7 +194,7 @@ namespace Microsoft.Recognizers.Text.DateTime
                             var begin = er[0].Start ?? 0;
                             var end = (er[0].Start ?? 0) + (er[0].Length ?? 0);
                             var middleStr = followedStr.Substring(0, begin).Trim().ToLower();
-                            if (string.IsNullOrEmpty(middleStr) || this.config.PrepositionRegex.IsMatch(middleStr))
+                            if (string.IsNullOrEmpty(middleStr) || this.config.PrepositionRegex.IsExactMatch(middleStr))
                             {
                                 ret.Add(new Token(match.Index, match.Index + match.Length + end));
                             }
---
+++ .NET/Microsoft.Recognizers.Text.DateTime/Utilities/RegexExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Microsoft.Recognizers.Text.DateTime
+{
+    public static class RegexExtension
+    {
+        // Regex match with match length equals to text length
+        public static bool IsExactMatch(this Regex regex, string text)
+        {
+            var match = regex.Match(text);
+
+            return (match.Success && match.Length == text.Trim().Length);
+        }
+    }
+}
