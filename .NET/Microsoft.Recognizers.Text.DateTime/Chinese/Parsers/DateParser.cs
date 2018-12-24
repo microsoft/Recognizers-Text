b0df0b32b9c8913db8c0ba732e12762108bb2484
Jin Wang <810256009@qq.com>
2018-12-24 14:50:03 +0800

    code format

--- .NET/Microsoft.Recognizers.Text.DateTime/Chinese/Parsers/DateParser.cs
+++ .NET/Microsoft.Recognizers.Text.DateTime/Chinese/Parsers/DateParser.cs
@@ -197,7 +197,6 @@ namespace Microsoft.Recognizers.Text.DateTime.Chinese
                         futureDate = DateObject.MinValue.SafeCreateFromValue(year, month - 1, day);
                         pastDate = DateObject.MinValue.SafeCreateFromValue(year, month - 1, day);
                     }
-
                     else
                     {
                         futureDate = DateObject.MinValue.SafeCreateFromValue(year, month, day);
@@ -210,7 +209,7 @@ namespace Microsoft.Recognizers.Text.DateTime.Chinese
                         {
                             if (IsLeapYear(year) || !IsFeb29th(month, day))
                             {
-                                 futureDate = futureDate.AddMonths(1);
+                                futureDate = futureDate.AddMonths(1);
                             }
                         }
 
