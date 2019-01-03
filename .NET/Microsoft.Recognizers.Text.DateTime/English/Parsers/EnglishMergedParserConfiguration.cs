1260d668cbe3b17434ee954e0278600e0a9cba78
DiegoCardozo94 <diego.cardozo@southworks.com>
2019-01-03 11:49:06 -0300

    Fix all StyleCop warnings in EnglishMergedParserConfiguration

--- .NET/Microsoft.Recognizers.Text.DateTime/English/Parsers/EnglishMergedParserConfiguration.cs
+++ .NET/Microsoft.Recognizers.Text.DateTime/English/Parsers/EnglishMergedParserConfiguration.cs
@@ -1,11 +1,28 @@
 ﻿using System.Text.RegularExpressions;
-
 using Microsoft.Recognizers.Text.Matcher;
 
 namespace Microsoft.Recognizers.Text.DateTime.English
 {
     public sealed class EnglishMergedParserConfiguration : EnglishCommonDateTimeParserConfiguration, IMergedParserConfiguration
     {
+        public EnglishMergedParserConfiguration(IOptionsConfiguration config)
+            : base(config)
+        {
+            BeforeRegex = EnglishMergedExtractorConfiguration.BeforeRegex;
+            AfterRegex = EnglishMergedExtractorConfiguration.AfterRegex;
+            SinceRegex = EnglishMergedExtractorConfiguration.SinceRegex;
+            AroundRegex = EnglishMergedExtractorConfiguration.AroundRegex;
+            DateAfter = EnglishMergedExtractorConfiguration.DateAfterRegex;
+            YearRegex = EnglishDatePeriodExtractorConfiguration.YearRegex;
+            SuperfluousWordMatcher = EnglishMergedExtractorConfiguration.SuperfluousWordMatcher;
+
+            DatePeriodParser = new BaseDatePeriodParser(new EnglishDatePeriodParserConfiguration(this));
+            TimePeriodParser = new BaseTimePeriodParser(new EnglishTimePeriodParserConfiguration(this));
+            DateTimePeriodParser = new BaseDateTimePeriodParser(new EnglishDateTimePeriodParserConfiguration(this));
+            SetParser = new BaseSetParser(new EnglishSetParserConfiguration(this));
+            HolidayParser = new BaseHolidayParser(new EnglishHolidayParserConfiguration(this));
+            TimeZoneParser = new BaseTimeZoneParser();
+        }
 
         public Regex BeforeRegex { get; }
 
@@ -24,23 +41,5 @@ namespace Microsoft.Recognizers.Text.DateTime.English
         public IDateTimeParser HolidayParser { get; }
 
         public StringMatcher SuperfluousWordMatcher { get; }
-
-        public EnglishMergedParserConfiguration(IOptionsConfiguration config) : base(config)
-        {
-            BeforeRegex = EnglishMergedExtractorConfiguration.BeforeRegex;
-            AfterRegex = EnglishMergedExtractorConfiguration.AfterRegex;
-            SinceRegex = EnglishMergedExtractorConfiguration.SinceRegex;
-            AroundRegex = EnglishMergedExtractorConfiguration.AroundRegex;
-            DateAfter = EnglishMergedExtractorConfiguration.DateAfterRegex;
-            YearRegex = EnglishDatePeriodExtractorConfiguration.YearRegex;
-            SuperfluousWordMatcher = EnglishMergedExtractorConfiguration.SuperfluousWordMatcher;
-
-            DatePeriodParser = new BaseDatePeriodParser(new EnglishDatePeriodParserConfiguration(this));
-            TimePeriodParser = new BaseTimePeriodParser(new EnglishTimePeriodParserConfiguration(this));
-            DateTimePeriodParser = new BaseDateTimePeriodParser(new EnglishDateTimePeriodParserConfiguration(this));
-            SetParser = new BaseSetParser(new EnglishSetParserConfiguration(this));
-            HolidayParser = new BaseHolidayParser(new EnglishHolidayParserConfiguration(this));
-            TimeZoneParser = new BaseTimeZoneParser();
-        }
     }
 }
