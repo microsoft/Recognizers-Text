dc4870450dfcc5087b2dd1e018647e6986cc3e47
Martin Battaglino <martinbatta32@gmail.com>
2019-01-03 11:32:50 -0300

    Fix all StyleCop warnings in FrenchDatePeriodExtractorConfiguration

--- .NET/Microsoft.Recognizers.Text.DateTime/French/Extractors/FrenchDatePeriodExtractorConfiguration.cs
+++ .NET/Microsoft.Recognizers.Text.DateTime/French/Extractors/FrenchDatePeriodExtractorConfiguration.cs
@@ -11,12 +11,12 @@ namespace Microsoft.Recognizers.Text.DateTime.French
     {
         // base regexes
 
-        // until 
-        public static readonly Regex TillRegex = 
+        // until
+        public static readonly Regex TillRegex =
             new Regex(DateTimeDefinitions.TillRegex, RegexOptions.Singleline);
 
-        // and 
-        public static readonly Regex AndRegex = 
+        // and
+        public static readonly Regex AndRegex =
             new Regex(DateTimeDefinitions.RangeConnectorRegex, RegexOptions.Singleline);
 
         public static readonly Regex DayRegex =
@@ -28,15 +28,15 @@ namespace Microsoft.Recognizers.Text.DateTime.French
         public static readonly Regex IllegalYearRegex =
             new Regex(BaseDateTime.IllegalYearRegex, RegexOptions.Singleline);
 
-        public static readonly Regex YearRegex = 
+        public static readonly Regex YearRegex =
             new Regex(DateTimeDefinitions.YearRegex, RegexOptions.Singleline);
 
         public static readonly Regex WeekDayRegex =
             new Regex(DateTimeDefinitions.WeekDayRegex, RegexOptions.Singleline);
 
         // this month, next month, last month
-        public static readonly Regex RelativeMonthRegex = 
-            new Regex( DateTimeDefinitions.RelativeMonthRegex, RegexOptions.Singleline);
+        public static readonly Regex RelativeMonthRegex =
+            new Regex(DateTimeDefinitions.RelativeMonthRegex, RegexOptions.Singleline);
 
         public static readonly Regex WrittenMonthRegex =
             new Regex(DateTimeDefinitions.WrittenMonthRegex, RegexOptions.Singleline);
@@ -52,14 +52,14 @@ namespace Microsoft.Recognizers.Text.DateTime.French
         public static readonly Regex TimeUnitRegex =
             new Regex(DateTimeDefinitions.TimeUnitRegex, RegexOptions.Singleline);
 
-        // **In French, Past/Next is suffix, but interface enforces this 
+        // **In French, Past/Next is suffix, but interface enforces this
         // past, last, previous
-        public static readonly Regex PastPrefixRegex = 
+        public static readonly Regex PastPrefixRegex =
             new Regex(DateTimeDefinitions.PastSuffixRegex, RegexOptions.Singleline);
 
-        // **In French, Past/Next is suffix, but interface enforces this 
+        // **In French, Past/Next is suffix, but interface enforces this
         // next, in
-        public static readonly Regex NextPrefixRegex = 
+        public static readonly Regex NextPrefixRegex =
             new Regex(DateTimeDefinitions.NextSuffixRegex, RegexOptions.Singleline);
 
         public static readonly Regex FutureSuffixRegex =
@@ -73,7 +73,7 @@ namespace Microsoft.Recognizers.Text.DateTime.French
             new Regex(DateTimeDefinitions.SimpleCasesRegex, RegexOptions.Singleline);
 
         // between 'x' until 'y', from 'x' until 'y'
-        public static readonly Regex MonthFrontSimpleCasesRegex = 
+        public static readonly Regex MonthFrontSimpleCasesRegex =
             new Regex(DateTimeDefinitions.MonthFrontSimpleCasesRegex, RegexOptions.Singleline);
 
         public static readonly Regex MonthFrontBetweenRegex =
@@ -85,9 +85,9 @@ namespace Microsoft.Recognizers.Text.DateTime.French
         public static readonly Regex MonthWithYear =
             new Regex(DateTimeDefinitions.MonthWithYear, RegexOptions.Singleline);
 
-        // a cote de - 'next to', cette - 'this', dernier - 'last' (always after the noun, i.e annee dernier - 'last year'  
+        // a cote de - 'next to', cette - 'this', dernier - 'last' (always after the noun, i.e annee dernier - 'last year'
         public static readonly Regex OneWordPeriodRegex =
-            new Regex( DateTimeDefinitions.OneWordPeriodRegex, RegexOptions.Singleline); 
+            new Regex(DateTimeDefinitions.OneWordPeriodRegex, RegexOptions.Singleline);
 
         public static readonly Regex MonthNumWithYear =
             new Regex(DateTimeDefinitions.MonthNumWithYear, RegexOptions.Singleline);
@@ -99,18 +99,18 @@ namespace Microsoft.Recognizers.Text.DateTime.French
         public static readonly Regex WeekOfYearRegex =
             new Regex(DateTimeDefinitions.WeekOfYearRegex, RegexOptions.Singleline);
 
-        public static readonly Regex FollowedDateUnit = 
+        public static readonly Regex FollowedDateUnit =
             new Regex(DateTimeDefinitions.FollowedDateUnit, RegexOptions.Singleline);
 
         public static readonly Regex NumberCombinedWithDateUnit =
             new Regex(DateTimeDefinitions.NumberCombinedWithDateUnit, RegexOptions.Singleline);
 
-        // 1st quarter of this year, 2nd quarter of next/last year, etc 
+        // 1st quarter of this year, 2nd quarter of next/last year, etc
         public static readonly Regex QuarterRegex =
             new Regex(DateTimeDefinitions.QuarterRegex, RegexOptions.Singleline);
 
         public static readonly Regex QuarterRegexYearFront =
-            new Regex(DateTimeDefinitions.QuarterRegexYearFront, RegexOptions.Singleline); 
+            new Regex(DateTimeDefinitions.QuarterRegexYearFront, RegexOptions.Singleline);
 
         public static readonly Regex AllHalfYearRegex =
             new Regex(DateTimeDefinitions.AllHalfYearRegex, RegexOptions.Singleline);
@@ -127,7 +127,7 @@ namespace Microsoft.Recognizers.Text.DateTime.French
         public static readonly Regex MonthOfRegex =
             new Regex(DateTimeDefinitions.MonthOfRegex, RegexOptions.Singleline);
 
-        //TODO: add regexs below
+        // TODO: add regexs below
         public static readonly Regex RangeUnitRegex =
             new Regex(DateTimeDefinitions.RangeUnitRegex, RegexOptions.Singleline);
 
@@ -164,17 +164,8 @@ namespace Microsoft.Recognizers.Text.DateTime.French
         public static readonly Regex ReferenceDatePeriodRegex =
             new Regex(DateTimeDefinitions.ReferenceDatePeriodRegex, RegexOptions.Singleline);
 
-        private static readonly Regex FromRegex = 
-            new Regex(DateTimeDefinitions.FromRegex, RegexOptions.Singleline);
-
-        private static readonly Regex ConnectorAndRegex = 
-            new Regex(DateTimeDefinitions.ConnectorAndRegex, RegexOptions.Singleline);
-
-        private static readonly Regex BeforeRegex = 
-            new Regex(DateTimeDefinitions.BeforeRegex2, RegexOptions.Singleline);
-
         public static readonly Regex AgoRegex =
-            new Regex(DateTimeDefinitions.AgoRegex, RegexOptions.Singleline);
+                    new Regex(DateTimeDefinitions.AgoRegex, RegexOptions.Singleline);
 
         public static readonly Regex LaterRegex =
             new Regex(DateTimeDefinitions.LaterRegex, RegexOptions.Singleline);
@@ -187,6 +178,15 @@ namespace Microsoft.Recognizers.Text.DateTime.French
 
         public static readonly Regex CenturySuffixRegex =
             new Regex(DateTimeDefinitions.CenturySuffixRegex, RegexOptions.Singleline);
+
+        private static readonly Regex FromRegex =
+            new Regex(DateTimeDefinitions.FromRegex, RegexOptions.Singleline);
+
+        private static readonly Regex ConnectorAndRegex =
+            new Regex(DateTimeDefinitions.ConnectorAndRegex, RegexOptions.Singleline);
+
+        private static readonly Regex BeforeRegex =
+            new Regex(DateTimeDefinitions.BeforeRegex2, RegexOptions.Singleline);
 
         private static readonly Regex[] SimpleCasesRegexes =
         {
@@ -211,10 +211,11 @@ namespace Microsoft.Recognizers.Text.DateTime.French
             WeekWithWeekDayRangeRegex,
             YearPlusNumberRegex,
             DecadeWithCenturyRegex,
-            RelativeDecadeRegex
+            RelativeDecadeRegex,
         };
 
-        public FrenchDatePeriodExtractorConfiguration(IOptionsConfiguration config) : base(config)
+        public FrenchDatePeriodExtractorConfiguration(IOptionsConfiguration config)
+            : base(config)
         {
             DatePointExtractor = new BaseDateExtractor(new FrenchDateExtractorConfiguration(this));
             CardinalExtractor = Number.French.CardinalExtractor.GetInstance();
