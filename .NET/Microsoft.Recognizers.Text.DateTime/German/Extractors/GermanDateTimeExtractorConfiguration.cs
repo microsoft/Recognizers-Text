3c3cb6c09328f4c096825725ef03b7548d186546
Martin Battaglino <martinbatta32@gmail.com>
2019-01-04 17:28:54 -0300

    Implement Regex Convention to GermanDateTimeExtractorConfiguration

--- .NET/Microsoft.Recognizers.Text.DateTime/German/Extractors/GermanDateTimeExtractorConfiguration.cs
+++ .NET/Microsoft.Recognizers.Text.DateTime/German/Extractors/GermanDateTimeExtractorConfiguration.cs
@@ -8,27 +8,20 @@ namespace Microsoft.Recognizers.Text.DateTime.German
 {
     public class GermanDateTimeExtractorConfiguration : BaseOptionsConfiguration, IDateTimeExtractorConfiguration
     {
-        public static readonly Regex PrepositionRegex = new Regex(
-            DateTimeDefinitions.PrepositionRegex,
-            RegexOptions.Singleline);
+        public static readonly Regex PrepositionRegex =
+            new Regex(DateTimeDefinitions.PrepositionRegex, RegexOptions.Singleline);
 
         public static readonly Regex NowRegex =
-            new Regex(
-                DateTimeDefinitions.NowRegex,
-                RegexOptions.Singleline);
+            new Regex(DateTimeDefinitions.NowRegex, RegexOptions.Singleline);
 
-        public static readonly Regex SuffixRegex = new Regex(
-            DateTimeDefinitions.SuffixRegex,
-            RegexOptions.Singleline);
+        public static readonly Regex SuffixRegex =
+            new Regex(DateTimeDefinitions.SuffixRegex, RegexOptions.Singleline);
 
-        public static readonly Regex TimeOfDayRegex = new Regex(
-            DateTimeDefinitions.TimeOfDayRegex,
-            RegexOptions.Singleline);
+        public static readonly Regex TimeOfDayRegex =
+            new Regex(DateTimeDefinitions.TimeOfDayRegex, RegexOptions.Singleline);
 
         public static readonly Regex SpecificTimeOfDayRegex =
-            new Regex(
-                DateTimeDefinitions.SpecificTimeOfDayRegex,
-                RegexOptions.Singleline);
+            new Regex(DateTimeDefinitions.SpecificTimeOfDayRegex, RegexOptions.Singleline);
 
         public static readonly Regex TimeOfTodayAfterRegex =
              new Regex(DateTimeDefinitions.TimeOfTodayAfterRegex, RegexOptions.Singleline);
@@ -40,33 +33,25 @@ namespace Microsoft.Recognizers.Text.DateTime.German
             new Regex(DateTimeDefinitions.SimpleTimeOfTodayAfterRegex, RegexOptions.Singleline);
 
         public static readonly Regex SimpleTimeOfTodayBeforeRegex =
-            new Regex(
-                DateTimeDefinitions.SimpleTimeOfTodayBeforeRegex,
-                RegexOptions.Singleline);
+            new Regex(DateTimeDefinitions.SimpleTimeOfTodayBeforeRegex, RegexOptions.Singleline);
 
-        public static readonly Regex SpecificEndOfRegex = new Regex(
-            DateTimeDefinitions.SpecificEndOfRegex,
-            RegexOptions.Singleline);
+        public static readonly Regex SpecificEndOfRegex =
+            new Regex(DateTimeDefinitions.SpecificEndOfRegex, RegexOptions.Singleline);
 
-        public static readonly Regex UnspecificEndOfRegex = new Regex(
-            DateTimeDefinitions.UnspecificEndOfRegex,
-            RegexOptions.Singleline);
+        public static readonly Regex UnspecificEndOfRegex =
+            new Regex(DateTimeDefinitions.UnspecificEndOfRegex, RegexOptions.Singleline);
 
-        public static readonly Regex UnitRegex = new Regex(
-            DateTimeDefinitions.TimeUnitRegex,
-            RegexOptions.Singleline);
+        public static readonly Regex UnitRegex =
+            new Regex(DateTimeDefinitions.TimeUnitRegex, RegexOptions.Singleline);
 
-        public static readonly Regex ConnectorRegex = new Regex(
-            DateTimeDefinitions.ConnectorRegex,
-            RegexOptions.Singleline);
+        public static readonly Regex ConnectorRegex =
+            new Regex(DateTimeDefinitions.ConnectorRegex, RegexOptions.Singleline);
 
-        public static readonly Regex NumberAsTimeRegex = new Regex(
-            DateTimeDefinitions.NumberAsTimeRegex,
-            RegexOptions.Singleline);
+        public static readonly Regex NumberAsTimeRegex =
+            new Regex(DateTimeDefinitions.NumberAsTimeRegex, RegexOptions.Singleline);
 
-        public static readonly Regex DateNumberConnectorRegex = new Regex(
-            DateTimeDefinitions.DateNumberConnectorRegex,
-            RegexOptions.Singleline);
+        public static readonly Regex DateNumberConnectorRegex =
+            new Regex(DateTimeDefinitions.DateNumberConnectorRegex, RegexOptions.Singleline);
 
         public GermanDateTimeExtractorConfiguration(IOptionsConfiguration config)
             : base(config)
