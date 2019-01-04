a3e58596e468a16fa4f9e7bd93078f20d2212d6b
Martin Battaglino <martinbatta32@gmail.com>
2019-01-04 09:38:03 -0300

    Fix all StyleCop warnings in PortugueseTimeZoneExtractorConfiguration
    - Added internal keyword
    - Added spaces when needed

--- .NET/Microsoft.Recognizers.Text.DateTime/Portuguese/Extractors/PortugueseTimeZoneExtractorConfiguration.cs
+++ .NET/Microsoft.Recognizers.Text.DateTime/Portuguese/Extractors/PortugueseTimeZoneExtractorConfiguration.cs
@@ -6,19 +6,23 @@ using Microsoft.Recognizers.Text.Matcher;
 
 namespace Microsoft.Recognizers.Text.DateTime.Portuguese
 {
-    class PortugueseTimeZoneExtractorConfiguration : BaseOptionsConfiguration, ITimeZoneExtractorConfiguration
+    internal class PortugueseTimeZoneExtractorConfiguration : BaseOptionsConfiguration, ITimeZoneExtractorConfiguration
     {
         public static readonly Regex[] TimeZoneRegexList =
         {
         };
 
-        public PortugueseTimeZoneExtractorConfiguration(IOptionsConfiguration config) : base(config)
+        public PortugueseTimeZoneExtractorConfiguration(IOptionsConfiguration config)
+            : base(config)
         {
         }
 
         public IEnumerable<Regex> TimeZoneRegexes => TimeZoneRegexList;
+
         public Regex LocationTimeSuffixRegex { get; }
+
         public StringMatcher LocationMatcher { get; }
+
         public List<string> AmbiguousTimezoneList => new List<string>();
     }
 }
