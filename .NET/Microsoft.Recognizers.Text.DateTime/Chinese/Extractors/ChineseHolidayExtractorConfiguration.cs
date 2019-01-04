7e646c673f8ec64c89779428e8de9d1ab993da22
Cecilia Avila <cecilia.avila@southworks.com>
2019-01-04 12:48:54 -0300

    Fix all StyleCop warnings in HolidayExtractorChs
    - File has been renamed to 'ChineseHolidayExtractorConfiguration' to match class name.
    - Added spaces when needed

---
+++ .NET/Microsoft.Recognizers.Text.DateTime/Chinese/Extractors/ChineseHolidayExtractorConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Microsoft.Recognizers.Definitions.Chinese;
+
+namespace Microsoft.Recognizers.Text.DateTime.Chinese
+{
+    public class ChineseHolidayExtractorConfiguration : BaseOptionsConfiguration, IHolidayExtractorConfiguration
+    {
+        public static readonly Regex LunarHolidayRegex = new Regex(DateTimeDefinitions.LunarHolidayRegex, RegexOptions.Singleline);
+
+        public static readonly Regex[] HolidayRegexList =
+        {
+            new Regex(DateTimeDefinitions.HolidayRegexList1, RegexOptions.Singleline),
+            new Regex(DateTimeDefinitions.HolidayRegexList2, RegexOptions.Singleline),
+            LunarHolidayRegex,
+        };
+
+        public ChineseHolidayExtractorConfiguration()
+            : base()
+        {
+        }
+
+        public IEnumerable<Regex> HolidayRegexes => HolidayRegexList;
+    }
+}
