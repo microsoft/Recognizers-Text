1981e1aada723ce5e254f8bf6f3af9aa3e5208ca
DiegoCardozo94 <diego.cardozo@southworks.com>
2019-01-04 11:56:46 -0300

    Fix all StyleCop warnings in ItalianMergedParserConfiguration

--- .NET/Microsoft.Recognizers.Text.DateTime/Italian/Parsers/ItalianMergedParserConfiguration.cs
+++ .NET/Microsoft.Recognizers.Text.DateTime/Italian/Parsers/ItalianMergedParserConfiguration.cs
@@ -1,11 +1,27 @@
 ﻿using System.Text.RegularExpressions;
-
 using Microsoft.Recognizers.Text.Matcher;
 
 namespace Microsoft.Recognizers.Text.DateTime.Italian
 {
     public sealed class ItalianMergedParserConfiguration : ItalianCommonDateTimeParserConfiguration, IMergedParserConfiguration
     {
+        public ItalianMergedParserConfiguration(IOptionsConfiguration options)
+            : base(options)
+        {
+            BeforeRegex = ItalianMergedExtractorConfiguration.BeforeRegex;
+            AfterRegex = ItalianMergedExtractorConfiguration.AfterRegex;
+            SinceRegex = ItalianMergedExtractorConfiguration.SinceRegex;
+            AroundRegex = ItalianMergedExtractorConfiguration.AroundRegex;
+            DateAfter = ItalianMergedExtractorConfiguration.DateAfterRegex;
+            YearRegex = ItalianDatePeriodExtractorConfiguration.YearRegex;
+            SuperfluousWordMatcher = ItalianMergedExtractorConfiguration.SuperfluousWordMatcher;
+            DatePeriodParser = new BaseDatePeriodParser(new ItalianDatePeriodParserConfiguration(this));
+            TimePeriodParser = new BaseTimePeriodParser(new ItalianTimePeriodParserConfiguration(this));
+            DateTimePeriodParser = new BaseDateTimePeriodParser(new ItalianDateTimePeriodParserConfiguration(this));
+            SetParser = new BaseSetParser(new ItalianSetParserConfiguration(this));
+            HolidayParser = new BaseHolidayParser(new ItalianHolidayParserConfiguration(this));
+            TimeZoneParser = new DummyTimeZoneParser();
+        }
 
         public Regex BeforeRegex { get; }
 
@@ -24,22 +40,5 @@ namespace Microsoft.Recognizers.Text.DateTime.Italian
         public IDateTimeParser HolidayParser { get; }
 
         public StringMatcher SuperfluousWordMatcher { get; }
-
-        public ItalianMergedParserConfiguration(IOptionsConfiguration options) : base(options)
-        {
-            BeforeRegex = ItalianMergedExtractorConfiguration.BeforeRegex;
-            AfterRegex = ItalianMergedExtractorConfiguration.AfterRegex;
-            SinceRegex = ItalianMergedExtractorConfiguration.SinceRegex;
-            AroundRegex = ItalianMergedExtractorConfiguration.AroundRegex;
-            DateAfter = ItalianMergedExtractorConfiguration.DateAfterRegex;
-            YearRegex = ItalianDatePeriodExtractorConfiguration.YearRegex;
-            SuperfluousWordMatcher = ItalianMergedExtractorConfiguration.SuperfluousWordMatcher;
-            DatePeriodParser = new BaseDatePeriodParser(new ItalianDatePeriodParserConfiguration(this));
-            TimePeriodParser = new BaseTimePeriodParser(new ItalianTimePeriodParserConfiguration(this));
-            DateTimePeriodParser = new BaseDateTimePeriodParser(new ItalianDateTimePeriodParserConfiguration(this));
-            SetParser = new BaseSetParser(new ItalianSetParserConfiguration(this));
-            HolidayParser = new BaseHolidayParser(new ItalianHolidayParserConfiguration(this));
-            TimeZoneParser = new DummyTimeZoneParser();
-        }
     }
 }
