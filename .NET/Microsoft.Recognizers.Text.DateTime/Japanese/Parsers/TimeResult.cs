cff9dfb706e4db6c50eabd49f92275576522f497
Billy Delgado <37461749+Bill7zz@users.noreply.github.com>
2019-01-04 11:24:30 -0300

    Fix StyleCop warnings in Japanese\TimeParser

---
+++ .NET/Microsoft.Recognizers.Text.DateTime/Japanese/Parsers/TimeResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.Recognizers.Definitions.Japanese;
+using DateObject = System.DateTime;
+
+namespace Microsoft.Recognizers.Text.DateTime.Japanese
+{
+
+    public class TimeResult
+    {
+        public int Hour { get; set; }
+
+        public int Minute { get; set; }
+
+        public int Second { get; set; }
+
+        public int LowBound { get; set; } = -1;
+    }
+}
