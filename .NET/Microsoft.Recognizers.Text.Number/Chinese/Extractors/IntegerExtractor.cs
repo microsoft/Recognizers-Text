67c6727aa3ae29d06f2ce907ca4fd5e81a48ee86
aliandi <mica.andini@gmail.com>
2019-01-07 11:15:33 -0300

    Fix all the StyleCop warnings in IntegerExtractor -Move constructor before properties -Add spaces before comments -Add trailing comma

--- .NET/Microsoft.Recognizers.Text.Number/Chinese/Extractors/IntegerExtractor.cs
+++ .NET/Microsoft.Recognizers.Text.Number/Chinese/Extractors/IntegerExtractor.cs
@@ -8,10 +8,6 @@ namespace Microsoft.Recognizers.Text.Number.Chinese
 {
     public class IntegerExtractor : BaseNumberExtractor
     {
-        internal sealed override ImmutableDictionary<Regex, TypeTag> Regexes { get; }
-
-        protected sealed override string ExtractType { get; } = Constants.SYS_NUM_INTEGER;
-        
         public IntegerExtractor(ChineseNumberExtractorMode mode = ChineseNumberExtractorMode.Default)
         {
             var regexes = new Dictionary<Regex, TypeTag>()
@@ -21,37 +17,37 @@ namespace Microsoft.Recognizers.Text.Number.Chinese
                     new Regex(NumbersDefinitions.NumbersSpecialsChars, RegexOptions.Singleline), RegexTagGenerator.GenerateRegexTag(Constants.INTEGER_PREFIX, Constants.NUMBER_SUFFIX)
                 },
                 {
-                    //15k,  16 G
+                    // 15k,  16 G
                     new Regex(NumbersDefinitions.NumbersSpecialsCharsWithSuffix, RegexOptions.Singleline), RegexTagGenerator.GenerateRegexTag(Constants.INTEGER_PREFIX, Constants.NUMBER_SUFFIX)
                 },
                 {
-                    //1,234,  ２，３３２，１１１
+                    // 1,234,  ２，３３２，１１１
                     new Regex(NumbersDefinitions.DottedNumbersSpecialsChar, RegexOptions.Singleline), RegexTagGenerator.GenerateRegexTag(Constants.INTEGER_PREFIX, Constants.NUMBER_SUFFIX)
                 },
                 {
-                    //半百  半打
+                    // 半百  半打
                     new Regex(NumbersDefinitions.NumbersWithHalfDozen, RegexOptions.Singleline), RegexTagGenerator.GenerateRegexTag(Constants.INTEGER_PREFIX, Constants.CHINESE)
                 },
                 {
-                    //半
+                    // 半
                     new Regex(NumbersDefinitions.HalfUnitRegex, RegexOptions.Singleline), RegexTagGenerator.GenerateRegexTag(Constants.INTEGER_PREFIX, Constants.CHINESE)
                 },
                 {
-                    //一打  五十打
+                    // 一打  五十打
                     new Regex(NumbersDefinitions.NumbersWithDozen, RegexOptions.Singleline), RegexTagGenerator.GenerateRegexTag(Constants.INTEGER_PREFIX, Constants.CHINESE)
-                }
+                },
             };
 
             switch (mode)
             {
                 case ChineseNumberExtractorMode.Default:
-                    // 一百五十五, 负一亿三百二十二. 
+                    // 一百五十五, 负一亿三百二十二.
                     // Uses an allow list to avoid extracting "四" from "四川"
                     regexes.Add(new Regex(NumbersDefinitions.NumbersWithAllowListRegex, RegexOptions.Singleline), RegexTagGenerator.GenerateRegexTag(Constants.INTEGER_PREFIX, Constants.CHINESE));
                     break;
 
                 case ChineseNumberExtractorMode.ExtractAll:
-                    // 一百五十五, 负一亿三百二十二, "四" from "四川". 
+                    // 一百五十五, 负一亿三百二十二, "四" from "四川".
                     // Uses no allow lists and extracts all potential integers (useful in Units, for example).
                     regexes.Add(new Regex(NumbersDefinitions.NumbersAggressiveRegex, RegexOptions.Singleline), RegexTagGenerator.GenerateRegexTag(Constants.INTEGER_PREFIX, Constants.CHINESE));
                     break;
@@ -59,5 +55,9 @@ namespace Microsoft.Recognizers.Text.Number.Chinese
 
             Regexes = regexes.ToImmutableDictionary();
         }
+
+        internal sealed override ImmutableDictionary<Regex, TypeTag> Regexes { get; }
+
+        protected sealed override string ExtractType { get; } = Constants.SYS_NUM_INTEGER;
     }
 }
