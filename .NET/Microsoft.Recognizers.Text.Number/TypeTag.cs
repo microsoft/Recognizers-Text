1b8c3a0cc1b8c2fb4fbf6e5b2c4cd071859fece3
Billy Delgado <37461749+Bill7zz@users.noreply.github.com>
2019-01-07 10:35:58 -0300

    Fix StyleCop warnings in TypeTag file

--- .NET/Microsoft.Recognizers.Text.Number/TypeTag.cs
+++ .NET/Microsoft.Recognizers.Text.Number/TypeTag.cs
@@ -1,16 +1,15 @@
 ﻿namespace Microsoft.Recognizers.Text.Number
 {
-
     public class TypeTag
     {
-        public string Name { get; }
-
-        public int Priority { get; }
-
         public TypeTag(string name, int priority)
         {
             Name = name;
             Priority = priority;
         }
+
+        public string Name { get; }
+
+        public int Priority { get; }
     }
 }
