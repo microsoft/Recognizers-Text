14ac82d31c5e984882b2878fd240ddc1f805f14d
aliandi <mica.andini@gmail.com>
2019-01-07 11:26:18 -0300

    Fix all the StyleCop warnings in NumberExtractor -Move constructor before properties -Add description to enum and moved before the class -Move protected propertie after internal propertie

--- .NET/Microsoft.Recognizers.Text.Number/Chinese/Extractors/NumberExtractor.cs
+++ .NET/Microsoft.Recognizers.Text.Number/Chinese/Extractors/NumberExtractor.cs
@@ -3,12 +3,26 @@ using System.Text.RegularExpressions;
 
 namespace Microsoft.Recognizers.Text.Number.Chinese
 {
+    /// <summary>
+    /// These modes only apply to ChineseNumberExtractor.
+    /// The default more urilizes an allow list to avoid extracting numbers in ambiguous/undesired combinations of Chinese ideograms.
+    /// ExtractAll mode is to be used in cases where extraction should be more aggressive (e.g. in Units extraction).
+    /// </summary>
+    public enum ChineseNumberExtractorMode
+    {
+        /// <summary>
+        /// Number extraction with an allow list that filters what numbers to extract.
+        /// </summary>
+        Default,
+
+        /// <summary>
+        /// Extract all number-related terms aggressively.
+        /// </summary>
+        ExtractAll,
+    }
+
     public class NumberExtractor : BaseNumberExtractor
     {
-        internal sealed override ImmutableDictionary<Regex, TypeTag> Regexes { get; }
-
-        protected sealed override string ExtractType { get; } = Constants.SYS_NUM;
-
         public NumberExtractor(ChineseNumberExtractorMode mode = ChineseNumberExtractorMode.Default)
         {
             var builder = ImmutableDictionary.CreateBuilder<Regex, TypeTag>();
@@ -16,21 +30,16 @@ namespace Microsoft.Recognizers.Text.Number.Chinese
             // Add Cardinal
             var cardExtractChs = new CardinalExtractor(mode);
             builder.AddRange(cardExtractChs.Regexes);
-            
+
             // Add Fraction
             var fracExtractChs = new FractionExtractor();
             builder.AddRange(fracExtractChs.Regexes);
 
             Regexes = builder.ToImmutable();
         }
-    }
 
-    // These modes only apply to ChineseNumberExtractor.
-    // The default more urilizes an allow list to avoid extracting numbers in ambiguous/undesired combinations of Chinese ideograms.
-    // ExtractAll mode is to be used in cases where extraction should be more aggressive (e.g. in Units extraction).
-    public enum ChineseNumberExtractorMode
-    {
-        Default, // Number extraction with an allow list that filters what numbers to extract.
-        ExtractAll, // Extract all number-related terms aggressively.
+        internal sealed override ImmutableDictionary<Regex, TypeTag> Regexes { get; }
+
+        protected sealed override string ExtractType { get; } = Constants.SYS_NUM;
     }
 }
