9b0e40bc806750b87665fa6af0649c07ec4604ce
Cecilia Avila <cecilia.avila@southworks.com>
2019-01-07 13:56:57 -0300

    Fix all StyleCop warnings in English\	PercentageExtractor, EnglishNumberParserConfiguration and EnglishNumberRangeParserConfiguration
    - Reordered using statements, constructors, methods and properties.
    - Fixed spacing.

--- .NET/Microsoft.Recognizers.Text.Number/English/Extractors/PercentageExtractor.cs
+++ .NET/Microsoft.Recognizers.Text.Number/English/Extractors/PercentageExtractor.cs
@@ -8,14 +8,14 @@ namespace Microsoft.Recognizers.Text.Number.English
 {
     public sealed class PercentageExtractor : BasePercentageExtractor
     {
-        protected override NumberOptions Options { get; }
-
-        public PercentageExtractor(NumberOptions options = NumberOptions.None) : base(
-            NumberExtractor.GetInstance(options: options))
+        public PercentageExtractor(NumberOptions options = NumberOptions.None)
+            : base(NumberExtractor.GetInstance(options: options))
         {
             Options = options;
             Regexes = InitRegexes();
         }
+
+        protected override NumberOptions Options { get; }
 
         protected override ImmutableHashSet<Regex> InitRegexes()
         {
