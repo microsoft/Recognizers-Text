0b1d472f2fbbcb0266e264bfa888636f8c815178
Cecilia Avila <cecilia.avila@southworks.com>
2019-01-07 12:43:11 -0300

    Fix all StyleCop warnings in Dutch\CardinalExtractor and Dutch\DoubleExtractor
    - Reordered properties and methods.
    - Added spaces and trailing commas when needed.

--- .NET/Microsoft.Recognizers.Text.Number/Dutch/Extractors/CardinalExtractor.cs
+++ .NET/Microsoft.Recognizers.Text.Number/Dutch/Extractors/CardinalExtractor.cs
@@ -8,15 +8,29 @@ namespace Microsoft.Recognizers.Text.Number.Dutch
 {
     public class CardinalExtractor : BaseNumberExtractor
     {
+        private static readonly ConcurrentDictionary<string, CardinalExtractor> Instances = new ConcurrentDictionary<string, CardinalExtractor>();
+
+        private CardinalExtractor(string placeholder = NumbersDefinitions.PlaceHolderDefault)
+        {
+            var builder = ImmutableDictionary.CreateBuilder<Regex, TypeTag>();
+
+            // Add Integer Regexes
+            var intExtract = IntegerExtractor.GetInstance(placeholder);
+            builder.AddRange(intExtract.Regexes);
+
+            // Add Double Regexes
+            var douExtract = DoubleExtractor.GetInstance(placeholder);
+            builder.AddRange(douExtract.Regexes);
+
+            Regexes = builder.ToImmutable();
+        }
+
         internal sealed override ImmutableDictionary<Regex, TypeTag> Regexes { get; }
 
-        protected sealed override string ExtractType { get; } = Constants.SYS_NUM_CARDINAL; //"Cardinal";
-
-        private static readonly ConcurrentDictionary<string, CardinalExtractor> Instances = new ConcurrentDictionary<string, CardinalExtractor>();
+        protected sealed override string ExtractType { get; } = Constants.SYS_NUM_CARDINAL; // "Cardinal";
 
         public static CardinalExtractor GetInstance(string placeholder = NumbersDefinitions.PlaceHolderDefault)
         {
-
             if (!Instances.ContainsKey(placeholder))
             {
                 var instance = new CardinalExtractor(placeholder);
@@ -24,21 +38,6 @@ namespace Microsoft.Recognizers.Text.Number.Dutch
             }
 
             return Instances[placeholder];
-        }
-
-        private CardinalExtractor(string placeholder = NumbersDefinitions.PlaceHolderDefault)
-        {
-            var builder = ImmutableDictionary.CreateBuilder<Regex, TypeTag>();
-
-            //Add Integer Regexes
-            var intExtract = IntegerExtractor.GetInstance(placeholder);
-            builder.AddRange(intExtract.Regexes);
-
-            //Add Double Regexes
-            var douExtract = DoubleExtractor.GetInstance(placeholder);
-            builder.AddRange(douExtract.Regexes);
-
-            Regexes = builder.ToImmutable();
         }
     }
 }
