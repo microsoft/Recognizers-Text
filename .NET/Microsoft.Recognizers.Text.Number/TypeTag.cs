7a14ae9d7ae820bc1cd12e0a5564752bd8a99a0b
Billy Delgado <37461749+Bill7zz@users.noreply.github.com>
2019-01-07 10:34:13 -0300

    Fix extract class TypeTag of RegexTagGenerator file

---
+++ .NET/Microsoft.Recognizers.Text.Number/TypeTag.cs
@@ -0,0 +1,16 @@
+﻿namespace Microsoft.Recognizers.Text.Number
+{
+
+    public class TypeTag
+    {
+        public string Name { get; }
+
+        public int Priority { get; }
+
+        public TypeTag(string name, int priority)
+        {
+            Name = name;
+            Priority = priority;
+        }
+    }
+}
