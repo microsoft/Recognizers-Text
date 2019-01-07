8cfed4e70ae5d41f52744ac442820d76792edd29
Billy Delgado <37461749+Bill7zz@users.noreply.github.com>
2019-01-07 11:33:04 -0300

    Fix StyleCop warnings add emun documentation

--- .NET/Microsoft.Recognizers.Text.Number/NumberOptions.cs
+++ .NET/Microsoft.Recognizers.Text.Number/NumberOptions.cs
@@ -5,8 +5,19 @@ namespace Microsoft.Recognizers.Text.Number
     [Flags]
     public enum NumberOptions
     {
+        /// <summary>
+        /// Represents None
+        /// </summary>
         None = 0,
+
+        /// <summary>
+        /// Represents PercentageMode
+        /// </summary>
         PercentageMode = 1,
+
+        /// <summary>
+        /// Represents ExperimentalMode
+        /// </summary>
         ExperimentalMode = 2,
     }
 }
