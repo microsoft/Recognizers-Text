ba2b5f686928f7b697dc809a35187cd97bde6463
aliandi <mica.andini@gmail.com>
2019-01-07 15:27:21 -0300

    Fix all the StyleCop warnings in -Reorder fields, constructors and properties -Fix enum description style

--- .NET/Microsoft.Recognizers.Text.Number/Korean/Extractors/NumberExtractor.cs
+++ .NET/Microsoft.Recognizers.Text.Number/Korean/Extractors/NumberExtractor.cs
@@ -3,12 +3,27 @@ using System.Text.RegularExpressions;
 
 namespace Microsoft.Recognizers.Text.Number.Korean
 {
+     /// <summary>
+     /// These modes can be applied to KoreanNumberExtractor.
+     /// The default more urilizes an allow list to avoid extracting numbers in ambiguous/undesired combinations of Korean ideograms.
+     /// --> such as "십이지장(十二指腸)" is organ name(duodenum, part of small intestine) in Korean, should not be extracted.
+     /// ExtractAll mode is to be used in cases where extraction should be more aggressive (e.g. in Units extraction).
+     /// </summary>
+    public enum KoreanNumberExtractorMode
+    {
+        /// <summary>
+        /// Number extraction with an allow list that filters what numbers to extract.
+        /// </summary>
+        Default,
+
+        /// <summary>
+        /// Extract all number-related terms aggressively.
+        /// </summary>
+        ExtractAll,
+    }
+
     public class NumberExtractor : BaseNumberExtractor
     {
-        internal sealed override ImmutableDictionary<Regex, TypeTag> Regexes { get; }
-
-        protected sealed override string ExtractType { get; } = Constants.SYS_NUM;
-
         public NumberExtractor(KoreanNumberExtractorMode mode = KoreanNumberExtractorMode.Default)
         {
             var builder = ImmutableDictionary.CreateBuilder<Regex, TypeTag>();
@@ -23,15 +38,9 @@ namespace Microsoft.Recognizers.Text.Number.Korean
 
             Regexes = builder.ToImmutable();
         }
-    }
 
-    // These modes can be applied to KoreanNumberExtractor.
-    // The default more urilizes an allow list to avoid extracting numbers in ambiguous/undesired combinations of Korean ideograms.
-    // --> such as "십이지장(十二指腸)" is organ name(duodenum, part of small intestine) in Korean, should not be extracted.
-    // ExtractAll mode is to be used in cases where extraction should be more aggressive (e.g. in Units extraction).
-    public enum KoreanNumberExtractorMode
-    {
-        Default, // Number extraction with an allow list that filters what numbers to extract.
-        ExtractAll, // Extract all number-related terms aggressively.
+        internal sealed override ImmutableDictionary<Regex, TypeTag> Regexes { get; }
+
+        protected sealed override string ExtractType { get; } = Constants.SYS_NUM;
     }
 }
