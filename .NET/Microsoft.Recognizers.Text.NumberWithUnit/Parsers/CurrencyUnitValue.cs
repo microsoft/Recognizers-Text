f94c9e6978c648118d4c88273968752e782f0b59
aliandi <mica.andini@gmail.com>
2019-01-08 13:54:09 -0300

    Fix all the StyleCop warnings in CurrencyUnitValue

--- .NET/Microsoft.Recognizers.Text.NumberWithUnit/Parsers/CurrencyUnitValue.cs
+++ .NET/Microsoft.Recognizers.Text.NumberWithUnit/Parsers/CurrencyUnitValue.cs
@@ -1,7 +1,19 @@
-﻿namespace Microsoft.Recognizers.Text.NumberWithUnit
+﻿using System.Collections.Generic;
+
+using Microsoft.Recognizers.Text.NumberWithUnit.Utilities;
+
+namespace Microsoft.Recognizers.Text.NumberWithUnit
 {
-    public class CurrencyUnitValue : UnitValue
+    public class CurrencyUnitValue
     {
-        public string IsoCurrency { get; set; } = string.Empty;
+        private string number = string.Empty;
+        private string unit = string.Empty;
+        private string isoCurrency = string.Empty;
+
+        public string Number { get => number; set => number = value; }
+
+        public string Unit { get => unit; set => unit = value; }
+
+        public string IsoCurrency { get => isoCurrency; set => isoCurrency = value; }
     }
 }
