cd3b7d94d374db49d6f8c0aaa59435baae560454
aliandi <mica.andini@gmail.com>
2019-01-08 09:48:02 -0300

    Fix all the StyleCop warnings in IDateTimeParser -Add spaces to comments -Change "" to string.Empty -Add the file TimeTypeConstants with the class TimeTypeConstants -Add rule to ignore the underscore in the TimeTypeConstants variables names

--- .NET/Microsoft.Recognizers.Text.DateTime/Parsers/IDateTimeParser.cs
+++ .NET/Microsoft.Recognizers.Text.DateTime/Parsers/IDateTimeParser.cs
@@ -1,5 +1,4 @@
-﻿// ReSharper disable InconsistentNaming
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 using DateObject = System.DateTime;
 
@@ -11,29 +10,11 @@ namespace Microsoft.Recognizers.Text.DateTime
 
         List<DateTimeParseResult> FilterResults(string query, List<DateTimeParseResult> candidateResults);
     }
-    
+
     public class DateTimeParseResult : ParseResult
     {
-        //TimexStr is only used in extractors related with date and time
-        //It will output the TIMEX representation of a time string.
-        public string TimexStr { get; set; } = "";
-    }
-
-    public static class TimeTypeConstants
-    {
-        public const string DATE = "date";
-        public const string DATETIME = "dateTime";
-        public const string DATETIMEALT = "dateTimeAlt";
-        public const string DURATION = "duration";
-        public const string SET = "set";
-        public const string TIME = "time";
-
-        // Internal SubType for Future/Past in DateTimeResolutionResult
-        public const string START_DATE = "startDate";
-        public const string END_DATE = "endDate";
-        public const string START_DATETIME = "startDateTime";
-        public const string END_DATETIME = "endDateTime";
-        public const string START_TIME = "startTime";
-        public const string END_TIME = "endTime";
+        // TimexStr is only used in extractors related with date and time
+        // It will output the TIMEX representation of a time string.
+        public string TimexStr { get; set; } = string.Empty;
     }
 }
