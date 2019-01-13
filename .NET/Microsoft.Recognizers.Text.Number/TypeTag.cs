87d8bc736492e58715f52ebf39d9aff87da910e6
JuanAr <juan.arguello@southworks.com>
2019-01-13 04:31:45 -0300

    [StyleCop] Fix warnings TextNumber Options (#1277)
    * Fix StyleCop warnings in NumberOptions
    
    * Fix StyleCop warnings add emun documentation
    
    * Fix StyleCop warnings suppress message in Constants class
    
    * Fix justification in suppress message StyleCop warning
    
    * Fix StyleCop warnings in RegexTagGenerator
    
    * Fix extract class TypeTag of RegexTagGenerator file
    
    * Fix StyleCop warnings in TypeTag file
    
    * Fix StyleCop warnings in NumberRecognizer file
    
    * Fix StyleCop warnings in NumberMapGenerator file
    
    * Fix StyleCop warnings in AssemblyInfo
    -Remove trailing whitespace

---
+++ .NET/Microsoft.Recognizers.Text.Number/TypeTag.cs
@@ -0,0 +1,15 @@
+﻿namespace Microsoft.Recognizers.Text.Number
+{
+    public class TypeTag
+    {
+        public TypeTag(string name, int priority)
+        {
+            Name = name;
+            Priority = priority;
+        }
+
+        public string Name { get; }
+
+        public int Priority { get; }
+    }
+}
