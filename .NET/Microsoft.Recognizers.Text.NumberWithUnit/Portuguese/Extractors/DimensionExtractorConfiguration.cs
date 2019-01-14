79944f75cbd71a1c0c0f9ee83c8a7dcc53a50330
JuanAr <juan.arguello@southworks.com>
2019-01-14 00:45:04 -0300

    Fix StyleCop warnings in Portuguese\Extractors (#1295)
    - WeightExtractorConfiguration
    - VolumeExtractorConfiguration
    - TemperatureExtractorConfiguration
    - SpeedExtractorConfiguration
    - PortugueseNumberWithUnitExtractorConfiguration
    - LengthExtractorConfiguration
    - DimensionExtractorConfiguration
    - CurrencyExtractorConfiguration
    - AreaExtractorConfiguration
    - AgeExtractorConfiguration

--- .NET/Microsoft.Recognizers.Text.NumberWithUnit/Portuguese/Extractors/DimensionExtractorConfiguration.cs
+++ .NET/Microsoft.Recognizers.Text.NumberWithUnit/Portuguese/Extractors/DimensionExtractorConfiguration.cs
@@ -8,18 +8,6 @@ namespace Microsoft.Recognizers.Text.NumberWithUnit.Portuguese
 {
     public class DimensionExtractorConfiguration : PortugueseNumberWithUnitExtractorConfiguration
     {
-        public DimensionExtractorConfiguration() : base(new CultureInfo(Culture.Portuguese)) { }
-
-        public DimensionExtractorConfiguration(CultureInfo ci) : base(ci) { }
-
-        public override ImmutableDictionary<string, string> SuffixList => DimensionSuffixList;
-
-        public override ImmutableDictionary<string, string> PrefixList => null;
-
-        public override ImmutableList<string> AmbiguousUnitList => AmbiguousValues;
-
-        public override string ExtractType => Constants.SYS_UNIT_DIMENSION;
-
         public static readonly ImmutableDictionary<string, string> DimensionSuffixList = NumbersWithUnitDefinitions.InformationSuffixList
             .Concat(AreaExtractorConfiguration.AreaSuffixList)
             .Concat(LengthExtractorConfiguration.LengthSuffixList)
@@ -29,5 +17,23 @@ namespace Microsoft.Recognizers.Text.NumberWithUnit.Portuguese
             .ToImmutableDictionary(x => x.Key, x => x.Value);
 
         private static readonly ImmutableList<string> AmbiguousValues = NumbersWithUnitDefinitions.AmbiguousDimensionUnitList.ToImmutableList();
+
+        public DimensionExtractorConfiguration()
+                : base(new CultureInfo(Culture.Portuguese))
+        {
+        }
+
+        public DimensionExtractorConfiguration(CultureInfo ci)
+               : base(ci)
+        {
+        }
+
+        public override ImmutableDictionary<string, string> SuffixList => DimensionSuffixList;
+
+        public override ImmutableDictionary<string, string> PrefixList => null;
+
+        public override ImmutableList<string> AmbiguousUnitList => AmbiguousValues;
+
+        public override string ExtractType => Constants.SYS_UNIT_DIMENSION;
     }
 }
