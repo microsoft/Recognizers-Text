925be8ae2673137e512fdfeebf8584659b7a2447
JuanAr <juan.arguello@southworks.com>
2019-01-15 02:26:44 -0300

    [StyleCop] Fix all the warnings in DateTime files (#1305)
    * Fix StyleCop warnings in MatchingUtil
    
    * Fix StyleCop warnings in DurationParsingUtil
    
    * Fix StyleCop warnings in ConditionalMatch
    
    * Fix all StyleCop warnings in DateTimePeriodParser
    - Reordered using statements
    - Added spaces when needed
    
    * Fix StyleCop warnings in FullDateTimeParser
    
    * Fix StyleCop warnings in BaseHolidayParserConfiguration
    
    * Fix StyleCop warnings in BaseHolidayParser
    
    * Fix StyleCop warnings in BaseDurationParser
    
    * Revert "Fix all StyleCop warnings in DateTimePeriodParser"

--- .NET/Microsoft.Recognizers.Text.DateTime/Parsers/BaseHolidayParserConfiguration.cs
+++ .NET/Microsoft.Recognizers.Text.DateTime/Parsers/BaseHolidayParserConfiguration.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Text.RegularExpressions;
-using DateObject = System.DateTime;
-
 using Microsoft.Recognizers.Definitions;
+using DateObject = System.DateTime;
 
 namespace Microsoft.Recognizers.Text.DateTime
 {
     public abstract class BaseHolidayParserConfiguration : BaseOptionsConfiguration, IHolidayParserConfiguration
     {
+        protected BaseHolidayParserConfiguration(IOptionsConfiguration config)
+                : base(config)
+        {
+            this.VariableHolidaysTimexDictionary = BaseDateTime.VariableHolidaysTimexDictionary.ToImmutableDictionary();
+            this.HolidayFuncDictionary = InitHolidayFuncs().ToImmutableDictionary();
+        }
+
         public IImmutableDictionary<string, string> VariableHolidaysTimexDictionary { get; protected set; }
 
         public IImmutableDictionary<string, Func<int, DateObject>> HolidayFuncDictionary { get; protected set; }
@@ -19,29 +25,6 @@ namespace Microsoft.Recognizers.Text.DateTime
 
         public IEnumerable<Regex> HolidayRegexList { get; protected set; }
 
-        protected BaseHolidayParserConfiguration(IOptionsConfiguration config) : base(config)
-        {
-            this.VariableHolidaysTimexDictionary = BaseDateTime.VariableHolidaysTimexDictionary.ToImmutableDictionary();
-            this.HolidayFuncDictionary = InitHolidayFuncs().ToImmutableDictionary();
-        }
-
-        protected virtual IDictionary<string, Func<int, DateObject>> InitHolidayFuncs()
-        {
-            return new Dictionary<string, Func<int, DateObject>>
-            {
-                {"fathers", FathersDay},
-                {"mothers", MothersDay},
-                {"thanksgivingday", ThanksgivingDay},
-                {"thanksgiving", ThanksgivingDay},
-                {"martinlutherking", MartinLutherKingDay},
-                {"washingtonsbirthday", WashingtonsBirthday},
-                {"canberra", CanberraDay},
-                {"labour", LabourDay},
-                {"columbus", ColumbusDay},
-                {"memorial", MemorialDay}
-            };
-        }
-
         public abstract int GetSwiftYear(string text);
 
         public abstract string SanitizeHolidayToken(string holiday);
@@ -49,12 +32,6 @@ namespace Microsoft.Recognizers.Text.DateTime
         protected static DateObject MothersDay(int year) => DateObject.MinValue.SafeCreateFromValue(year, 5, GetDay(year, 5, 1, DayOfWeek.Sunday));
 
         protected static DateObject FathersDay(int year) => DateObject.MinValue.SafeCreateFromValue(year, 6, GetDay(year, 6, 2, DayOfWeek.Sunday));
-
-        private static DateObject MartinLutherKingDay(int year) => DateObject.MinValue.SafeCreateFromValue(year, 1, GetDay(year, 1, 2, DayOfWeek.Monday));
-
-        private static DateObject WashingtonsBirthday(int year) => DateObject.MinValue.SafeCreateFromValue(year, 2, GetDay(year, 2, 2, DayOfWeek.Monday));
-
-        private static DateObject CanberraDay(int year) => DateObject.MinValue.SafeCreateFromValue(year, 3, GetDay(year, 3, 0, DayOfWeek.Monday));
 
         protected static DateObject MemorialDay(int year) => DateObject.MinValue.SafeCreateFromValue(year, 5, GetLastDay(year, 5, DayOfWeek.Monday));
 
@@ -73,5 +50,28 @@ namespace Microsoft.Recognizers.Text.DateTime
             (from day in Enumerable.Range(1, DateObject.DaysInMonth(year, month))
              where DateObject.MinValue.SafeCreateFromValue(year, month, day).DayOfWeek == dayOfWeek
              select day).Last();
+
+        protected virtual IDictionary<string, Func<int, DateObject>> InitHolidayFuncs()
+        {
+            return new Dictionary<string, Func<int, DateObject>>
+            {
+                { "fathers", FathersDay },
+                { "mothers", MothersDay },
+                { "thanksgivingday", ThanksgivingDay },
+                { "thanksgiving", ThanksgivingDay },
+                { "martinlutherking", MartinLutherKingDay },
+                { "washingtonsbirthday", WashingtonsBirthday },
+                { "canberra", CanberraDay },
+                { "labour", LabourDay },
+                { "columbus", ColumbusDay },
+                { "memorial", MemorialDay },
+            };
+        }
+
+        private static DateObject MartinLutherKingDay(int year) => DateObject.MinValue.SafeCreateFromValue(year, 1, GetDay(year, 1, 2, DayOfWeek.Monday));
+
+        private static DateObject WashingtonsBirthday(int year) => DateObject.MinValue.SafeCreateFromValue(year, 2, GetDay(year, 2, 2, DayOfWeek.Monday));
+
+        private static DateObject CanberraDay(int year) => DateObject.MinValue.SafeCreateFromValue(year, 3, GetDay(year, 3, 0, DayOfWeek.Monday));
     }
 }
