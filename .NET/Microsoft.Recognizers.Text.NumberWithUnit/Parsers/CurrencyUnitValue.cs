5664b8af84996a0e38d3bf4c7dfcc390b4177b8a
JuanAr <juan.arguello@southworks.com>
2019-01-15 03:25:30 -0300

    [StyleCop] Fix all the warnings in NumberWithUnit Parsers (#1308)
    * Fix all the StyleCop warnings in BaseCurrencyParser
    
    Warnings:
    - SA1400: Class should declare an access modifier
    - SA1401: Field should be private
    - SA1122: Use string.Empty for empty strings
    - SA1028: Code should not contain trailing whitespace
    - SA1413: Use trailing comma in multi-line initializers
    - Reorder fields, properties and constructor.
    - Add spaces before comments
    
    * Fix all the StyleCop warnings in CurrencyUnitValue
    
    * Fix all the StyleCop warnings in Parsers
    Files:
        -BaseMergedUnitParser.cs
        -INumberWithUnitParserConfiguration.cs
    Warnings:
        -SA1400	Class should declare an access modifier
        -SA1401	Field should be private
        -SA1122	Use string.Empty for empty strings
    
    * Fix all the StyleCop warnings in NumberWithUnitParser
    
    Warnings:
        -SA1401	 Field should be private
        -SA1122	 Use string.Empty for empty strings
        -SA1124	Do not use regions
        -Add file with the class UnitValue
    
    * Rename Config to config to avoid breaking change

---
+++ .NET/Microsoft.Recognizers.Text.NumberWithUnit/Parsers/CurrencyUnitValue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.Recognizers.Text.NumberWithUnit.Utilities;
+
+namespace Microsoft.Recognizers.Text.NumberWithUnit
+{
+    public class CurrencyUnitValue
+    {
+        private string number = string.Empty;
+        private string unit = string.Empty;
+        private string isoCurrency = string.Empty;
+    }
+}
---
+++ .NET/Microsoft.Recognizers.Text.NumberWithUnit/Parsers/UnitValue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.Recognizers.Text.NumberWithUnit
+{
+    public class UnitValue
+    {
+        public string Number { get; set; } = string.Empty;
+
+        public string Unit { get; set; } = string.Empty;
+    }
+}
