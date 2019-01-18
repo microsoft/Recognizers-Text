7aea53097ee0cd8c09580e745bbebf62f8912399
aliandi <mica.andini@gmail.com>
2019-01-18 12:43:19 -0300

    [StyleCop] Fix all the warnings in Metadata - Add setters and getters to the fields - Remove spacing

--- .NET/Microsoft.Recognizers.Text/Extractors/Metadata.cs
+++ .NET/Microsoft.Recognizers.Text/Extractors/Metadata.cs
@@ -2,15 +2,14 @@
 
 namespace Microsoft.Recognizers.Text
 {
-
     public class Metadata
     {
         // For cases like "from 2014 to 2018", the period end "2018" could be inclusive or exclusive
         // For extraction, we only mark this flag to avoid future duplicate judgment, whether to include the period end or not is not determined in the extraction step
-        public bool PossiblyIncludePeriodEnd = false;
+        public bool PossiblyIncludePeriodEnd { get; set; } = false;
 
         // For cases like "2015年以前" (usually regards as "before 2015" in English), "5天以前" (usually regards as "5 days ago" in English) in Chinese, we need to decide whether this is a "Date with Mode" or "Duration with Before and After". We use this flag to avoid duplicate judgment both in the Extraction step and Parse step.
         // Currently, this flag is only used in Chinese DateTime as other languages don't have this ambiguity cases.
-        public bool IsDurationWithBeforeAndAfter = false;
+        public bool IsDurationWithBeforeAndAfter { get; set; } = false;
     }
 }
