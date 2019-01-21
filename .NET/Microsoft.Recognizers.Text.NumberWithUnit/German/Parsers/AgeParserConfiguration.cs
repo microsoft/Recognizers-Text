a323d50eae3279285c3cbe51d8341a0d0a938eb2
JuanAr <juan.arguello@southworks.com>
2019-01-20 23:03:58 -0300

    Fix all the StyleCop warnings in German Parsers (#1358)
    Files:
        -AgeParserConfiguration.cs
        -AreaParserConfiguration.cs
        -CurrencyParserConfiguration.cs
        -DimensionParserConfiguration.cs
        -ItalianNumberWithUnitParserConfiguration.cs
        -LengthParserConfiguration.cs
        -SpeedParserConfiguration.cs
        -TemperatureParserConfiguration.cs
        -VolumeParserConfiguration.cs
        -WeightParserConfiguration.cs
    Warnings:
        -SA1502: Element should not be on a single line - Move curly braces to different lines
        -SA1201: A field should not follow a property - Reorder fields and properties
        -SA1128: Put constructor initializers on their own line - Move constructor initializer to a different line

--- .NET/Microsoft.Recognizers.Text.NumberWithUnit/German/Parsers/AgeParserConfiguration.cs
+++ .NET/Microsoft.Recognizers.Text.NumberWithUnit/German/Parsers/AgeParserConfiguration.cs
@@ -4,9 +4,13 @@ namespace Microsoft.Recognizers.Text.NumberWithUnit.German
 {
     public class AgeParserConfiguration : GermanNumberWithUnitParserConfiguration
     {
-        public AgeParserConfiguration() : this(new CultureInfo(Culture.German)) { }
+        public AgeParserConfiguration()
+            : this(new CultureInfo(Culture.German))
+        {
+        }
 
-        public AgeParserConfiguration(CultureInfo ci) : base(ci)
+        public AgeParserConfiguration(CultureInfo ci)
+            : base(ci)
         {
             this.BindDictionary(AgeExtractorConfiguration.AgeSuffixList);
         }
--- .NET/Microsoft.Recognizers.Text.NumberWithUnit/German/Parsers/AreaParserConfiguration.cs
+++ .NET/Microsoft.Recognizers.Text.NumberWithUnit/German/Parsers/AreaParserConfiguration.cs
@@ -4,9 +4,13 @@ namespace Microsoft.Recognizers.Text.NumberWithUnit.German
 {
     public class AreaParserConfiguration : GermanNumberWithUnitParserConfiguration
     {
-        public AreaParserConfiguration() : this(new CultureInfo(Culture.German)) { }
+        public AreaParserConfiguration()
+            : this(new CultureInfo(Culture.German))
+        {
+        }
 
-        public AreaParserConfiguration(CultureInfo ci) : base(ci)
+        public AreaParserConfiguration(CultureInfo ci)
+            : base(ci)
         {
             this.BindDictionary(AreaExtractorConfiguration.AreaSuffixList);
         }
