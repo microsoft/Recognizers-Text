e443b703a38b00b7309abd77dee7cd65cfccb180
JuanAr <juan.arguello@southworks.com>
2019-01-22 04:25:08 -0300

    [StyleCop] Fix all the warnings in ChoiceConfig (#1348)
    * Fix StyleCop warnings in
    -ChoiceRecognizer
    -Constants
    -BooleanParserConfiguration
    
    * Fix all StyleCop warnings in ChoiseOption file.
    - Added enum comments.

--- .NET/Microsoft.Recognizers.Text.Choice/Config/BooleanParserConfiguration.cs
+++ .NET/Microsoft.Recognizers.Text.Choice/Config/BooleanParserConfiguration.cs
@@ -4,10 +4,10 @@ namespace Microsoft.Recognizers.Text.Choice
 {
     public class BooleanParserConfiguration : IChoiceParserConfiguration<bool>
     {
-        public static IDictionary<string, bool> Resolutions = new Dictionary<string, bool>
+        public static IDictionary<string, bool> Resolutions { get; set; } = new Dictionary<string, bool>
         {
             { Constants.SYS_BOOLEAN_TRUE, true },
-            { Constants.SYS_BOOLEAN_FALSE, false }
+            { Constants.SYS_BOOLEAN_FALSE, false },
         };
 
         IDictionary<string, bool> IChoiceParserConfiguration<bool>.Resolutions => Resolutions;
