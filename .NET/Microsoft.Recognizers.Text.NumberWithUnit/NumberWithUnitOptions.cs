fdbae9e52cdd186931f7ea69fbb0df15bb7fbba0
JuanAr <juan.arguello@southworks.com>
2019-01-22 21:31:59 -0300

    [StyleCop] Fix all warnings in the NumberWithUnit basic files (#1374)
    * Fix all the StyleCop warnings in NumberWithUnitRecognizer
    
    Warnings:
    - SA1009	Closing parenthesis should not be followed by a space
    - SA1012	Opening brace should be followed by a space.
    - SA1413	Use trailing comma in multi-line initializers
    - SA1202	'protected' members should come before 'private' members
    
    * Fix all the StyleCop warnings in CurrencyUnitValue
    
    * Fix all the StyleCop warnings in Parsers
    Files:
        -BaseMergedUnitParser.cs
        -INumberWithUnitParserConfiguration.cs
    Warnings:
        -SA1400	Class should declare an access modifier
        -SA1401	Field should be private
        -SA1122	Use string.Empty for empty strings
    
    * Fix StyleCop warnings in Constants file.
    
    * Fix all the StyleCop warnings in NumberWithUnitParser
    
    Warnings:
        -SA1401	 Field should be private
        -SA1122	 Use string.Empty for empty strings
        -SA1124	Do not use regions
        -Add file with the class UnitValue
    
    * Fix StyleCop warnings in NumberWithUnitOptions
    
    - Add emun documentation
    
    * Change config to Config
    
    * Apply feedback from PR

--- .NET/Microsoft.Recognizers.Text.NumberWithUnit/NumberWithUnitOptions.cs
+++ .NET/Microsoft.Recognizers.Text.NumberWithUnit/NumberWithUnitOptions.cs
@@ -5,6 +5,9 @@ namespace Microsoft.Recognizers.Text.NumberWithUnit
     [Flags]
     public enum NumberWithUnitOptions
     {
+        /// <summary>
+        /// Represents None
+        /// </summary>
         None = 0,
     }
 }
