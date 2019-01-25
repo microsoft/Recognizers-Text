e0c9867ca975676dfc726c1666d7406bec07bc69
Gaspar Acevedo Zain <43762887+GasparAcevedoZainSouthworks@users.noreply.github.com>
2019-01-24 23:38:37 -0300

    [StyleCop] Fix all the warnings in Definitions  (#1409)
    * Fix StyleCop warnings in Definition csproj
     -Add rulset reference
    
    * Fix StyleCop's warnings in AssemblyInfo.
    - Added header to the file
    
    * Fix all the warnings in DefinitionLoader
    - Fixed spacing

--- .NET/Microsoft.Recognizers.Definitions/Utilities/DefinitionLoader.cs
+++ .NET/Microsoft.Recognizers.Definitions/Utilities/DefinitionLoader.cs
@@ -5,25 +5,22 @@ namespace Microsoft.Recognizers.Definitions.Utilities
 {
     public static class DefinitionLoader
     {
-
         public static Dictionary<Regex, Regex> LoadAmbiguityFilters(Dictionary<string, string> filters)
         {
             var ambiguityFiltersDict = new Dictionary<Regex, Regex>();
 
-            if (filters != null) {
-
+            if (filters != null)
+            {
                 foreach (var item in filters)
                 {
                     if (!"null".Equals(item.Key))
                     {
-                        ambiguityFiltersDict.Add(new Regex(item.Key, RegexOptions.Singleline),
-                                                 new Regex(item.Value, RegexOptions.Singleline));
+                        ambiguityFiltersDict.Add(new Regex(item.Key, RegexOptions.Singleline), new Regex(item.Value, RegexOptions.Singleline));
                     }
                 }
             }
 
             return ambiguityFiltersDict;
         }
-
     }
 }
