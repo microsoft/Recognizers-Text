2d23d41e0888ed13871e37fdda68ba762f2c1787
Yuying Zhu <v-yuyzhu@microsoft.com>
2019-04-16 21:14:34 +0800

    Fix cases with arabic numerals (e.g.next 3) and remove flag checks (#1415)

--- .NET/Microsoft.Recognizers.Text.Number/Extractors/BaseNumberExtractor.cs
+++ .NET/Microsoft.Recognizers.Text.Number/Extractors/BaseNumberExtractor.cs
@@ -53,12 +53,6 @@ namespace Microsoft.Recognizers.Text.Number
                         continue;
                     }
 
-                    // In EnablePreview, cases like "last", "next" should not be skipped
-                    if ((Options & NumberOptions.EnablePreview) == 0 && IsRelativeOrdinal(m.Value))
-                    {
-                        continue;
-                    }
-
                     for (var j = 0; j < m.Length; j++)
                     {
                         matched[m.Index + j] = true;
