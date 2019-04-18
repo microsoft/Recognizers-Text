2ac0c24a12fce2831fd0dccb39c844b9448c4213
Yuying Zhu <v-yuyzhu@microsoft.com>
2019-04-18 14:23:52 +0800

    Change BR and US phonenumber regex

--- .NET/Microsoft.Recognizers.Definitions.Common/BasePhoneNumbers.cs
+++ .NET/Microsoft.Recognizers.Definitions.Common/BasePhoneNumbers.cs
@@ -23,10 +23,10 @@ namespace Microsoft.Recognizers.Definitions
 	{
 		public const string NumberReplaceToken = @"@builtin.phonenumber";
 		public const string GeneralPhoneNumberRegex = @"(\b(((\d[\s]?){4,12}))(-?[\d\s?]{3}\d)(?!-)\b)|(\(\d{5}\)\s?\d{5,6})|\+\d{2}\(\d\)\d{10}";
-		public const string BRPhoneNumberRegex = @"(((\B\(\s?))\d{2,3}(\s?\))|(\b\d{2,3}))\s?\d{4,5}-?\d{3,5}(?!-)\b";
+		public const string BRPhoneNumberRegex = @"((\(\s?(\+\s?|00)55\s?\)\s?)|(((?<!\d)\+\s?|\b00)55\s?)|\b)(((\B\(\s?))\d{2,3}(\s?\))|(\b\d{2,3}))\s?\d{4,5}-?\d{3,5}(?!-)\b";
 		public const string UKPhoneNumberRegex = @"(((\b(00)|\B\+)\s?)?(\b\d{2}\s?)?((\s?\(0\)[-\s]?|\b|(?<=(\b^#)\d{2}))\d{2,5}|\(0\d{3,4}\))[/-]?\s?(\d{5,8}|\d{3,4}[-\s]?\d{3,4})(?!-)\b)";
 		public const string DEPhoneNumberRegex = @"((\+\d{2}\s?((\(0\))?\d\s?)?|\b)(\d{2,4}\s?[-/]?[\s\d]{7,10}\d)(?!-)\b)";
-		public const string USPhoneNumberRegex = @"(((((\B\+)|\b)1(\s|-)?)|\b)?(\d{3}\)[-\s]?|\(\d{3}\)[-\s]?|\b\d{3}\s?[-\.]?\s?)|\b)[2-9]\d{2}\s?[-\.]?\s?\d{4}(\s?(x|X|ext)\s?\d{3,5})?(?!(-\s?\d))\b";
+		public const string USPhoneNumberRegex = @"(((((\B\+)|\b)1(\s|-)?)|\b)?(\d{3}\)[-\s]?|\(\d{3}\)[-\.\s]?|\b\d{3}\s?[-\.]?\s?)|\b)[2-9]\d{2}\s?[-\.]?\s?\d{4}(\s?(x|X|ext)\s?\d{3,5})?(?!(-\s?\d))\b";
 		public const string CNPhoneNumberRegex = @"((\b00\s?)?\+?86\s?-?\s?)?(((\b|(?<=86))\d{2,5}\s?-?\s?|\(\d{2,5}\)\s?)\d{4}\s?-?\s?\d{4}(\s?-?\s?\d{4})?|(\b|(?<=86))\d{3}\s?-?\s?\d{4}\s?-?\s?\d{4})(?!-)\b";
 		public const string DKPhoneNumberRegex = @"((\(\s?(\+\s?|00)45\s?\)\s?)|(((?<!\d)\+\s?|\b00)45\s?)|\b)(\s?\(0\)\s?)?((\d{8})|(\d{4}\s?-?\s?\d{4,6})|((\d{2}[\s-]){3}\d{2})|(\d{2}\s?-?\s?\d{3}\s?-?\s?\d{3}))(?!-)\b";
 		public const string ITPhoneNumberRegex = @"((\(\s?(\+\s?|00)39\s?\)\s?)|(((?<!\d)\+\s?|\b00)39\s?)|\b)((0[\d\s-]{4,12}\d)|(3[\d\s-]{7,12}\d))(?!-)\b";
