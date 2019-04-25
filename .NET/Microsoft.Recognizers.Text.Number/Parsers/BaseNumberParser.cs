0a47132277bee3a6dcbc81bc64aea98ab0332aa9
dimitar dimitrov <mitkodi@live.com>
2019-04-25 11:32:22 +0300

    - Added NotSupported attribute for javascript and java - Removed Bulgarian from the list of languages with compound numbers (more investigation is needed)

--- .NET/Microsoft.Recognizers.Text.Number/Parsers/BaseNumberParser.cs
+++ .NET/Microsoft.Recognizers.Text.Number/Parsers/BaseNumberParser.cs
@@ -15,7 +15,7 @@ namespace Microsoft.Recognizers.Text.Number
         private static readonly Regex MultiDecimalSeparatorCultureRegex =
             new Regex(@"^(en|es|fr|bg)(-)?\b", RegexOptions.Singleline | RegexOptions.Compiled);
 
-        private static readonly List<string> CompoundNumberLanguages = new List<string> { "de-DE", "nl-NL", "sv-SE", "bg-BG" };
+        private static readonly List<string> CompoundNumberLanguages = new List<string> { "de-DE", "nl-NL", "sv-SE" };
 
         private readonly bool isMultiDecimalSeparatorCulture = false;
 
