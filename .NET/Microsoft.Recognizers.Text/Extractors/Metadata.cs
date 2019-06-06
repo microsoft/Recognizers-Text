326e1eb352d9d92e43f3e7608077b111bd1a124d
Börje Karlsson <tellarin@gmail.com>
2019-06-06 11:07:23 +0800

    Minor declaration order change

--- .NET/Microsoft.Recognizers.Text/Extractors/Metadata.cs
+++ .NET/Microsoft.Recognizers.Text/Extractors/Metadata.cs
@@ -10,10 +10,11 @@
         // Currently, this flag is only used in Chinese DateTime as other languages don't have this ambiguity cases.
         public bool IsDurationWithBeforeAndAfter { get; set; } = false;
 
-        // For Ordinal
-        public bool IsOrdinalRelative { get; set; } = false;
-
+        // For Holiday cases as they are special cases of Date
         public bool IsHoliday { get; set; } = false;
+        
+        // For Ordinal.relative, a subtype of Ordinal
+        public bool IsOrdinalRelative { get; set; } = false;
 
         public string Offset { get; set; } = string.Empty;
 
