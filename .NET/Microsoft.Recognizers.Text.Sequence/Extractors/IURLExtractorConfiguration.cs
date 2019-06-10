a0f16745b00ee279f7c81e76dd6bbd695feb1112
Bo Li <306414383@qq.com>
2019-06-10 20:12:54 +0800

    Refactored .NET URL

---
+++ .NET/Microsoft.Recognizers.Text.Sequence/Extractors/IURLExtractorConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Microsoft.Recognizers.Text.Sequence
+{
+    public interface IURLExtractorConfiguration : ISequenceConfiguration
+    {
+        Regex IpUrlRegex { get; }
+
+        Regex UrlRegex { get; }
+
+        Regex UrlRegex2 { get; }
+    }
+
+}
