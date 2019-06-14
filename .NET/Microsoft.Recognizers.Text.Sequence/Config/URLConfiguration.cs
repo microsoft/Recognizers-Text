7273b131a5c704696c2f69d13e0d02b277dc1a5d
Bo Li <306414383@qq.com>
2019-06-14 19:04:19 +0800

    Fix URL issue in Japanese/Chinese (#1634)
    * Fix URL issue in CN/JP
    
    * Fix review comments

---
+++ .NET/Microsoft.Recognizers.Text.Sequence/Config/URLConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Microsoft.Recognizers.Text.Sequence
+{
+    public class URLConfiguration : ISequenceConfiguration
+    {
+        public URLConfiguration(SequenceOptions options = SequenceOptions.None)
+        {
+            Options = options;
+        }
+
+        public SequenceOptions Options { get; }
+
+        public Regex IpUrlRegex { get; set; }
+
+        public Regex UrlRegex { get; set; }
+
+    }
+}
--- JavaScript/packages/recognizers-sequence/src/recognizers-text-sequence.ts
+++ JavaScript/packages/recognizers-sequence/src/recognizers-text-sequence.ts
@@ -2,7 +2,8 @@ export { default as SequenceRecognizer, SequenceOptions, recognizePhoneNumber, r
 export { Culture } from "@microsoft/recognizers-text";
 export { AbstractSequenceModel, PhoneNumberModel, IpAddressModel, MentionModel, HashtagModel, EmailModel, URLModel, GUIDModel } from "./sequence/models";
 export { BaseSequenceExtractor, BasePhoneNumberExtractor, BaseIpExtractor, BaseMentionExtractor, BaseHashtagExtractor, BaseEmailExtractor, BaseURLExtractor, BaseGUIDExtractor } from "./sequence/extractors"
-export { PhoneNumberExtractor, IpExtractor, MentionExtractor, HashtagExtractor, EmailExtractor, URLExtractor, GUIDExtractor } from "./sequence/english/extractors";
+export { PhoneNumberExtractor, IpExtractor, MentionExtractor, HashtagExtractor, EmailExtractor, EnglishURLExtractorConfiguration, GUIDExtractor } from "./sequence/english/extractors";
+export { ChineseURLExtractorConfiguration } from "./sequence/chinese/extractors";
 export { BaseSequenceParser, BaseIpParser } from "./sequence/parsers"
 export { PhoneNumberParser, IpParser, MentionParser, HashtagParser, EmailParser, URLParser, GUIDParser } from "./sequence/english/parsers";
 export { BasePhoneNumbers } from "./resources/basePhoneNumbers";
