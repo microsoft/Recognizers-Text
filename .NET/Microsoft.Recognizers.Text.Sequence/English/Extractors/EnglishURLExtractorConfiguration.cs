640c4dfd0939b123fa1960d16bd80d0c36383b05
Bo Li <306414383@qq.com>
2019-06-14 19:04:19 +0800

    Fix URL issue in Japanese/Chinese (#1634)
    * Fix URL issue in CN/JP
    
    * Fix review comments

---
+++ .NET/Microsoft.Recognizers.Text.Sequence/English/Extractors/EnglishURLExtractorConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Recognizers.Definitions;
+
+namespace Microsoft.Recognizers.Text.Sequence.English
+{
+    public class EnglishURLExtractorConfiguration : URLConfiguration
+    {
+
+        public EnglishURLExtractorConfiguration(SequenceOptions options)
+            : base(options)
+        {
+            IpUrlRegex = new Regex(BaseURL.IpUrlRegex, RegexOptions.Compiled);
+            UrlRegex = new Regex(BaseURL.UrlRegex, RegexOptions.Compiled);
+        }
+
+    }
+}
