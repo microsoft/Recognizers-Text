aa90075a94912700798c82b8bf89b3b44f523890
Wenhao Song <32570730+songwenhao1@users.noreply.github.com>
2019-06-28 15:58:23 +0800

    Mitigate incorrect recognition for "from three thirty to four thirty" (#1674)

--- Java/libraries/recognizers-text-date-time/src/main/java/com/microsoft/recognizers/text/datetime/extractors/BaseDatePeriodExtractor.java
+++ Java/libraries/recognizers-text-date-time/src/main/java/com/microsoft/recognizers/text/datetime/extractors/BaseDatePeriodExtractor.java
@@ -401,6 +401,21 @@ public class BaseDatePeriodExtractor implements IDateTimeExtractor {
                 }
                 // Possibly include period end only apply for cases like "2014-2018", which are not single year cases
                 metadata.setPossiblyIncludePeriodEnd(false);
+            } else {
+                Match[] yearMatches = RegExpUtility.getMatches(config.getYearRegex(), match.value);
+                boolean isValidYear = true;
+                for (Match yearMatch : yearMatches) {
+                    int year = ((BaseDateExtractor)config.getDatePointExtractor()).getYearFromText(yearMatch);
+                    if (!(year >= Constants.MinYearNum && year <= Constants.MaxYearNum)) {
+                        isValidYear = false;
+                        break;
+                    }
+                }
+
+                if (!isValidYear) {
+                    continue;
+                }
+
             }
 
             results.add(new Token(match.index, match.index + match.length, metadata));
