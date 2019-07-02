3cf3693af945b964c3f18dfe3bce54fe0f4cc815
LionbridgeCS <LionbridgeCS@gmail.com>
2019-07-02 16:18:21 +0300

    Turkish NumberWithUnit tests

---
+++ .NET/Microsoft.Recognizers.Text.DataDrivenTests/NumberWithUnit/TestNumberWithUnit_Turkish.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Recognizers.Text.DataDrivenTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Recognizers.Text.NumberWithUnit.Tests
+{
+    [TestClass]
+    public class TestNumberWithUnit_Turkish : TestBase
+    {
+        public static TestResources TestResources { get; protected set; }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            TestResources = new TestResources();
+            TestResources.InitFromTestContext(context);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestSpecInitialize(TestResources);
+        }
+        
+        /* TO DO uncomment once Numbers changes are merged
+        * 
+        *[DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "AgeModel-Turkish.csv", "AgeModel-Turkish#csv", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void AgeModel()
+        {
+            TestNumberWithUnit();
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "CurrencyModel-Turkish.csv", "CurrencyModel-Turkish#csv", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void CurrencyModel()
+        {
+            TestNumberWithUnit();
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "DimensionModel-Turkish.csv", "DimensionModel-Turkish#csv", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void DimensionModel()
+        {
+            TestNumberWithUnit();
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "TemperatureModel-Turkish.csv", "TemperatureModel-Turkish#csv", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void TemperatureModel()
+        {
+            TestNumberWithUnit();
+        }*/
+    }
+}
