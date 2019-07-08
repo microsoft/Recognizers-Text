cada120fe227bd2c1077b5ed35c1843e1eab0bd3
LionbridgeCS <50670696+LionbridgeCS@users.noreply.github.com>
2019-07-08 17:06:43 +0300

    [.NET] Basic Turkish Numbers Support(Cardinal, Ordinal) (#1657)
    * [.NET] Turkish Numbers Support
    
    * fix for test cases
    
    * fix for test cases
    
    * revert ',' for percent model test cases
    
    * Updates to Turkish Numbers
    
    * removed duplicated cases
    
    * duplicate case in NumberModel
    
    * Updates to Numbers yaml, definitions and extractors. deactivate percent test.
    
    * unregistering percent model
    
    * fix for test case
    
    * updated NegativeNumberTermsRegex
    
    * Updates, adding test cases for OrdinalModel.
    
    * Update Turkish-Numbers.yaml

---
+++ .NET/Microsoft.Recognizers.Text.Number/Turkish/Extractors/PercentageExtractor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+
+using Microsoft.Recognizers.Definitions.Turkish;
+
+namespace Microsoft.Recognizers.Text.Number.Turkish
+{
+    public sealed class PercentageExtractor : BasePercentageExtractor
+    {
+        public PercentageExtractor(NumberOptions options = NumberOptions.None)
+            : base(NumberExtractor.GetInstance(options: options))
+        {
+            Options = options;
+            Regexes = InitRegexes();
+        }
+
+        protected override NumberOptions Options { get; }
+
+        protected override ImmutableHashSet<Regex> InitRegexes()
+        {
+            var regexStrs = new HashSet<string>
+            {
+                NumbersDefinitions.NumberWithPrefixPercentage,
+                NumbersDefinitions.NumberWithSuffixPercentage,
+            };
+
+            return BuildRegexes(regexStrs);
+        }
+    }
+}
