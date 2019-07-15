35118aa345da26b024c3bbf8423e204868ef81c0
LionbridgeCS <50670696+LionbridgeCS@users.noreply.github.com>
2019-07-15 09:09:26 +0300

    [.NET] Turkish NumbersWithUnit (Dimension) support (#1703)

---
+++ .NET/Microsoft.Recognizers.Text.NumberWithUnit/Turkish/Extractors/SpeedExtractorConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Immutable;
+using System.Globalization;
+
+using Microsoft.Recognizers.Definitions.Turkish;
+
+namespace Microsoft.Recognizers.Text.NumberWithUnit.Turkish
+{
+    public class SpeedExtractorConfiguration : TurkishNumberWithUnitExtractorConfiguration
+    {
+        public static readonly ImmutableDictionary<string, string> SpeedSuffixList = NumbersWithUnitDefinitions.SpeedSuffixList.ToImmutableDictionary();
+
+        public static readonly ImmutableDictionary<string, string> SpeedPrefixList = NumbersWithUnitDefinitions.SpeedPrefixList.ToImmutableDictionary();
+
+        public SpeedExtractorConfiguration()
+               : base(new CultureInfo(Culture.Turkish))
+        {
+        }
+
+        public SpeedExtractorConfiguration(CultureInfo ci)
+               : base(ci)
+        {
+        }
+
+        public override ImmutableDictionary<string, string> SuffixList => SpeedSuffixList;
+
+        public override ImmutableDictionary<string, string> PrefixList => SpeedPrefixList;
+
+        public override ImmutableList<string> AmbiguousUnitList => null;
+
+        public override string ExtractType => Constants.SYS_UNIT_SPEED;
+    }
+}
---
+++ .NET/Microsoft.Recognizers.Text.NumberWithUnit/Turkish/Extractors/VolumeExtractorConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Immutable;
+using System.Globalization;
+
+using Microsoft.Recognizers.Definitions.Turkish;
+
+namespace Microsoft.Recognizers.Text.NumberWithUnit.Turkish
+{
+    public class VolumeExtractorConfiguration : TurkishNumberWithUnitExtractorConfiguration
+    {
+        public static readonly ImmutableDictionary<string, string> VolumeSuffixList = NumbersWithUnitDefinitions.VolumeSuffixList.ToImmutableDictionary();
+
+        private static readonly ImmutableList<string> AmbiguousValues = NumbersWithUnitDefinitions.AmbiguousVolumeUnitList.ToImmutableList();
+
+        public VolumeExtractorConfiguration()
+               : this(new CultureInfo(Culture.Turkish))
+        {
+        }
+
+        public VolumeExtractorConfiguration(CultureInfo ci)
+               : base(ci)
+        {
+        }
+
+        public override ImmutableDictionary<string, string> SuffixList => VolumeSuffixList;
+
+        public override ImmutableDictionary<string, string> PrefixList => null;
+
+        public override ImmutableList<string> AmbiguousUnitList => AmbiguousValues;
+
+        public override string ExtractType => Constants.SYS_UNIT_VOLUME;
+    }
+}
