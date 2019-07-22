24fc7ceed617e0f05ac71e38c5c64043804c6b69
Sanxing Chen <mestayinloves@gmail.com>
2019-07-22 10:11:17 +0800

    Fix conflicts in NumberWithUnits resource files  (#1724)
    * Fix conflicts in NumberWithUnits resources files
    
    * Add checking code in C#
    
    * Update test specs
    
    * Re-write DimensionSuffixList pattern

--- JavaScript/packages/recognizers-number-with-unit/src/numberWithUnit/spanish/dimension.ts
+++ JavaScript/packages/recognizers-number-with-unit/src/numberWithUnit/spanish/dimension.ts
@@ -3,6 +3,16 @@ import { Constants } from "../constants";
 import { SpanishNumberWithUnitExtractorConfiguration, SpanishNumberWithUnitParserConfiguration } from "./base";
 import { SpanishNumericWithUnit } from "../../resources/spanishNumericWithUnit";
 
+const dimensionSuffixList = new Map<string, string>([
+    ...SpanishNumericWithUnit.InformationSuffixList,
+    ...SpanishNumericWithUnit.AreaSuffixList,
+    ...SpanishNumericWithUnit.LengthSuffixList,
+    ...SpanishNumericWithUnit.SpeedSuffixList,
+    ...SpanishNumericWithUnit.VolumeSuffixList,
+    ...SpanishNumericWithUnit.WeightSuffixList
+]);
+
+
 export class SpanishDimensionExtractorConfiguration extends SpanishNumberWithUnitExtractorConfiguration {
 
     readonly suffixList: ReadonlyMap<string, string>;
@@ -19,7 +29,7 @@ export class SpanishDimensionExtractorConfiguration extends SpanishNumberWithUni
 
         this.extractType = Constants.SYS_UNIT_DIMENSION;
 
-        this.suffixList = SpanishNumericWithUnit.DimensionSuffixList;
+        this.suffixList = dimensionSuffixList;
         this.prefixList = new Map<string, string>();
         this.ambiguousUnitList = SpanishNumericWithUnit.AmbiguousDimensionUnitList
     }
@@ -33,6 +43,6 @@ export class SpanishDimensionParserConfiguration extends SpanishNumberWithUnitPa
 
         super(ci);
 
-        this.BindDictionary(SpanishNumericWithUnit.DimensionSuffixList);
+        this.BindDictionary(dimensionSuffixList);
     }
 }
