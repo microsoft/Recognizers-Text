648071e0dddd9063a0f2877e560c734cea72beee
Mai <mailen.havela@southworks.com>
2019-08-02 17:28:42 -0300

    Change type of value to [srt]

--- Python/libraries/recognizers-text/recognizers_text/matcher/abstract_matcher.py
+++ Python/libraries/recognizers-text/recognizers_text/matcher/abstract_matcher.py
@@ -17,28 +17,28 @@ class Matcher(ABC):
 class AbstractMatcher(Matcher):
     @property
     @abstractmethod
-    def __init__(self, values: List[str], ids: List[str]):
+    def __init__(self, values: [str], ids: [str]):
         pass
 
     @property
     @abstractmethod
-    def find(self, query_text: List[str]) -> List[str]:
+    def find(self, query_text: [str]) -> [str]:
         pass
 
     @property
     @abstractmethod
-    def insert(self, value: List[str], id: str):
+    def insert(self, value: [str], id: str):
         pass
 
     @property
     @abstractmethod
-    def insert(self, value: List[str], id: str):
+    def insert(self, value: [str], id: str):
         pass
 
-    def is_match(self, query_text: List[str]):
+    def is_match(self, query_text: [str]):
         pass
 
-    def batch_insert(self, query_text: List[str], ids: str):
+    def batch_insert(self, query_text: [str], ids: str):
         pass
 
     def convert_dict_to_list(self, node):
