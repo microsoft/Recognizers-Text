9be37dafb5912fff6afe916815acf49cd790ca93
Luis Sergio <37625424+LCarbonellRod@users.noreply.github.com>
2019-08-06 17:49:27 -0300

    QuotesFixing

--- Python/tests/test_simple_tokenizer.py
+++ Python/tests/test_simple_tokenizer.py
@@ -15,7 +15,7 @@ class TestStringMatcher:
     @staticmethod
     def test_chinese_tokenized():
         tokenizer = SimpleTokenizer()
-        text = "你好，请给我一杯啤酒！"
+        text = '你好，请给我一杯啤酒！'
         tokenized_text = tokenizer.tokenize(text)
 
         assert "你" == tokenized_text[0].text
@@ -24,8 +24,8 @@ class TestStringMatcher:
     @staticmethod
     def test_mixed_tokenized():
         tokenizer = SimpleTokenizer()
-        text = "Hello，请给我1杯beer谢谢！"
+        text = 'Hello，请给我1杯beer谢谢！'
         tokenized_text = tokenizer.tokenize(text)
 
-        assert "Hello" == tokenized_text[0].text
+        assert 'Hello' == tokenized_text[0].text
         assert 11 == len(tokenized_text)
