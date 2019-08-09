e9bb91ed0be91fab94e59c5190425b198f4102db
aliandi <mica.andini@gmail.com>
2019-08-09 11:31:57 -0300

    Add matcher implementation and passing tests

---
+++ Python/libraries/recognizers-text/recognizers_text/Matcher/trie_tree.py
@@ -0,0 +1,57 @@
+from typing import List
+
+from recognizers_text import ModelResult
+
+from .node import Node
+from .abstract_matcher import AbstractMatcher
+from .match_result import MatchResult
+
+
+class TrieTree(AbstractMatcher):
+
+    def __init__(self):
+        self.__root = Node()
+
+    @property
+    def root(self) -> Node:
+        return self.__root
+
+    def insert(self, value: [], id: str) -> None:
+        node = self.root
+        for item in value:
+            child = node[item]
+
+            if child is None:
+                node[item] = Node()
+                child = node[item]
+
+            node = child
+
+        node.add_value(id)
+
+    def init(self, values: [], ids: []) -> None:
+        self.batch_insert(values, ids)
+        list(self.root)
+
+    def find(self, query_text: []) -> []:
+        query_array = query_text
+
+        for i in range(0, len(query_array)):
+            node = self.root
+
+            j = i
+
+            for j in range(j, len(query_array)):
+
+                if node.end:
+                    yield MatchResult(i, j - i, node.values)
+
+                if j == len(query_array):
+                    break
+
+                text = query_array[j]
+
+                if node[text] is None:
+                    break
+
+                node = node[text]
---
+++ Python/tests/matcher/test_string_matcher.py
@@ -0,0 +1,52 @@
+from recognizers_text.Matcher.string_matcher import StringMatcher
+from recognizers_text.Matcher.match_result import MatchResult
+
+
+class TestStringMatcher:
+
+    @staticmethod
+    def test_simple_string_matcher():
+        values = ["China", "Beijing", "City"]
+        string_matcher = StringMatcher()
+        string_matcher.init(values)
+        for value in values:
+            match = string_matcher.find(value)
+            if match is not None:
+                assert value == match.text()
+
+    @staticmethod
+    def test_simple_with_ids_string_matcher():
+        values = ["China", "Beijing", "City"]
+        ids = ["1", "2", "3"]
+        string_matcher = StringMatcher()
+        string_matcher.init(values, ids)
+        for i in range(0, len(values)):
+            value = values[i]
+            match = string_matcher.find(value)
+            if match is not None:
+                assert value == match.text()
+                assert ids[i] == match.canonical_values()
+
+    @staticmethod
+    def test_string_matcher():
+        utc_8_value = 'UTC+08:00'
+        utc_8_words = ['beijing time', 'chongqing time', 'hong kong time', 'urumqi time']
+        utc_2_value = 'UTC+02:00'
+        utc_2_words = ['cairo time', 'beirut time', 'gaza time', 'amman time']
+        value_dictionary = {utc_2_value: utc_2_words, utc_8_value: utc_8_words}
+        string_matcher = StringMatcher()
+        string_matcher.init(value_dictionary)
+
+        for value in utc_8_words:
+            sentence = 'please change {}, thanks'.format(value)
+            matches: [MatchResult] = string_matcher.find(sentence)
+            assert value == matches.text
+            assert utc_8_value == matches.canonical_values[0]
+            assert 14 == matches.start
+
+        for value in utc_2_words:
+            sentence = 'please change {}, thanks'.format(value)
+            matches: [MatchResult] = string_matcher.find(sentence)
+            assert value == matches.text
+            assert str(utc_2_value) == matches.canonical_values[0]
+            assert 14 == matches.start
