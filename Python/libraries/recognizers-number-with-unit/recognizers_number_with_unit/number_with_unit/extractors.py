684fe6966dfbb2e046ad8a5fac4000f9053196db
Luis Sergio <37625424+LCarbonellRod@users.noreply.github.com>
2019-08-14 17:21:54 -0300

    Update extractors.py

--- Python/libraries/recognizers-number-with-unit/recognizers_number_with_unit/number_with_unit/extractors.py
+++ Python/libraries/recognizers-number-with-unit/recognizers_number_with_unit/number_with_unit/extractors.py
@@ -464,8 +464,8 @@ class BaseMergedUnitExtractor(Extractor):
             middle_begin = ers[idx].start + ers[idx].length
             middle_end = ers[idx + 1].start
 
-            middle_str = source[middle_begin: middle_begin +(middle_end -
-                                middle_begin)].strip().lower()
+            middle_str = source[middle_begin: middle_begin + (middle_end -
+                                                              middle_begin)].strip().lower()
 
             # Separated by whitespace
             if not middle_str:
