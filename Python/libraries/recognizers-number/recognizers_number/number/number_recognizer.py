e7287e8205e98faf6e1f0df0f5e982b6c9bd9bf9
Luis Sergio <37625424+LCarbonellRod@users.noreply.github.com>
2019-08-27 17:59:55 -0300

    migration advances 5

--- Python/libraries/recognizers-number/recognizers_number/number/number_recognizer.py
+++ Python/libraries/recognizers-number/recognizers_number/number/number_recognizer.py
@@ -19,6 +19,10 @@ from recognizers_number.number.french.parsers import FrenchNumberParserConfigura
 
 class NumberOptions(IntFlag):
     NONE = 0
+    PERCENTAGE_MODE = 1
+    SUPPRESS_EXTENDED_TYPES = 2097152
+    EXPERIMENTAL_MODE = 4194304
+    ENABLE_PREVIEW = 8388608
 
 
 class NumberRecognizer(Recognizer[NumberOptions]):
