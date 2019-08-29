e58c64690aff872bd5e042e3ffd60dbaa62e43bb
LionbridgeCS <LionbridgeCS@Lionbridge.com>
2019-08-29 13:28:06 +0300

    [.NET] Hindi NumbersWithUnit code skeleton and initial specs

---
+++ .NET/Microsoft.Recognizers.Text.NumberWithUnit/Hindi/Extractors/LengthExtractorConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Immutable;
+using System.Globalization;
+
+using Microsoft.Recognizers.Definitions.Hindi;
+
+namespace Microsoft.Recognizers.Text.NumberWithUnit.Hindi
+{
+    public class LengthExtractorConfiguration : HindiNumberWithUnitExtractorConfiguration
+    {
+        public static readonly ImmutableDictionary<string, string> LengthSuffixList =
+            NumbersWithUnitDefinitions.LengthSuffixList.ToImmutableDictionary();
+
+        private static readonly ImmutableList<string> AmbiguousValues =
+            NumbersWithUnitDefinitions.AmbiguousLengthUnitList.ToImmutableList();
+
+        public LengthExtractorConfiguration()
+               : base(new CultureInfo(Culture.Hindi))
+        {
+        }
+
+        public LengthExtractorConfiguration(CultureInfo ci)
+               : base(ci)
+        {
+        }
+
+        public override ImmutableDictionary<string, string> SuffixList => LengthSuffixList;
+
+        public override ImmutableDictionary<string, string> PrefixList => null;
+
+        public override ImmutableList<string> AmbiguousUnitList => AmbiguousValues;
+
+        public override string ExtractType => Constants.SYS_UNIT_LENGTH;
+    }
+}
---
+++ .NET/Microsoft.Recognizers.Text.NumberWithUnit/Hindi/Extractors/SpeedExtractorConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Immutable;
+using System.Globalization;
+
+using Microsoft.Recognizers.Definitions.Hindi;
+
+namespace Microsoft.Recognizers.Text.NumberWithUnit.Hindi
+{
+    public class SpeedExtractorConfiguration : HindiNumberWithUnitExtractorConfiguration
+    {
+        public static readonly ImmutableDictionary<string, string> SpeedSuffixList =
+            NumbersWithUnitDefinitions.SpeedSuffixList.ToImmutableDictionary();
+
+        public SpeedExtractorConfiguration()
+               : base(new CultureInfo(Culture.Hindi))
+        {
+        }
+
+        public SpeedExtractorConfiguration(CultureInfo ci)
+               : base(ci)
+        {
+        }
+
+        public override ImmutableDictionary<string, string> SuffixList => SpeedSuffixList;
+
+        public override ImmutableDictionary<string, string> PrefixList => null;
+
+        public override ImmutableList<string> AmbiguousUnitList => null;
+
+        public override string ExtractType => Constants.SYS_UNIT_SPEED;
+    }
+}
