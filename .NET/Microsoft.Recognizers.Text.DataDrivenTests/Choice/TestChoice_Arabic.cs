bd98d4e8b487a4ce54b287e242c45c1576e1dc67
Ahmed Abuamra <ah.abuamra@gmail.com>
2019-09-03 10:23:10 +0200

    added arabic choice

---
+++ .NET/Microsoft.Recognizers.Text.DataDrivenTests/Choice/TestChoice_Arabic.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Recognizers.Text.DataDrivenTests.Choice
+{
+    [TestClass]
+    public class TestChoice_Arabic : TestBase
+    {
+        [NetCoreTestDataSource]
+        [TestMethod]
+        public void BooleanModel(TestModel testSpec)
+        {
+            TestChoice(testSpec);
+        }
+    }
+}
