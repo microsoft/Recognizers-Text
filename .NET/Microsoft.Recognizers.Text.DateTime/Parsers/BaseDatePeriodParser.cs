12ea7b05546da9eb3388eb909650408fb7d3123d
songwenhao1 <v-wesong@microsoft.com>
2019-09-08 22:06:56 +0800

    [DateTimeV2] Fix cases like "next monday to friday"

--- .NET/Microsoft.Recognizers.Text.DateTime/Parsers/BaseDatePeriodParser.cs
+++ .NET/Microsoft.Recognizers.Text.DateTime/Parsers/BaseDatePeriodParser.cs
@@ -1351,6 +1351,15 @@ namespace Microsoft.Recognizers.Text.DateTime
 
             if (er.Count >= 2)
             {
+                // Handle like "next monday to friday"
+                var futureMatchForStartDate = config.FutureRegex.Match(er[0].Text);
+                var futureMatchForEndDate = config.FutureRegex.Match(er[1].Text);
+
+                if (futureMatchForStartDate.Success && !futureMatchForEndDate.Success)
+                {
+                    er[1].Text = futureMatchForStartDate.Value + " " + er[1].Text;
+                }
+
                 var match = this.config.WeekWithWeekDayRangeRegex.Match(text);
                 string weekPrefix = null;
 
