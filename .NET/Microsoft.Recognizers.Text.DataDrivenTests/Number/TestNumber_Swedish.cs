d108fac36bcae7092d18fc2ca98b16f34a24eb83
Peter Rundqvist <47314226+pete1854@users.noreply.github.com>
2019-10-21 12:46:15 +0200

    Added all test methods. Leaving currently unused method commented out.

--- .NET/Microsoft.Recognizers.Text.DataDrivenTests/Number/TestNumber_Swedish.cs
+++ .NET/Microsoft.Recognizers.Text.DataDrivenTests/Number/TestNumber_Swedish.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Recognizers.Text.DataDrivenTests;
+using Microsoft.Recognizers.Text.DataDrivenTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Microsoft.Recognizers.Text.Number.Tests
@@ -6,6 +6,15 @@ namespace Microsoft.Recognizers.Text.Number.Tests
     [TestClass]
     public class TestNumber_Swedish : TestBase
     {
+        /*
+        [NetCoreTestDataSource]
+        [TestMethod]
+        public void OrdinalModelSuppressExtendedTypes(TestModel testSpec)
+        {
+            TestNumber(testSpec);
+        }
+        */
+
         [NetCoreTestDataSource]
         [TestMethod]
         public void NumberModel(TestModel testSpec)
@@ -13,11 +22,63 @@ namespace Microsoft.Recognizers.Text.Number.Tests
             TestNumber(testSpec);
         }
 
+        /*
+        [NetCoreTestDataSource]
+        [TestMethod]
+        public void NumberModelPercentMode(TestModel testSpec)
+        {
+            TestNumber(testSpec);
+        }
+        */
+
+        /*
+        [NetCoreTestDataSource]
+        [TestMethod]
+        public void NumberModelExperimentalMode(TestModel testSpec)
+        {
+            TestNumber(testSpec);
+        }
+        */
+
         [NetCoreTestDataSource]
         [TestMethod]
         public void OrdinalModel(TestModel testSpec)
         {
             TestNumber(testSpec);
         }
+
+        [NetCoreTestDataSource]
+        [TestMethod]
+        public void PercentModel(TestModel testSpec)
+        {
+            TestNumber(testSpec);
+        }
+
+        /*
+        [NetCoreTestDataSource]
+        [TestMethod]
+        public void PercentModelPercentMode(TestModel testSpec)
+        {
+            TestNumber(testSpec);
+        }
+        */
+
+        /*
+        [NetCoreTestDataSource]
+        [TestMethod]
+        public void NumberRangeModel(TestModel testSpec)
+        {
+            TestNumber(testSpec);
+        }
+        */
+
+        /*
+        [NetCoreTestDataSource]
+        [TestMethod]
+        public void NumberRangeModelExperimentalMode(TestModel testSpec)
+        {
+            TestNumber(testSpec);
+        }
+        */
     }
 }
