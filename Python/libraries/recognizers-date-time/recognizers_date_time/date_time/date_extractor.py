6d252a8f3f2960fde877781a897116f614e938f3
Giuliano Taliano <giuliano.taliano@southworks.com>
2019-11-15 13:18:15 -0300

    Minor changes in date_extractor.py

--- Python/libraries/recognizers-date-time/recognizers_date_time/date_time/date_extractor.py
+++ Python/libraries/recognizers-date-time/recognizers_date_time/date_time/date_extractor.py
@@ -1,10 +1,10 @@
 from abc import abstractmethod
-from typing import Pattern
+from typing import Match
 from recognizers_date_time.date_time.date_time_extractor import DateTimeExtractor
 
 
 class DateExtractor(DateTimeExtractor):
 
     @abstractmethod
-    def get_year_from_text(self, match: Pattern) -> int:
+    def get_year_from_text(self, match: Match) -> int:
         raise NotImplementedError
