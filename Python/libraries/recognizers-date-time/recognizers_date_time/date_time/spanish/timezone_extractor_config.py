e7f5bf71a5235eaaa70151b73de70b3e91e381cd
Giuliano Taliano <giuliano.taliano@southworks.com>
2020-01-02 10:18:25 -0300

    Remove properties initialization in TimeZone Spanish and French config files

--- Python/libraries/recognizers-date-time/recognizers_date_time/date_time/spanish/timezone_extractor_config.py
+++ Python/libraries/recognizers-date-time/recognizers_date_time/date_time/spanish/timezone_extractor_config.py
@@ -1,8 +1,6 @@
 from typing import List, Pattern
 from recognizers_text.matcher.string_matcher import StringMatcher
 from ..base_timezone import TimeZoneExtractorConfiguration
-from ...resources.english_time_zone import TimeZoneDefinitions
-from ..utilities import TimeZoneUtility, RegExpUtility
 
 
 class SpanishTimeZoneExtractorConfiguration(TimeZoneExtractorConfiguration):
@@ -25,13 +23,3 @@ class SpanishTimeZoneExtractorConfiguration(TimeZoneExtractorConfiguration):
     @property
     def ambiguous_timezone_list(self) -> List[str]:
         return self._ambiguous_timezone_list
-
-    def __init__(self):
-        super().__init__()
-        self._direct_utc_regex = RegExpUtility.get_safe_reg_exp(TimeZoneDefinitions.DirectUtcRegex)
-        self._abbreviations_list = list(TimeZoneDefinitions.AbbreviationsList)
-        self._full_name_list = list(TimeZoneDefinitions.FullNameList)
-        self._timezone_matcher = TimeZoneUtility.build_matcher_from_lists(self._full_name_list, self._abbreviations_list)
-        self._location_time_suffix_regex = RegExpUtility.get_safe_reg_exp(TimeZoneDefinitions.LocationTimeSuffixRegex)
-        self._location_matcher = StringMatcher()
-        self._ambiguous_timezone_list = list(TimeZoneDefinitions.AmbiguousTimezoneList)
