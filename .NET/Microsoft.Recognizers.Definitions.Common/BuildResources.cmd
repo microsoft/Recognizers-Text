7b753af99b11541a462447328b348d328d1c2523
Börje Karlsson <borjekar@microsoft.com>
2020-03-26 23:31:53 +0800

    Naive changes in .NET build for speed-up (#2058)

--- .NET/Microsoft.Recognizers.Definitions.Common/BuildResources.cmd
+++ .NET/Microsoft.Recognizers.Definitions.Common/BuildResources.cmd
@@ -1,6 +1,9 @@
 @ECHO off
 SETLOCAL EnableDelayedExpansion
 
+ECHO.
+ECHO # Building Resources
+
 SET COMMONASSEMBLYPATH=%1
 IF [%1] == [] SET COMMONASSEMBLYPATH=%~dp0bin\Release\net462\Microsoft.Recognizers.Definitions.Common.dll
 
@@ -13,10 +16,23 @@ ECHO.
 ECHO # Transform All T4 Templates
 
 ECHO.
-FOR /R %%i IN (*.tt) DO (ECHO # Transform %%i to %%~dpni.cs & dotnet tt "%%i" -o "%%~dpni.cs" -r "%COMMONASSEMBLYPATH%")
+FOR /R %%i IN (*.tt) DO ( CALL :COMPARER %%i %%~dpni)
 
 EXIT /b 0
+
+:COMPARER
+REM Workaround to issue with exitCode/ERRORLEVEL in previous FOR loop
+FOR /f "delims=" %%a in ('Powershell -ExecutionPolicy Bypass -Command "& {"..\buildtools\tsComparer.ps1" "%2.tt"}"') DO SET "RET=%%a"
+
+IF %RET% NEQ 0 (
+	ECHO # Transform %1 to %2.cs & dotnet tt "%1" -o "%2.cs" -r "%COMMONASSEMBLYPATH%"
+) ELSE (
+	ECHO # No need to re-generate %1
+)
+GOTO :EOF
 
 :ERROR
 ECHO Error found Transforming T4 Templates
 EXIT /b 1
+
+:EOF
