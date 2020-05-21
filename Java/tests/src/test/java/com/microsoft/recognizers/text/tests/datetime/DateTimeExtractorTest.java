7a814daec7a9ddb0df8362573b32420a52c25e11
Max Russek <maxwell.russek@workday.com>
2020-05-20 22:43:57 -0600

    Revert formatting

--- Java/tests/src/test/java/com/microsoft/recognizers/text/tests/datetime/DateTimeExtractorTest.java
+++ Java/tests/src/test/java/com/microsoft/recognizers/text/tests/datetime/DateTimeExtractorTest.java
@@ -1,15 +1,5 @@
 package com.microsoft.recognizers.text.tests.datetime;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Locale;
-import java.util.stream.IntStream;
-
-import org.javatuples.Pair;
-import org.junit.Assert;
-import org.junit.AssumptionViolatedException;
-import org.junit.runners.Parameterized;
-
 import com.microsoft.recognizers.text.Culture;
 import com.microsoft.recognizers.text.ExtractResult;
 import com.microsoft.recognizers.text.ModelResult;
@@ -29,6 +19,7 @@ import com.microsoft.recognizers.text.datetime.english.extractors.EnglishTimePer
 import com.microsoft.recognizers.text.datetime.english.extractors.EnglishTimeZoneExtractorConfiguration;
 import com.microsoft.recognizers.text.datetime.extractors.BaseDateExtractor;
 import com.microsoft.recognizers.text.datetime.extractors.BaseDatePeriodExtractor;
+import com.microsoft.recognizers.text.datetime.extractors.BaseDateTimeAltExtractor;
 import com.microsoft.recognizers.text.datetime.extractors.BaseDateTimeExtractor;
 import com.microsoft.recognizers.text.datetime.extractors.BaseDateTimePeriodExtractor;
 import com.microsoft.recognizers.text.datetime.extractors.BaseDurationExtractor;
@@ -41,6 +32,7 @@ import com.microsoft.recognizers.text.datetime.extractors.BaseTimeZoneExtractor;
 import com.microsoft.recognizers.text.datetime.extractors.IDateTimeExtractor;
 import com.microsoft.recognizers.text.datetime.french.extractors.FrenchDateExtractorConfiguration;
 import com.microsoft.recognizers.text.datetime.french.extractors.FrenchDatePeriodExtractorConfiguration;
+import com.microsoft.recognizers.text.datetime.french.extractors.FrenchDateTimeAltExtractorConfiguration;
 import com.microsoft.recognizers.text.datetime.french.extractors.FrenchDateTimeExtractorConfiguration;
 import com.microsoft.recognizers.text.datetime.french.extractors.FrenchDateTimePeriodExtractorConfiguration;
 import com.microsoft.recognizers.text.datetime.french.extractors.FrenchDurationExtractorConfiguration;
@@ -49,6 +41,7 @@ import com.microsoft.recognizers.text.datetime.french.extractors.FrenchMergedExt
 import com.microsoft.recognizers.text.datetime.french.extractors.FrenchSetExtractorConfiguration;
 import com.microsoft.recognizers.text.datetime.french.extractors.FrenchTimeExtractorConfiguration;
 import com.microsoft.recognizers.text.datetime.french.extractors.FrenchTimePeriodExtractorConfiguration;
+import com.microsoft.recognizers.text.datetime.french.extractors.FrenchTimeZoneExtractorConfiguration;
 import com.microsoft.recognizers.text.datetime.spanish.extractors.SpanishDateExtractorConfiguration;
 import com.microsoft.recognizers.text.datetime.spanish.extractors.SpanishDatePeriodExtractorConfiguration;
 import com.microsoft.recognizers.text.datetime.spanish.extractors.SpanishDateTimeExtractorConfiguration;
@@ -62,23 +55,37 @@ import com.microsoft.recognizers.text.datetime.spanish.extractors.SpanishTimePer
 import com.microsoft.recognizers.text.tests.AbstractTest;
 import com.microsoft.recognizers.text.tests.TestCase;
 
-public class DateTimeExtractorTest
-        extends AbstractTest {
+import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.IntStream;
+
+import org.javatuples.Pair;
+import org.junit.Assert;
+import org.junit.AssumptionViolatedException;
+import org.junit.runners.Parameterized;
+
+public class DateTimeExtractorTest extends AbstractTest {
 
     private static final String recognizerType = "DateTime";
-
-    public DateTimeExtractorTest(TestCase currentCase) {
-        super(currentCase);
-    }
 
     @Parameterized.Parameters(name = "{0}")
     public static Collection<TestCase> testCases() {
         return AbstractTest.enumerateTestCases(recognizerType, "Extractor");
     }
 
+    public DateTimeExtractorTest(TestCase currentCase) {
+        super(currentCase);
+    }
+
     @Override
     protected List<ModelResult> recognize(TestCase currentCase) {
         return null;
+    }
+
+    protected List<ExtractResult> extract(TestCase currentCase) {
+        IDateTimeExtractor extractor = getExtractor(currentCase);
+        return extractor.extract(currentCase.input.toLowerCase(Locale.ROOT), currentCase.getReferenceDateTime());
     }
 
     @Override
@@ -99,18 +106,10 @@ public class DateTimeExtractorTest
                     ExtractResult actual = t.getValue1();
 
                     Assert.assertEquals(getMessage(currentCase, "type"), expected.getType(), actual.getType());
-                    Assert.assertTrue(
-                            String.format("%s: Text was \"%s\", expected \"%s\"", getMessage(currentCase, "text"),
-                                    actual.getText(), expected.getText()),
-                            expected.getText().equalsIgnoreCase(actual.getText()));
+                    Assert.assertTrue(getMessage(currentCase, "text"), expected.getText().equalsIgnoreCase(actual.getText()));
                     Assert.assertEquals(getMessage(currentCase, "start"), expected.getStart(), actual.getStart());
                     Assert.assertEquals(getMessage(currentCase, "length"), expected.getLength(), actual.getLength());
                 });
-    }
-
-    protected List<ExtractResult> extract(TestCase currentCase) {
-        IDateTimeExtractor extractor = getExtractor(currentCase);
-        return extractor.extract(currentCase.input.toLowerCase(Locale.ROOT), currentCase.getReferenceDateTime());
     }
 
     public static IDateTimeExtractor getExtractor(TestCase currentCase) {
@@ -131,8 +130,7 @@ public class DateTimeExtractorTest
             default:
                 throw new AssumptionViolatedException("Extractor Type/Name not supported.");
             }
-        }
-        catch (IllegalArgumentException ex) {
+        } catch (IllegalArgumentException ex) {
             throw new AssumptionViolatedException(ex.getMessage(), ex);
         }
     }
@@ -141,36 +139,35 @@ public class DateTimeExtractorTest
 
         IOptionsConfiguration config = new BaseOptionsConfiguration();
         switch (name) {
-        case "DateExtractor":
-            return new BaseDateExtractor(new EnglishDateExtractorConfiguration(config));
-        case "DatePeriodExtractor":
-            return new BaseDatePeriodExtractor(new EnglishDatePeriodExtractorConfiguration(config));
-        //case "DateTimeAltExtractor":
-        //    return new BaseDateTimeAltExtractor(new EnglishDateTimeAltExtractorConfiguration());
-        case "DateTimeExtractor":
-            return new BaseDateTimeExtractor(new EnglishDateTimeExtractorConfiguration());
-        case "DateTimePeriodExtractor":
-            return new BaseDateTimePeriodExtractor(new EnglishDateTimePeriodExtractorConfiguration());
-        case "DurationExtractor":
-            return new BaseDurationExtractor(new EnglishDurationExtractorConfiguration());
-        case "HolidayExtractor":
-            return new BaseHolidayExtractor(new EnglishHolidayExtractorConfiguration());
-        case "MergedExtractor":
-            return new BaseMergedDateTimeExtractor(new EnglishMergedExtractorConfiguration(DateTimeOptions.None));
-        case "MergedExtractorSkipFromTo":
-            return new BaseMergedDateTimeExtractor(
-                    new EnglishMergedExtractorConfiguration(DateTimeOptions.SkipFromToMerge));
-        case "SetExtractor":
-            return new BaseSetExtractor(new EnglishSetExtractorConfiguration());
-        case "TimeExtractor":
-            return new BaseTimeExtractor(new EnglishTimeExtractorConfiguration());
-        case "TimePeriodExtractor":
-            return new BaseTimePeriodExtractor(new EnglishTimePeriodExtractorConfiguration());
-        case "TimeZoneExtractor":
-            return new BaseTimeZoneExtractor(new EnglishTimeZoneExtractorConfiguration(DateTimeOptions.EnablePreview));
+            case "DateExtractor":
+                return new BaseDateExtractor(new EnglishDateExtractorConfiguration(config));
+            case "DatePeriodExtractor":
+                return new BaseDatePeriodExtractor(new EnglishDatePeriodExtractorConfiguration(config));
+            //case "DateTimeAltExtractor":
+            //    return new BaseDateTimeAltExtractor(new EnglishDateTimeAltExtractorConfiguration());
+            case "DateTimeExtractor":
+                return new BaseDateTimeExtractor(new EnglishDateTimeExtractorConfiguration());
+            case "DateTimePeriodExtractor":
+                return new BaseDateTimePeriodExtractor(new EnglishDateTimePeriodExtractorConfiguration());
+            case "DurationExtractor":
+                return new BaseDurationExtractor(new EnglishDurationExtractorConfiguration());
+            case "HolidayExtractor":
+                return new BaseHolidayExtractor(new EnglishHolidayExtractorConfiguration());
+            case "MergedExtractor":
+                return new BaseMergedDateTimeExtractor(new EnglishMergedExtractorConfiguration(DateTimeOptions.None));
+            case "MergedExtractorSkipFromTo":
+                return new BaseMergedDateTimeExtractor(new EnglishMergedExtractorConfiguration(DateTimeOptions.SkipFromToMerge));
+            case "SetExtractor":
+                return new BaseSetExtractor(new EnglishSetExtractorConfiguration());
+            case "TimeExtractor":
+                return new BaseTimeExtractor(new EnglishTimeExtractorConfiguration());
+            case "TimePeriodExtractor":
+                return new BaseTimePeriodExtractor(new EnglishTimePeriodExtractorConfiguration());
+            case "TimeZoneExtractor":
+                return new BaseTimeZoneExtractor(new EnglishTimeZoneExtractorConfiguration(DateTimeOptions.EnablePreview));
 
-        default:
-            throw new AssumptionViolatedException("Extractor Type/Name not supported.");
+            default:
+                throw new AssumptionViolatedException("Extractor Type/Name not supported.");
         }
     }
 
@@ -178,65 +175,71 @@ public class DateTimeExtractorTest
 
         IOptionsConfiguration config = new BaseOptionsConfiguration();
         switch (name) {
-        case "DateExtractor":
-            return new BaseDateExtractor(new SpanishDateExtractorConfiguration(config));
-        case "DatePeriodExtractor":
-            return new BaseDatePeriodExtractor(new SpanishDatePeriodExtractorConfiguration(config));
-        //case "DateTimeAltExtractor":
-        //    return new BaseDateTimeAltExtractor(new SpanishDateTimeAltExtractorConfiguration());
-        case "DateTimeExtractor":
-            return new BaseDateTimeExtractor(new SpanishDateTimeExtractorConfiguration());
-        case "DateTimePeriodExtractor":
-            return new BaseDateTimePeriodExtractor(new SpanishDateTimePeriodExtractorConfiguration());
-        case "DurationExtractor":
-            return new BaseDurationExtractor(new SpanishDurationExtractorConfiguration());
-        case "HolidayExtractor":
-            return new BaseHolidayExtractor(new SpanishHolidayExtractorConfiguration());
-        case "MergedExtractor":
-            return new BaseMergedDateTimeExtractor(new SpanishMergedExtractorConfiguration(DateTimeOptions.None));
-        //case "MergedExtractorSkipFromTo":
-        //    return new BaseMergedDateTimeExtractor(new SpanishMergedExtractorConfiguration(DateTimeOptions.SkipFromToMerge));
-        case "SetExtractor":
-            return new BaseSetExtractor(new SpanishSetExtractorConfiguration());
-        case "TimeExtractor":
-            return new BaseTimeExtractor(new SpanishTimeExtractorConfiguration());
-        case "TimePeriodExtractor":
-            return new BaseTimePeriodExtractor(new SpanishTimePeriodExtractorConfiguration());
-        //case "TimeZoneExtractor":
-        //    return new BaseTimeZoneExtractor(new SpanishTimeZoneExtractorConfiguration(DateTimeOptions.EnablePreview));
+            case "DateExtractor":
+                return new BaseDateExtractor(new SpanishDateExtractorConfiguration(config));
+            case "DatePeriodExtractor":
+                return new BaseDatePeriodExtractor(new SpanishDatePeriodExtractorConfiguration(config));
+            //case "DateTimeAltExtractor":
+            //    return new BaseDateTimeAltExtractor(new SpanishDateTimeAltExtractorConfiguration());
+            case "DateTimeExtractor":
+                return new BaseDateTimeExtractor(new SpanishDateTimeExtractorConfiguration());
+            case "DateTimePeriodExtractor":
+                return new BaseDateTimePeriodExtractor(new SpanishDateTimePeriodExtractorConfiguration());
+            case "DurationExtractor":
+                return new BaseDurationExtractor(new SpanishDurationExtractorConfiguration());
+            case "HolidayExtractor":
+                return new BaseHolidayExtractor(new SpanishHolidayExtractorConfiguration());
+            case "MergedExtractor":
+               return new BaseMergedDateTimeExtractor(new SpanishMergedExtractorConfiguration(DateTimeOptions.None));
+            //case "MergedExtractorSkipFromTo":
+            //    return new BaseMergedDateTimeExtractor(new SpanishMergedExtractorConfiguration(DateTimeOptions.SkipFromToMerge));
+            case "SetExtractor":
+                return new BaseSetExtractor(new SpanishSetExtractorConfiguration());
+            case "TimeExtractor":
+                return new BaseTimeExtractor(new SpanishTimeExtractorConfiguration());
+            case "TimePeriodExtractor":
+                return new BaseTimePeriodExtractor(new SpanishTimePeriodExtractorConfiguration());
+            //case "TimeZoneExtractor":
+            //    return new BaseTimeZoneExtractor(new SpanishTimeZoneExtractorConfiguration(DateTimeOptions.EnablePreview));
 
-        default:
-            throw new AssumptionViolatedException("Extractor Type/Name not supported.");
+            default:
+                throw new AssumptionViolatedException("Extractor Type/Name not supported.");
         }
     }
 
-    public static IDateTimeExtractor getFrenchExtractor(String name) {
+    private static IDateTimeExtractor getFrenchExtractor(String name) {
 
         IOptionsConfiguration config = new BaseOptionsConfiguration();
         switch (name) {
-        case "DateExtractor":
-            return new BaseDateExtractor(new FrenchDateExtractorConfiguration(config));
-        case "DatePeriodExtractor":
-            return new BaseDatePeriodExtractor(new FrenchDatePeriodExtractorConfiguration(config));
-        case "DateTimeExtractor":
-            return new BaseDateTimeExtractor(new FrenchDateTimeExtractorConfiguration());
-        case "DateTimePeriodExtractor":
-            return new BaseDateTimePeriodExtractor(new FrenchDateTimePeriodExtractorConfiguration());
-        case "DurationExtractor":
-            return new BaseDurationExtractor(new FrenchDurationExtractorConfiguration());
-        case "HolidayExtractor":
-            return new BaseHolidayExtractor(new FrenchHolidayExtractorConfiguration());
-        case "MergedExtractor":
-            return new BaseMergedDateTimeExtractor(new FrenchMergedExtractorConfiguration(DateTimeOptions.None));
-        case "SetExtractor":
-            return new BaseSetExtractor(new FrenchSetExtractorConfiguration());
-        case "TimeExtractor":
-            return new BaseTimeExtractor(new FrenchTimeExtractorConfiguration());
-        case "TimePeriodExtractor":
-            return new BaseTimePeriodExtractor(new FrenchTimePeriodExtractorConfiguration());
+            case "DateExtractor":
+                return new BaseDateExtractor(new FrenchDateExtractorConfiguration(config));
+            case "DatePeriodExtractor":
+                return new BaseDatePeriodExtractor(new FrenchDatePeriodExtractorConfiguration(config));
+//            case "DateTimeAltExtractor":
+//                return new BaseDateTimeAltExtractor(new FrenchDateTimeAltExtractorConfiguration(config));
+            case "DateTimeExtractor":
+                return new BaseDateTimeExtractor(new FrenchDateTimeExtractorConfiguration());
+            case "DateTimePeriodExtractor":
+                return new BaseDateTimePeriodExtractor(new FrenchDateTimePeriodExtractorConfiguration());
+            case "DurationExtractor":
+                return new BaseDurationExtractor(new FrenchDurationExtractorConfiguration());
+            case "HolidayExtractor":
+                return new BaseHolidayExtractor(new FrenchHolidayExtractorConfiguration());
+            case "MergedExtractor":
+                return new BaseMergedDateTimeExtractor(new FrenchMergedExtractorConfiguration(DateTimeOptions.None));
+            case "MergedExtractorSkipFromTo":
+                return new BaseMergedDateTimeExtractor(new FrenchMergedExtractorConfiguration(DateTimeOptions.SkipFromToMerge));
+            case "SetExtractor":
+                return new BaseSetExtractor(new FrenchSetExtractorConfiguration());
+            case "TimeExtractor":
+                return new BaseTimeExtractor(new FrenchTimeExtractorConfiguration());
+            case "TimePeriodExtractor":
+                return new BaseTimePeriodExtractor(new FrenchTimePeriodExtractorConfiguration());
+            case "TimeZoneExtractor":
+                return new BaseTimeZoneExtractor(new FrenchTimeZoneExtractorConfiguration(DateTimeOptions.EnablePreview));
 
-        default:
-            throw new AssumptionViolatedException("Extractor Type/Name not supported.");
+            default:
+                throw new AssumptionViolatedException("Extractor Type/Name not supported.");
         }
     }
 }
