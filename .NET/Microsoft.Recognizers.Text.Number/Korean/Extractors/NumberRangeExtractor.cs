9b825bf7c10a41816ad8b8b7a000e21ae10728ce
neudurgeshp <49878420+neudurgeshp@users.noreply.github.com>
2020-05-27 19:11:47 +0530

    --NumberRange Extractor Enabled --Number Definitions --Spec

---
+++ .NET/Microsoft.Recognizers.Text.Number/Korean/Extractors/NumberRangeExtractor.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+using Microsoft.Recognizers.Definitions.Korean;
+
+namespace Microsoft.Recognizers.Text.Number.Korean
+{
+    public class NumberRangeExtractor : BaseNumberRangeExtractor
+    {
+
+        private const RegexOptions RegexFlags = RegexOptions.Singleline | RegexOptions.ExplicitCapture;
+
+        public NumberRangeExtractor(INumberOptionsConfiguration config)
+            : base(new NumberExtractor(),
+                   new OrdinalExtractor(),
+                   new BaseCJKNumberParser(new KoreanNumberParserConfiguration(config)),
+                   config)
+        {
+
+            var regexes = new Dictionary<Regex, string>
+            {
+                {
+                    // ...과...사이
+                    new Regex(NumbersDefinitions.TwoNumberRangeRegex1, RegexFlags),
+                    NumberRangeConstants.TWONUMBETWEEN
+                },
+                {
+                    // 이상...이하...
+
+                    new Regex(NumbersDefinitions.TwoNumberRangeRegex2, RegexFlags),
+                    NumberRangeConstants.TWONUM
+                },
+                {
+                    // 이하...이상...
+                    new Regex(NumbersDefinitions.TwoNumberRangeRegex3, RegexFlags),
+                    NumberRangeConstants.TWONUM
+                },
+                {
+                    // ...에서..., 20~30
+                    new Regex(NumbersDefinitions.TwoNumberRangeRegex4, RegexFlags),
+                    NumberRangeConstants.TWONUMTILL
+                },
+                {
+                    new Regex(NumbersDefinitions.TwoNumberRangeRegex5, RegexFlags),
+                    NumberRangeConstants.TWONUMTILL
+                },
+                {
+                    // ...이상|초과|많|높|크|더많|더높|더크|>
+                    new Regex(NumbersDefinitions.OneNumberRangeMoreRegex1, RegexFlags),
+                    NumberRangeConstants.MORE
+                },
+                {
+                     // ...이상|초과|많|높|크|더많|더높|더크|>
+                    new Regex(NumbersDefinitions.OneNumberRangeMoreRegex2, RegexFlags),
+                    NumberRangeConstants.MORE
+                },
+                {
+                    // >|≥...
+                    new Regex(NumbersDefinitions.OneNumberRangeMoreRegex3, RegexFlags),
+                    NumberRangeConstants.MORE
+                },
+                {
+                    new Regex(NumbersDefinitions.OneNumberRangeLessRegex1, RegexFlags),
+                    NumberRangeConstants.LESS
+                },
+                {
+                    new Regex(NumbersDefinitions.OneNumberRangeLessRegex2, RegexFlags),
+                    NumberRangeConstants.LESS
+                },
+                {
+                    // 까지최소|<|≤...
+                    new Regex(NumbersDefinitions.OneNumberRangeLessRegex3, RegexFlags),
+                    NumberRangeConstants.LESS
+                },
+                {
+                    new Regex(NumbersDefinitions.OneNumberRangeEqualRegex, RegexFlags),
+                    NumberRangeConstants.EQUAL
+                },
+        };
+
+            Regexes = regexes.ToImmutableDictionary();
+
+        }
+
+        internal sealed override ImmutableDictionary<Regex, string> Regexes { get; }
+
+        internal sealed override Regex AmbiguousFractionConnectorsRegex { get; }
+
+        protected sealed override string ExtractType { get; } = Constants.SYS_NUMRANGE;
+    }
+}
