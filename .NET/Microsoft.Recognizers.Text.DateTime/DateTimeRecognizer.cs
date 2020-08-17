7556fadda136f9354062ac76bf20ab13bcf00f28
Mick Vleeshouwer <mick@imick.nl>
2020-08-17 23:44:13 +0200

    Revert "Add test spec for Pinksteren"
    This reverts commit bafbbf50899c20e53d22c837d2bb2525a14c2920.

--- .NET/Microsoft.Recognizers.Text.DateTime/DateTimeRecognizer.cs
+++ .NET/Microsoft.Recognizers.Text.DateTime/DateTimeRecognizer.cs
@@ -129,13 +129,13 @@ namespace Microsoft.Recognizers.Text.DateTime
                         new HindiMergedExtractorConfiguration(new BaseDateTimeOptionsConfiguration(Culture.Hindi, options)))));
 
             // TODO to be uncommented when all tests for Dutch are green.
-            RegisterModel<DateTimeModel>(
-                Culture.Dutch,
-                options => new DateTimeModel(
-                    new BaseMergedDateTimeParser(
-                        new DutchMergedParserConfiguration(new BaseDateTimeOptionsConfiguration(Culture.Dutch, options))),
-                    new BaseMergedDateTimeExtractor(
-                        new DutchMergedExtractorConfiguration(new BaseDateTimeOptionsConfiguration(Culture.Dutch, options)))));
+            // RegisterModel<DateTimeModel>(
+            //     Culture.Dutch,
+            //     options => new DateTimeModel(
+            //         new BaseMergedDateTimeParser(
+            //             new DutchMergedParserConfiguration(new BaseDateTimeOptionsConfiguration(Culture.Dutch, options))),
+            //         new BaseMergedDateTimeExtractor(
+            //             new DutchMergedExtractorConfiguration(new BaseDateTimeOptionsConfiguration(Culture.Dutch, options)))));
 
             // TODO to be uncommented when all tests for Japanese are green.
             // RegisterModel<DateTimeModel>(
