b8167342f7e8f83bd81292e1e5aff08bd9907d6b
LionbridgeCS2 <v-Fabrizio.Sorba@lionbridge.com>
2020-09-02 14:05:15 +0300

    [FR DateTimeV2] Fix for hours duration wrongly recognized (#445)

--- .NET/Microsoft.Recognizers.Definitions.Common/French/DateTimeDefinitions.cs
+++ .NET/Microsoft.Recognizers.Definitions.Common/French/DateTimeDefinitions.cs
@@ -717,7 +717,7 @@ namespace Microsoft.Recognizers.Definitions.French
         };
       public static readonly Dictionary<string, string> AmbiguityTimeFiltersDict = new Dictionary<string, string>
         {
-            { @"heures?$", @"\b(pour|durée\s+de)\s+(\S+\s+){1,2}heures?\b" }
+            { @"heures?$", @"\b(pour|durée\s+de|pendant)\s+(\S+\s+){1,2}heures?\b" }
         };
       public static readonly IList<string> MorningTermList = new List<string>
         {
