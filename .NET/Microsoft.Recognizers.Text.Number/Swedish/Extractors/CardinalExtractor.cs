b9f7a6761851d1c633509435a331efc75a3faf2e
Peter Rundqvist <peter.rundqvist@simrishamn.se>
2020-11-13 15:12:42 +0100

    ok

--- .NET/Microsoft.Recognizers.Text.Number/Swedish/Extractors/CardinalExtractor.cs
+++ .NET/Microsoft.Recognizers.Text.Number/Swedish/Extractors/CardinalExtractor.cs
@@ -2,21 +2,18 @@
 using System.Collections.Immutable;
 using System.Text.RegularExpressions;
 
+using Microsoft.Recognizers.Definitions.Swedish;
+
 namespace Microsoft.Recognizers.Text.Number.Swedish
 {
-    public class CardinalExtractor : CachedNumberExtractor
+    public class CardinalExtractor : BaseNumberExtractor
     {
         private static readonly ConcurrentDictionary<string, CardinalExtractor> Instances =
             new ConcurrentDictionary<string, CardinalExtractor>();
 
-        private readonly string keyPrefix;
-
         private CardinalExtractor(BaseNumberOptionsConfiguration config)
             : base(config.Options)
         {
-
-            keyPrefix = string.Intern(ExtractType + "_" + config.Options + "_" + config.Placeholder + "_" + config.Culture);
-
             var builder = ImmutableDictionary.CreateBuilder<Regex, TypeTag>();
 
             // Add Integer Regexes
@@ -32,11 +29,11 @@ namespace Microsoft.Recognizers.Text.Number.Swedish
 
         internal sealed override ImmutableDictionary<Regex, TypeTag> Regexes { get; }
 
-        protected sealed override string ExtractType { get; } = Constants.SYS_NUM_CARDINAL; // "Cardinal";
+        // "Cardinal";
+        protected sealed override string ExtractType { get; } = Constants.SYS_NUM_CARDINAL;
 
         public static CardinalExtractor GetInstance(BaseNumberOptionsConfiguration config)
         {
-
             var extractorKey = config.Placeholder;
 
             if (!Instances.ContainsKey(extractorKey))
@@ -46,11 +43,6 @@ namespace Microsoft.Recognizers.Text.Number.Swedish
             }
 
             return Instances[extractorKey];
-        }
-
-        protected override object GenKey(string input)
-        {
-            return (keyPrefix, input);
         }
     }
 }
