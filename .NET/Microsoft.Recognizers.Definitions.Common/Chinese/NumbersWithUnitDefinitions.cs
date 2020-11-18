9acdef574bf73fbbdc3bc4ad9247584d5249ad0a
Yiying Wu <634436256@qq.com>
2020-11-18 14:12:03 +0000

    fıx Lıang Unit in Chinese

--- .NET/Microsoft.Recognizers.Definitions.Common/Chinese/NumbersWithUnitDefinitions.cs
+++ .NET/Microsoft.Recognizers.Definitions.Common/Chinese/NumbersWithUnitDefinitions.cs
@@ -608,14 +608,19 @@ namespace Microsoft.Recognizers.Definitions.Chinese
             { @"Ton", @"公吨|吨|t" },
             { @"Pound", @"磅" },
             { @"Ounce", @"盎司" },
+            { @"Liang", @"两" },
             { @"Bit", @"比特|位|b|bit" },
             { @"Kilobit", @"千比特|千位|kb|Kb" },
+            { @"Megabit", @"兆比特|兆位|mb|Mb" },
+            { @"Gigabit", @"十亿比特|千兆比特|十亿位|千兆位|gb|Gb" },
+            { @"Terabit", @"万亿比特|兆兆比特|万亿位|兆兆位|tb|Tb" },
+            { @"Petabit", @"千兆兆比特|千万亿比特|千兆兆位|千万亿位|pb|Pb" },
+            { @"Byte", @"字节|byte|Byte" },
+            { @"Kilobyte", @"千字节|kB|KB" },
             { @"Megabyte", @"兆字节|mB|MB" },
             { @"Gigabyte", @"十亿字节|千兆字节|gB|GB" },
             { @"Terabyte", @"万亿字节|兆兆字节|tB|TB" },
-            { @"Petabyte", @"千兆兆字节|千万亿字节|pB|PB" },
-            { @"Byte", @"字节|byte|Byte" },
-            { @"Kilobyte", @"千字节|kB|KB" }
+            { @"Petabyte", @"千兆兆字节|千万亿字节|pB|PB" }
         };
       public static readonly IList<string> DimensionAmbiguousValues = new List<string>
         {
