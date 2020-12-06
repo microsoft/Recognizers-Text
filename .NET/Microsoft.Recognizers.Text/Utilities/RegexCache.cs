f6d06f735a0ad4c42de007e15c6997b9f9007c25
rafael-aero <rafael@rafael.aero>
2020-12-06 15:11:58 +0100

    Add Regex cache class

---
+++ .NET/Microsoft.Recognizers.Text/Utilities/RegexCache.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+using System.Text.RegularExpressions;
+
+namespace Microsoft.Recognizers.Text
+{
+    public static class RegexCache
+    {
+        private static ConcurrentDictionary<(string pattern, RegexOptions options), Regex> _cache = new ConcurrentDictionary<(string pattern, RegexOptions options), Regex>();
+
+        public static bool Compiled { get; set; } = false;
+
+        public static Regex Get(string pattern, RegexOptions options)
+        {
+            if (Compiled)
+            {
+                return _cache.GetOrAdd((pattern, options), k => new Regex(k.pattern, k.options | RegexOptions.Compiled));
+            }
+            else
+            {
+                return _cache.GetOrAdd((pattern, options), k => new Regex(k.pattern, k.options));
+            }
+        }
+
+        public static Regex Get(string pattern) => Get(pattern, default);
+    }
+}
