701f1436fd477e47b1cdcb8ccd68c51ab9f50112
rafael-aero <rafael@rafael.aero>
2020-12-06 15:37:36 +0100

    Move DefinitionLoader to Text project to benefit from RegexCache

--- .NET/Microsoft.Recognizers.Text/Utilities/DefinitionLoader.cs
+++ .NET/Microsoft.Recognizers.Text/Utilities/DefinitionLoader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using Microsoft.Recognizers.Text;
 
 namespace Microsoft.Recognizers.Definitions.Utilities
 {
