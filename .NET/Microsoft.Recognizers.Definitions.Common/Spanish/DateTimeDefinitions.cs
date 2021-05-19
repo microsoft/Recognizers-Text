17e328911ce21031929cffb49259e85b047243a6
LionbridgeCS2 <v-Fabrizio.Sorba@lionbridge.com>
2021-05-19 07:25:58 +0100

    [ES DateTimeV2] Fixed 'mi próximo' incorrecly resolves to a date (#2597)

--- .NET/Microsoft.Recognizers.Definitions.Common/Spanish/DateTimeDefinitions.cs
+++ .NET/Microsoft.Recognizers.Definitions.Common/Spanish/DateTimeDefinitions.cs
@@ -604,7 +604,7 @@ namespace Microsoft.Recognizers.Definitions.Spanish
       public static readonly string[] DurationDateRestrictions = { @"hoy" };
       public static readonly Dictionary<string, string> AmbiguityFiltersDict = new Dictionary<string, string>
         {
-            { @"^mi$", @"\bmi\b" },
+            { @"^(este\s+)?mi(\s+([uú]ltimo|pasado|anterior|pr[oó]ximo|siguiente|que\s+viene))?$", @"\b(este\s+)?mi(\s+([uú]ltimo|pasado|anterior|pr[oó]ximo|siguiente|que\s+viene))?\b" },
             { @"^a[nñ]o$", @"(?<!el\s+)a[nñ]o" },
             { @"^semana$", @"(?<!la\s+)semana" },
             { @"^mes$", @"(?<!el\s+)mes" },
