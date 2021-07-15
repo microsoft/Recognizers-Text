ec20ad49db83bcc945c5c798ccdbe93f7f8d6e55
Börje F. Karlsson <borjekar@microsoft.com>
2021-07-15 19:04:38 +0800

    Add missing code file headers .NET (DateTime).

--- .NET/Microsoft.Recognizers.Text.DateTime/Extractors/IDateTimeListExtractor.cs
+++ .NET/Microsoft.Recognizers.Text.DateTime/Extractors/IDateTimeListExtractor.cs
@@ -1,4 +1,5 @@
-﻿// A type of Extractors receiving ExtractResult List as its input
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
 using System.Collections.Generic;
 
--- .NET/Microsoft.Recognizers.Text.DateTime/Hindi/Parsers/HindiDateTimeAltParserConfiguration.cs
+++ .NET/Microsoft.Recognizers.Text.DateTime/Hindi/Parsers/HindiDateTimeAltParserConfiguration.cs
@@ -1,4 +1,7 @@
-﻿namespace Microsoft.Recognizers.Text.DateTime.Hindi
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Recognizers.Text.DateTime.Hindi
 {
     public class HindiDateTimeAltParserConfiguration : IDateTimeAltParserConfiguration
     {
