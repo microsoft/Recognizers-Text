f98bb7ee0069de685124befbb8badf428b29c3d5
Nayer Wanas <nawanas@microsoft.com>
2021-12-19 21:36:27 +0200

    revertinhg an error in commit (#2809)

---
+++ .NET/Microsoft.Recognizers.Text.NumberWithUnit/Japanese/Extractors/TemperatureExtractorConfiguration.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Immutable;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+using Microsoft.Recognizers.Definitions;
+using Microsoft.Recognizers.Definitions.Japanese;
+
+namespace Microsoft.Recognizers.Text.NumberWithUnit.Japanese
+{
+    public class TemperatureExtractorConfiguration : JapaneseNumberWithUnitExtractorConfiguration
+    {
+
+        private const RegexOptions RegexFlags = RegexOptions.Singleline | RegexOptions.ExplicitCapture;
+
+        private static readonly Regex AmbiguousUnitMultiplierRegex =
+            new Regex(BaseUnits.AmbiguousUnitNumberMultiplierRegex, RegexFlags);
+
+        public TemperatureExtractorConfiguration()
+            : this(new CultureInfo(Culture.Japanese))
+        {
+        }
+
+        public TemperatureExtractorConfiguration(CultureInfo ci)
+            : base(ci)
+        {
+        }
+
+        public override ImmutableDictionary<string, string> SuffixList =>
+            NumbersWithUnitDefinitions.TemperatureSuffixList.ToImmutableDictionary();
+
+        public override ImmutableDictionary<string, string> PrefixList =>
+            NumbersWithUnitDefinitions.TemperaturePrefixList.ToImmutableDictionary();
+
+        public override ImmutableList<string> AmbiguousUnitList =>
+            NumbersWithUnitDefinitions.TemperatureAmbiguousValues.ToImmutableList();
+
+        public override string ExtractType => Constants.SYS_UNIT_TEMPERATURE;
+
+        public override Regex AmbiguousUnitNumberMultiplierRegex => AmbiguousUnitMultiplierRegex;
+    }
+}
