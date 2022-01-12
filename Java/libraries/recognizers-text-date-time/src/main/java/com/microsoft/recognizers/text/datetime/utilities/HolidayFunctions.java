764c67bec2bc156a21f921065ff3a9c61a6d19a8
Victor Hernandez <victor.hernandez@workday.com>
2022-01-12 17:46:56 -0500

    Fix: HolidayParserGer refactor into GermanHolidayParserConfiguration.

--- Java/libraries/recognizers-text-date-time/src/main/java/com/microsoft/recognizers/text/datetime/utilities/HolidayFunctions.java
+++ Java/libraries/recognizers-text-date-time/src/main/java/com/microsoft/recognizers/text/datetime/utilities/HolidayFunctions.java
@@ -12,16 +12,15 @@ public class HolidayFunctions {
     }
 
     public static LocalDateTime calculateHolidayByEaster(int year, int days) {
-
-        int day = 0;
+        int day;
         int month = 3;
 
         int g = year % 19;
         int c = year / 100;
-        int h = (c - (int)(c / 4) - (int)(((8 * c) + 13) / 25) + (19 * g) + 15) % 30;
-        int i = h - ((int)(h / 28) * (1 - ((int)(h / 28) * (int)(29 / (h + 1)) * (int)((21 - g) / 11))));
+        int h = (c - (c / 4) - (((8 * c) + 13) / 25) + (19 * g) + 15) % 30;
+        int i = h - ((h / 28) * (1 - ((h / 28) * (29 / (h + 1)) * ((21 - g) / 11))));
 
-        day = i - ((year + (int)(year / 4) + i + 2 - c + (int)(c / 4)) % 7) + 28;
+        day = i - ((year + (year / 4) + i + 2 - c + (c / 4)) % 7) + 28;
 
         if (day > 31) {
             month++;
@@ -29,6 +28,16 @@ public class HolidayFunctions {
         }
 
         return DateUtil.safeCreateFromMinValue(year, month, day).plusDays(days);
+    }
+
+    public static LocalDateTime calculateAdventDate(int year) {
+        return calculateAdventDate(year, 0);
+    }
+
+    public static LocalDateTime calculateAdventDate(int year, int days) {
+        LocalDateTime xmas = DateUtil.safeCreateFromMinValue(year, 12, 25);
+        int weekday = xmas.getDayOfWeek().getValue();
+        return xmas.minusDays(weekday).minusDays(days);
     }
 
 }
