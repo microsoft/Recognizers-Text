ad453ce3f925e00ab3d69c5cfe43566ed708fba2
Victor Hernandez <victor.hernandez@workday.com>
2022-01-12 16:21:13 -0500

    Fix: GermanHolidayParserConfiguration

--- Java/libraries/recognizers-text-date-time/src/main/java/com/microsoft/recognizers/text/datetime/german/parsers/GermanHolidayParserConfiguration.java
+++ Java/libraries/recognizers-text-date-time/src/main/java/com/microsoft/recognizers/text/datetime/german/parsers/GermanHolidayParserConfiguration.java
@@ -7,17 +7,22 @@ import com.google.common.collect.ImmutableMap;
 import com.microsoft.recognizers.text.datetime.german.extractors.GermanHolidayExtractorConfiguration;
 import com.microsoft.recognizers.text.datetime.parsers.BaseHolidayParserConfiguration;
 import com.microsoft.recognizers.text.datetime.resources.GermanDateTime;
-import com.microsoft.recognizers.text.datetime.utilities.DateUtil;
+import com.microsoft.recognizers.text.utilities.Match;
+import com.microsoft.recognizers.text.utilities.RegExpUtility;
 import com.microsoft.recognizers.text.utilities.StringUtility;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
-import java.util.function.IntFunction;
+import java.util.Optional;
+import java.util.regex.Pattern;
 
 public class GermanHolidayParserConfiguration extends BaseHolidayParserConfiguration {
+
+    private final Pattern thisPrefixRegex;
+    private final Pattern nextPrefixRegex;
+    private final Pattern previousPrefixRegex;
 
     public GermanHolidayParserConfiguration() {
 
@@ -32,177 +37,10 @@ public class GermanHolidayParserConfiguration extends BaseHolidayParserConfigura
             }
         }
         this.setHolidayNames(ImmutableMap.copyOf(newMap));
-    }
 
-    @Override
-    protected HashMap<String, IntFunction<LocalDateTime>> initHolidayFuncs() {
-
-        HashMap<String, IntFunction<LocalDateTime>> holidays = new HashMap<>(super.initHolidayFuncs());
-        holidays.put("mayday", GermanHolidayParserConfiguration::mayday);
-        holidays.put("yuandan", GermanHolidayParserConfiguration::newYear);
-        holidays.put("newyear", GermanHolidayParserConfiguration::newYear);
-        holidays.put("youthday", GermanHolidayParserConfiguration::youthDay);
-        holidays.put("girlsday", GermanHolidayParserConfiguration::girlsDay);
-        holidays.put("xmas", GermanHolidayParserConfiguration::christmasDay);
-        holidays.put("newyearday", GermanHolidayParserConfiguration::newYear);
-        holidays.put("aprilfools", GermanHolidayParserConfiguration::foolDay);
-        holidays.put("easterday", GermanHolidayParserConfiguration::easterDay);
-        holidays.put("newyearsday", GermanHolidayParserConfiguration::newYear);
-        holidays.put("femaleday", GermanHolidayParserConfiguration::femaleDay);
-        holidays.put("singleday", GermanHolidayParserConfiguration::singlesDay);
-        holidays.put("newyeareve", GermanHolidayParserConfiguration::newYearEve);
-        holidays.put("arborday", GermanHolidayParserConfiguration::treePlantDay);
-        holidays.put("loverday", GermanHolidayParserConfiguration::valentinesDay);
-        holidays.put("christmas", GermanHolidayParserConfiguration::christmasDay);
-        holidays.put("teachersday", GermanHolidayParserConfiguration::teacherDay);
-        holidays.put("stgeorgeday", GermanHolidayParserConfiguration::stGeorgeDay);
-        holidays.put("baptisteday", GermanHolidayParserConfiguration::baptisteDay);
-        holidays.put("bastilleday", GermanHolidayParserConfiguration::bastilleDay);
-        holidays.put("allsoulsday", GermanHolidayParserConfiguration::allSoulsDay);
-        holidays.put("veteransday", GermanHolidayParserConfiguration::veteransDay);
-        holidays.put("childrenday", GermanHolidayParserConfiguration::childrenDay);
-        holidays.put("maosbirthday", GermanHolidayParserConfiguration::maoBirthday);
-        holidays.put("allsaintsday", GermanHolidayParserConfiguration::halloweenDay);
-        holidays.put("stpatrickday", GermanHolidayParserConfiguration::stPatrickDay);
-        holidays.put("halloweenday", GermanHolidayParserConfiguration::halloweenDay);
-        holidays.put("allhallowday", GermanHolidayParserConfiguration::allHallowDay);
-        holidays.put("guyfawkesday", GermanHolidayParserConfiguration::guyFawkesDay);
-        holidays.put("christmaseve", GermanHolidayParserConfiguration::christmasEve);
-        holidays.put("groundhougday", GermanHolidayParserConfiguration::groundhogDay);
-        holidays.put("whiteloverday", GermanHolidayParserConfiguration::whiteLoverDay);
-        holidays.put("valentinesday", GermanHolidayParserConfiguration::valentinesDay);
-        holidays.put("treeplantingday", GermanHolidayParserConfiguration::treePlantDay);
-        holidays.put("cincodemayoday", GermanHolidayParserConfiguration::cincoDeMayoDay);
-        holidays.put("inaugurationday", GermanHolidayParserConfiguration::inaugurationDay);
-        holidays.put("independenceday", GermanHolidayParserConfiguration::usaIndependenceDay);
-        holidays.put("usindependenceday", GermanHolidayParserConfiguration::usaIndependenceDay);
-        holidays.put("juneteenth", GermanHolidayParserConfiguration::juneteenth);
-
-        return holidays;
-    }
-
-    private static LocalDateTime easterDay(int year) {
-        return DateUtil.minValue();
-    }
-
-    private static LocalDateTime mayday(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 5, 1);
-    }
-
-    private static LocalDateTime newYear(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 1, 1);
-    }
-
-    private static LocalDateTime foolDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 4, 1);
-    }
-
-    private static LocalDateTime girlsDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 3, 7);
-    }
-
-    private static LocalDateTime youthDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 5, 4);
-    }
-
-    private static LocalDateTime femaleDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 3, 8);
-    }
-
-    private static LocalDateTime childrenDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 6, 1);
-    }
-
-    private static LocalDateTime teacherDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 9, 10);
-    }
-
-    private static LocalDateTime groundhogDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 2, 2);
-    }
-
-    private static LocalDateTime stGeorgeDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 4, 23);
-    }
-
-    private static LocalDateTime baptisteDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 6, 24);
-    }
-
-    private static LocalDateTime bastilleDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 7, 14);
-    }
-
-    private static LocalDateTime allSoulsDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 11, 2);
-    }
-
-    private static LocalDateTime singlesDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 11, 11);
-    }
-
-    private static LocalDateTime newYearEve(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 12, 31);
-    }
-
-    private static LocalDateTime treePlantDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 3, 12);
-    }
-
-    private static LocalDateTime stPatrickDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 3, 17);
-    }
-
-    private static LocalDateTime allHallowDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 11, 1);
-    }
-
-    private static LocalDateTime guyFawkesDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 11, 5);
-    }
-
-    private static LocalDateTime veteransDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 11, 11);
-    }
-
-    private static LocalDateTime maoBirthday(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 12, 26);
-    }
-
-    private static LocalDateTime valentinesDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 2, 14);
-    }
-
-    private static LocalDateTime whiteLoverDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 3, 14);
-    }
-
-    private static LocalDateTime cincoDeMayoDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 5, 5);
-    }
-
-    private static LocalDateTime halloweenDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 10, 31);
-    }
-
-    private static LocalDateTime christmasEve(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 12, 24);
-    }
-
-    private static LocalDateTime christmasDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 12, 25);
-    }
-
-    private static LocalDateTime inaugurationDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 1, 20);
-    }
-
-    private static LocalDateTime usaIndependenceDay(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 7, 4);
-    }
-
-    private static LocalDateTime juneteenth(int year) {
-        return DateUtil.safeCreateFromMinValue(year, 6, 19);
+        thisPrefixRegex = RegExpUtility.getSafeRegExp(GermanDateTime.ThisPrefixRegex);
+        nextPrefixRegex = RegExpUtility.getSafeRegExp(GermanDateTime.NextPrefixRegex);
+        previousPrefixRegex = RegExpUtility.getSafeRegExp(GermanDateTime.PreviousPrefixRegex);
     }
 
     @Override
@@ -210,12 +48,15 @@ public class GermanHolidayParserConfiguration extends BaseHolidayParserConfigura
 
         String trimmedText = StringUtility.trimStart(StringUtility.trimEnd(text)).toLowerCase(Locale.ROOT);
         int swift = -10;
+        Optional<Match> matchNextPrefixRegex = Arrays.stream(RegExpUtility.getMatches(nextPrefixRegex, trimmedText)).findFirst();
+        Optional<Match> matchPreviousPrefixRegex = Arrays.stream(RegExpUtility.getMatches(previousPrefixRegex, trimmedText)).findFirst();
+        Optional<Match> matchThisPrefixRegex = Arrays.stream(RegExpUtility.getMatches(thisPrefixRegex, trimmedText)).findFirst();
 
-        if (trimmedText.startsWith("next")) {
+        if (matchNextPrefixRegex.isPresent()) {
             swift = 1;
-        } else if (trimmedText.startsWith("last")) {
+        } else if (matchPreviousPrefixRegex.isPresent()) {
             swift = -1;
-        } else if (trimmedText.startsWith("this")) {
+        } else if (matchThisPrefixRegex.isPresent()) {
             swift = 0;
         }
 
@@ -223,6 +64,8 @@ public class GermanHolidayParserConfiguration extends BaseHolidayParserConfigura
     }
 
     public String sanitizeHolidayToken(String holiday) {
-        return holiday.replace(" ", "").replace("'", "");
+        return holiday.replace(" ", "")
+                .replace("-", "")
+                .replace("'", "");
     }
 }
