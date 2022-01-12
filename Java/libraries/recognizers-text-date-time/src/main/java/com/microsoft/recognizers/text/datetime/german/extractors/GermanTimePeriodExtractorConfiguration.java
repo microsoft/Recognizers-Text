1370727edd0a031a7a4d1c5320c0c542063b8d80
Max Russek <maxwell.russek@workday.com>
2022-01-12 12:10:32 -0700

    GermanTimePeriodExtractorConfiguration

--- Java/libraries/recognizers-text-date-time/src/main/java/com/microsoft/recognizers/text/datetime/german/extractors/GermanTimePeriodExtractorConfiguration.java
+++ Java/libraries/recognizers-text-date-time/src/main/java/com/microsoft/recognizers/text/datetime/german/extractors/GermanTimePeriodExtractorConfiguration.java
@@ -3,6 +3,7 @@
 
 package com.microsoft.recognizers.text.datetime.german.extractors;
 
+import com.microsoft.recognizers.text.ExtractResult;
 import com.microsoft.recognizers.text.IExtractor;
 import com.microsoft.recognizers.text.datetime.DateTimeOptions;
 import com.microsoft.recognizers.text.datetime.config.BaseOptionsConfiguration;
@@ -15,36 +16,44 @@ import com.microsoft.recognizers.text.datetime.german.utilities.GermanDatetimeUt
 import com.microsoft.recognizers.text.datetime.resources.GermanDateTime;
 import com.microsoft.recognizers.text.datetime.utilities.IDateTimeUtilityConfiguration;
 import com.microsoft.recognizers.text.number.german.extractors.IntegerExtractor;
+import com.microsoft.recognizers.text.utilities.Match;
 import com.microsoft.recognizers.text.utilities.RegExpUtility;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class GermanTimePeriodExtractorConfiguration extends BaseOptionsConfiguration implements ITimePeriodExtractorConfiguration {
 
-    private String tokenBeforeDate;
-
-    public final String getTokenBeforeDate() {
-        return tokenBeforeDate;
-    }
-
-    public static final Pattern AmRegex = RegExpUtility.getSafeRegExp(GermanDateTime.AmRegex);
-    public static final Pattern PmRegex = RegExpUtility.getSafeRegExp(GermanDateTime.PmRegex);
-    public static final Pattern HourRegex = RegExpUtility.getSafeRegExp(GermanDateTime.HourRegex);
     public static final Pattern TillRegex = RegExpUtility.getSafeRegExp(GermanDateTime.TillRegex);
-    public static final Pattern PeriodDescRegex = RegExpUtility.getSafeRegExp(GermanDateTime.DescRegex);
-    public static final Pattern PureNumFromTo = RegExpUtility.getSafeRegExp(GermanDateTime.PureNumFromTo);
-    public static final Pattern TimeUnitRegex = RegExpUtility.getSafeRegExp(GermanDateTime.TimeUnitRegex);
-    public static final Pattern TimeOfDayRegex = RegExpUtility.getSafeRegExp(GermanDateTime.TimeOfDayRegex);
-    public static final Pattern PrepositionRegex = RegExpUtility.getSafeRegExp(GermanDateTime.PrepositionRegex);
-    public static final Pattern TimeFollowedUnit = RegExpUtility.getSafeRegExp(GermanDateTime.TimeFollowedUnit);
-    public static final Pattern PureNumBetweenAnd = RegExpUtility.getSafeRegExp(GermanDateTime.PureNumBetweenAnd);
-    public static final Pattern GeneralEndingRegex = RegExpUtility.getSafeRegExp(GermanDateTime.GeneralEndingRegex);
+    public static final Pattern HourRegex = RegExpUtility.getSafeRegExp(GermanDateTime.HourRegex);
     public static final Pattern PeriodHourNumRegex = RegExpUtility.getSafeRegExp(GermanDateTime.PeriodHourNumRegex);
+    public static final Pattern PeriodDescRegex = RegExpUtility.getSafeRegExp(GermanDateTime.DescRegex);
+    public static final Pattern PmRegex = RegExpUtility.getSafeRegExp(GermanDateTime.PmRegex);
+    public static final Pattern AmRegex = RegExpUtility.getSafeRegExp(GermanDateTime.AmRegex);
+    public static final Pattern PureNumFromTo = RegExpUtility.getSafeRegExp(GermanDateTime.PureNumFromTo);
+    public static final Pattern PureNumBetweenAnd = RegExpUtility.getSafeRegExp(GermanDateTime.PureNumBetweenAnd);
     public static final Pattern SpecificTimeFromTo = RegExpUtility.getSafeRegExp(GermanDateTime.SpecificTimeFromTo);
     public static final Pattern SpecificTimeBetweenAnd = RegExpUtility.getSafeRegExp(GermanDateTime.SpecificTimeBetweenAnd);
+    public static final Pattern PrepositionRegex = RegExpUtility.getSafeRegExp(GermanDateTime.PrepositionRegex);
+    public static final Pattern TimeOfDayRegex = RegExpUtility.getSafeRegExp(GermanDateTime.TimeOfDayRegex);
     public static final Pattern SpecificTimeOfDayRegex = RegExpUtility.getSafeRegExp(GermanDateTime.SpecificTimeOfDayRegex);
+    public static final Pattern TimeUnitRegex = RegExpUtility.getSafeRegExp(GermanDateTime.TimeUnitRegex);
+    public static final Pattern TimeFollowedUnit = RegExpUtility.getSafeRegExp(GermanDateTime.TimeFollowedUnit);
     public static final Pattern TimeNumberCombinedWithUnit = RegExpUtility.getSafeRegExp(GermanDateTime.TimeNumberCombinedWithUnit);
+    public static final Pattern GeneralEndingRegex = RegExpUtility.getSafeRegExp(GermanDateTime.GeneralEndingRegex);
+    public static final Pattern AmbiguousTimePeriodRegex = RegExpUtility.getSafeRegExp(GermanDateTime.AmbiguousTimePeriodRegex);
+    public final Iterable<Pattern> getSimpleCasesRegex = new ArrayList<Pattern>() {
+        {
+            add(PureNumFromTo);
+            add(PureNumBetweenAnd);
+        }
+    };
+    private final IDateTimeExtractor timeZoneExtractor;
+    private String tokenBeforeDate;
+    private IDateTimeUtilityConfiguration utilityConfiguration;
+    private IDateTimeExtractor singleTimeExtractor;
+    private IExtractor integerExtractor;
 
     public GermanTimePeriodExtractorConfiguration() {
         this(DateTimeOptions.None);
@@ -61,43 +70,37 @@ public class GermanTimePeriodExtractorConfiguration extends BaseOptionsConfigura
         timeZoneExtractor = new BaseTimeZoneExtractor(new GermanTimeZoneExtractorConfiguration(options));
     }
 
-    private IDateTimeUtilityConfiguration utilityConfiguration;
+    public final String getTokenBeforeDate() {
+        return tokenBeforeDate;
+    }
 
     public final IDateTimeUtilityConfiguration getUtilityConfiguration() {
         return utilityConfiguration;
     }
 
-    private IDateTimeExtractor singleTimeExtractor;
-
     public final IDateTimeExtractor getSingleTimeExtractor() {
         return singleTimeExtractor;
     }
-
-    private IExtractor integerExtractor;
 
     public final IExtractor getIntegerExtractor() {
         return integerExtractor;
     }
 
-    private final IDateTimeExtractor timeZoneExtractor;
-
     public IDateTimeExtractor getTimeZoneExtractor() {
         return timeZoneExtractor;
     }
-
 
     public Iterable<Pattern> getSimpleCasesRegex() {
         return getSimpleCasesRegex;
     }
 
-    public final Iterable<Pattern> getSimpleCasesRegex = new ArrayList<Pattern>() {
-        {
-            add(PureNumFromTo);
-            add(PureNumBetweenAnd);
-            add(SpecificTimeFromTo);
-            add(SpecificTimeBetweenAnd);
-        }
-    };
+    public Iterable<Pattern> getPureNumberRegex() {
+        return getSimpleCasesRegex;
+    }
+
+    public boolean checkBeforeAndAfter() {
+        return GermanDateTime.CheckBothBeforeAfter;
+    }
 
     public final Pattern getTillRegex() {
         return TillRegex;
@@ -113,8 +116,8 @@ public class GermanTimePeriodExtractorConfiguration extends BaseOptionsConfigura
 
     public final ResultIndex getFromTokenIndex(String input) {
         ResultIndex result = new ResultIndex(false, -1);
-        if (input.endsWith("from")) {
-            result = new ResultIndex(true, input.lastIndexOf("from"));
+        if (input.endsWith("von")) {
+            result = new ResultIndex(true, input.lastIndexOf("von"));
         }
 
         return result;
@@ -122,14 +125,32 @@ public class GermanTimePeriodExtractorConfiguration extends BaseOptionsConfigura
 
     public final ResultIndex getBetweenTokenIndex(String input) {
         ResultIndex result = new ResultIndex(false, -1);
-        if (input.endsWith("between")) {
-            result = new ResultIndex(true, input.lastIndexOf("between"));
+        if (input.endsWith("zwischen")) {
+            result = new ResultIndex(true, input.lastIndexOf("zwischen"));
         }
 
         return result;
     }
 
     public final boolean hasConnectorToken(String input) {
-        return input.equals("and");
+        return input.equals("und");
+    }
+
+    public List<ExtractResult> applyPotentialPeriodAmbiguityHotfix(String text, List<ExtractResult> timePeriodErs) {
+        List<ExtractResult> timePeriodErsResult = new ArrayList<>();
+        Match[] matches = RegExpUtility.getMatches(AmbiguousTimePeriodRegex, text);
+        for (ExtractResult timePeriodEr : timePeriodErs) {
+            if (matches.length > 0) {
+                for (Match match : matches) {
+                    if (!(timePeriodEr.getText().equals(match.value) && timePeriodEr.getStart() == match.index && timePeriodEr.getLength() == match.length)) {
+                        timePeriodErsResult.add(timePeriodEr);
+                    }
+                }
+            } else {
+                timePeriodErsResult.add(timePeriodEr);
+            }
+        }
+
+        return timePeriodErsResult;
     }
 }
