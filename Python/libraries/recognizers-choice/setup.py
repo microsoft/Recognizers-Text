f5fa79e86877439b89276767611efd88e729001b
Sam <sam.hickey@genesys.com>
2022-02-16 12:27:51 +0000

    Update version

--- Python/libraries/recognizers-choice/setup.py
+++ Python/libraries/recognizers-choice/setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 NAME = 'recognizers-text-choice-genesys'
-VERSION = '1.0.7a2'
+VERSION = '1.0.7a3'
 REQUIRES = ['recognizers-text-genesys', 'regex', 'grapheme']
 
 setup(
--- Python/libraries/recognizers-number/setup.py
+++ Python/libraries/recognizers-number/setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 
 NAME = "recognizers-text-number-genesys"
-VERSION = "1.0.7a2"
+VERSION = "1.0.7a3"
 REQUIRES = ['recognizers-text-genesys', 'regex']
 
 setup(
