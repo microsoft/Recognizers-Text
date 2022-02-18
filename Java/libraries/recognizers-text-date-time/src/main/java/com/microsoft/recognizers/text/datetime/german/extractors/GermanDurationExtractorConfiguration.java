765d976283c8dbda68f0814be8823c10aabcfe22
Maxwell Russek <maxwell.russek@gmail.com>
2022-02-17 22:09:18 -0700

    [Java] Add German DateTime Support in Java (#2855)
    * German date time parsing skeleton
    
    * Fix issues in GermanDateTime generated file (declaration order)
    
    * Add first, cloned from English, German implementations
    
    * First cut cleanup for German
    
    * Fixed GermanDateExtractorConfiguration, GermanHolidayExtractorConfiguration, GermanMergedExtractorConfiguration
    
    * German updates for DateTime variables
    
    * Fix compilation for GermanTimeExtractorConfiguration
    
    * Add getters for missing pattern
    
    * Fixing GermanDatePeriodExtractorConfiguration
    
    * Fix compilation for GermanTimeZoneExtractorConfiguration -- basically punt on this since we are date focused for now (like with French)
    
    * GermanDateParserConfiguration
    
    * Updated datetime config parsers for German
    
    * Adding GermanMergedParserConfiguration and GermanSetParserConfiguration
    
    * Use defined regexes for GermanDateTimePeriodParserConfiguration (following Spanish model)
    
    * Minor update to use lowercase value of time
    
    * Updated vars for GermanDatetimeUtilityConfiguration.java
    
    * Fix: GermanHolidayParserConfiguration, GermanTimePeriodParserConfiguration
    
    * Fix: GermanDateTimePeriodExtractorConfiguration, GermanDatePeriodParserConfiguration changes
    
    * HolidayParserGer refactor into GermanHolidayParserConfiguration
    
    * Add German to DateTimeExtractorTest and DateTimeParserTest. Still remaining failing tests.
    
    * Get tests running
    
    * Get holiday tests to pass for German
    
    * Update for German tests
    
    * Fix remaining test
    
    * Fix formatting
    
    * Updated parser to include quarter and three quarters
    
    * Handle "Was wird das wetter in den nächsten beiden Tage"
    
    * Marking currently unsupported java tests
    
    
    Co-authored-by: Max Russek <maxwell.russek@workday.com>
    Co-authored-by: Srinath Raghavan <srinath.raghavan@workday.com>
    Co-authored-by: jason.brant <jason.brant@workday.com>
    Co-authored-by: Mayank Vats <mayank.vats@workday.com>
    Co-authored-by: Victor Hernandez <victor.hernandez@workday.com>
    Co-authored-by: Srinath <srinath.ragh@gmail.com>

--- Java/libraries/recognizers-text-date-time/src/main/java/com/microsoft/recognizers/text/datetime/german/extractors/GermanDurationExtractorConfiguration.java
+++ Java/libraries/recognizers-text-date-time/src/main/java/com/microsoft/recognizers/text/datetime/german/extractors/GermanDurationExtractorConfiguration.java
@@ -1,52 +1,52 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.microsoft.recognizers.text.datetime.english.extractors;
+package com.microsoft.recognizers.text.datetime.german.extractors;
 
 import com.google.common.collect.ImmutableMap;
 import com.microsoft.recognizers.text.IExtractor;
 import com.microsoft.recognizers.text.datetime.DateTimeOptions;
 import com.microsoft.recognizers.text.datetime.config.BaseOptionsConfiguration;
 import com.microsoft.recognizers.text.datetime.extractors.config.IDurationExtractorConfiguration;
-import com.microsoft.recognizers.text.datetime.resources.EnglishDateTime;
-import com.microsoft.recognizers.text.number.english.extractors.CardinalExtractor;
+import com.microsoft.recognizers.text.datetime.resources.GermanDateTime;
+import com.microsoft.recognizers.text.number.german.extractors.CardinalExtractor;
 import com.microsoft.recognizers.text.utilities.RegExpUtility;
-
 import java.util.regex.Pattern;
 
-public class EnglishDurationExtractorConfiguration extends BaseOptionsConfiguration implements IDurationExtractorConfiguration {
+public class GermanDurationExtractorConfiguration extends BaseOptionsConfiguration implements IDurationExtractorConfiguration {
 
-    public static final Pattern DurationUnitRegex = RegExpUtility.getSafeRegExp(EnglishDateTime.DurationUnitRegex);
-    public static final Pattern SuffixAndRegex = RegExpUtility.getSafeRegExp(EnglishDateTime.SuffixAndRegex);
-    public static final Pattern DurationFollowedUnit = RegExpUtility.getSafeRegExp(EnglishDateTime.DurationFollowedUnit);
-    public static final Pattern NumberCombinedWithDurationUnit = RegExpUtility.getSafeRegExp(EnglishDateTime.NumberCombinedWithDurationUnit);
-    public static final Pattern AnUnitRegex = RegExpUtility.getSafeRegExp(EnglishDateTime.AnUnitRegex);
-    public static final Pattern DuringRegex = RegExpUtility.getSafeRegExp(EnglishDateTime.DuringRegex);
-    public static final Pattern AllRegex = RegExpUtility.getSafeRegExp(EnglishDateTime.AllRegex);
-    public static final Pattern HalfRegex = RegExpUtility.getSafeRegExp(EnglishDateTime.HalfRegex);
-    public static final Pattern ConjunctionRegex = RegExpUtility.getSafeRegExp(EnglishDateTime.ConjunctionRegex);
-    public static final Pattern InexactNumberRegex = RegExpUtility.getSafeRegExp(EnglishDateTime.InexactNumberRegex);
-    public static final Pattern InexactNumberUnitRegex = RegExpUtility.getSafeRegExp(EnglishDateTime.InexactNumberUnitRegex);
-    public static final Pattern RelativeDurationUnitRegex = RegExpUtility.getSafeRegExp(EnglishDateTime.RelativeDurationUnitRegex);
-    public static final Pattern DurationConnectorRegex = RegExpUtility.getSafeRegExp(EnglishDateTime.DurationConnectorRegex);
-    public static final Pattern MoreThanRegex = RegExpUtility.getSafeRegExp(EnglishDateTime.MoreThanRegex);
-    public static final Pattern LessThanRegex = RegExpUtility.getSafeRegExp(EnglishDateTime.LessThanRegex);
+    public static final Pattern DurationUnitRegex = RegExpUtility.getSafeRegExp(GermanDateTime.DurationUnitRegex);
+    public static final Pattern SuffixAndRegex = RegExpUtility.getSafeRegExp(GermanDateTime.SuffixAndRegex);
+    public static final Pattern DurationFollowedUnit = RegExpUtility.getSafeRegExp(GermanDateTime.DurationFollowedUnit);
+    public static final Pattern NumberCombinedWithDurationUnit = RegExpUtility.getSafeRegExp(GermanDateTime.NumberCombinedWithDurationUnit);
+    public static final Pattern AnUnitRegex = RegExpUtility.getSafeRegExp(GermanDateTime.AnUnitRegex);
+    public static final Pattern DuringRegex = RegExpUtility.getSafeRegExp(GermanDateTime.DuringRegex);
+    public static final Pattern AllRegex = RegExpUtility.getSafeRegExp(GermanDateTime.AllRegex);
+    public static final Pattern HalfRegex = RegExpUtility.getSafeRegExp(GermanDateTime.HalfRegex);
+    public static final Pattern ConjunctionRegex = RegExpUtility.getSafeRegExp(GermanDateTime.ConjunctionRegex);
+    public static final Pattern InexactNumberRegex = RegExpUtility.getSafeRegExp(GermanDateTime.InexactNumberRegex);
+    public static final Pattern InexactNumberUnitRegex = RegExpUtility.getSafeRegExp(GermanDateTime.InexactNumberUnitRegex);
+    public static final Pattern RelativeDurationUnitRegex = RegExpUtility.getSafeRegExp(GermanDateTime.RelativeDurationUnitRegex);
+    public static final Pattern DurationConnectorRegex = RegExpUtility.getSafeRegExp(GermanDateTime.DurationConnectorRegex);
+    public static final Pattern MoreThanRegex = RegExpUtility.getSafeRegExp(GermanDateTime.MoreThanRegex);
+    public static final Pattern LessThanRegex = RegExpUtility.getSafeRegExp(GermanDateTime.LessThanRegex);
+    public static final Pattern SpecialNumberUnitRegex = RegExpUtility.getSafeRegExp(GermanDateTime.SpecialNumberUnitRegex);
 
     private final IExtractor cardinalExtractor;
     private final ImmutableMap<String, String> unitMap;
     private final ImmutableMap<String, Long> unitValueMap;
 
-    public EnglishDurationExtractorConfiguration() {
+    public GermanDurationExtractorConfiguration() {
         this(DateTimeOptions.None);
     }
 
-    public EnglishDurationExtractorConfiguration(DateTimeOptions options) {
+    public GermanDurationExtractorConfiguration(DateTimeOptions options) {
 
         super(options);
 
         cardinalExtractor = CardinalExtractor.getInstance();
-        unitMap = EnglishDateTime.UnitMap;
-        unitValueMap = EnglishDateTime.UnitValueMap;
+        unitMap = GermanDateTime.UnitMap;
+        unitValueMap = GermanDateTime.UnitValueMap;
     }
 
     @Override
@@ -124,8 +124,8 @@ public class EnglishDurationExtractorConfiguration extends BaseOptionsConfigurat
         return MoreThanRegex;
     }
 
-    @Override public Pattern getSpecialNumberUnitRegex() {
-        return null;
+    public Pattern getSpecialNumberUnitRegex() {
+        return SpecialNumberUnitRegex;
     }
 
     @Override
---
+++ Java/libraries/recognizers-text-date-time/src/main/java/com/microsoft/recognizers/text/datetime/german/parsers/GermanTimeParserConfiguration.java
@@ -0,0 +1,198 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.microsoft.recognizers.text.datetime.german.parsers;
+
+import com.google.common.collect.ImmutableMap;
+import com.microsoft.recognizers.text.datetime.Constants;
+import com.microsoft.recognizers.text.datetime.config.BaseOptionsConfiguration;
+import com.microsoft.recognizers.text.datetime.german.extractors.GermanTimeExtractorConfiguration;
+import com.microsoft.recognizers.text.datetime.parsers.BaseTimeZoneParser;
+import com.microsoft.recognizers.text.datetime.parsers.IDateTimeParser;
+import com.microsoft.recognizers.text.datetime.parsers.config.ICommonDateTimeParserConfiguration;
+import com.microsoft.recognizers.text.datetime.parsers.config.ITimeParserConfiguration;
+import com.microsoft.recognizers.text.datetime.parsers.config.PrefixAdjustResult;
+import com.microsoft.recognizers.text.datetime.parsers.config.SuffixAdjustResult;
+import com.microsoft.recognizers.text.datetime.resources.GermanDateTime;
+import com.microsoft.recognizers.text.datetime.utilities.ConditionalMatch;
+import com.microsoft.recognizers.text.datetime.utilities.IDateTimeUtilityConfiguration;
+import com.microsoft.recognizers.text.datetime.utilities.RegexExtension;
+import com.microsoft.recognizers.text.utilities.Match;
+import com.microsoft.recognizers.text.utilities.RegExpUtility;
+import com.microsoft.recognizers.text.utilities.StringUtility;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+public class GermanTimeParserConfiguration extends BaseOptionsConfiguration implements ITimeParserConfiguration {
+
+    private final ImmutableMap<String, Integer> numbers;
+    private final IDateTimeUtilityConfiguration utilityConfiguration;
+    private final IDateTimeParser timeZoneParser;
+
+    private final Pattern atRegex;
+    private final Iterable<Pattern> timeRegexes;
+    private final Pattern timeSuffixFull = RegExpUtility.getSafeRegExp(GermanDateTime.TimeSuffixFull);
+    private final Pattern lunchRegex = RegExpUtility.getSafeRegExp(GermanDateTime.LunchRegex);
+    private final Pattern nightRegex = RegExpUtility.getSafeRegExp(GermanDateTime.NightRegex);
+    private final Pattern halfTokenRegex = RegExpUtility.getSafeRegExp(GermanDateTime.HalfTokenRegex);
+    private final Pattern quarterToTokenRegex = RegExpUtility.getSafeRegExp(GermanDateTime.QuarterToTokenRegex);
+    private final Pattern quarterPastTokenRegex = RegExpUtility.getSafeRegExp(GermanDateTime.QuarterPastTokenRegex);
+    private final Pattern threeQuarterToTokenRegex = RegExpUtility.getSafeRegExp(GermanDateTime.ThreeQuarterToTokenRegex);
+    private final Pattern threeQuarterPastTokenRegex = RegExpUtility.getSafeRegExp(GermanDateTime.ThreeQuarterPastTokenRegex);
+
+    public GermanTimeParserConfiguration(ICommonDateTimeParserConfiguration config) {
+
+        super(config.getOptions());
+
+        numbers = config.getNumbers();
+        utilityConfiguration = config.getUtilityConfiguration();
+        timeZoneParser = new BaseTimeZoneParser();
+
+        atRegex = GermanTimeExtractorConfiguration.AtRegex;
+        timeRegexes = GermanTimeExtractorConfiguration.TimeRegexList;
+    }
+
+    @Override
+    public String getTimeTokenPrefix() {
+        return GermanDateTime.TimeTokenPrefix;
+    }
+
+    @Override
+    public Pattern getAtRegex() {
+        return atRegex;
+    }
+
+    @Override
+    public Iterable<Pattern> getTimeRegexes() {
+        return timeRegexes;
+    }
+
+    @Override
+    public ImmutableMap<String, Integer> getNumbers() {
+        return numbers;
+    }
+
+    @Override
+    public IDateTimeUtilityConfiguration getUtilityConfiguration() {
+        return utilityConfiguration;
+    }
+
+    @Override
+    public IDateTimeParser getTimeZoneParser() {
+        return timeZoneParser;
+    }
+
+    @Override
+    public PrefixAdjustResult adjustByPrefix(String prefix, int hour, int min, boolean hasMin) {
+
+        int deltaMin;
+        String trimmedPrefix = prefix.trim().toLowerCase();
+
+        if (checkMatch(halfTokenRegex, trimmedPrefix)) {
+            deltaMin = -30;
+        } else if (checkMatch(quarterToTokenRegex, trimmedPrefix)) {
+            deltaMin = -15;
+        } else if (checkMatch(quarterPastTokenRegex, trimmedPrefix)) {
+            deltaMin = 15;
+        } else if (checkMatch(threeQuarterToTokenRegex, trimmedPrefix)) {
+            deltaMin = -45;
+        } else if (checkMatch(threeQuarterPastTokenRegex, trimmedPrefix)) {
+            deltaMin = 45;
+        } else {
+
+            Optional<Match> match = Arrays.stream(RegExpUtility.getMatches(GermanTimeExtractorConfiguration.LessThanOneHour, trimmedPrefix)).findFirst();
+            String minStr = match.get().getGroup("deltamin").value;
+            if (!StringUtility.isNullOrWhiteSpace(minStr)) {
+                deltaMin = Integer.parseInt(minStr);
+            } else {
+                minStr = match.get().getGroup("deltaminnum").value;
+                deltaMin = numbers.getOrDefault(minStr, 0);
+            }
+        }
+
+        if (trimmedPrefix.endsWith("zum")) {
+            deltaMin = -deltaMin;
+        }
+
+        min += deltaMin;
+        if (min < 0) {
+            min += 60;
+            hour -= 1;
+        }
+
+        hasMin = true;
+
+        return new PrefixAdjustResult(hour, min, hasMin);
+    }
+
+    @Override
+    public SuffixAdjustResult adjustBySuffix(String suffix, int hour, int min, boolean hasMin, boolean hasAm, boolean hasPm) {
+
+        String lowerSuffix = suffix.toLowerCase();
+        int deltaHour = 0;
+        ConditionalMatch match = RegexExtension.matchExact(timeSuffixFull, lowerSuffix, true);
+        if (match.getSuccess()) {
+
+            String oclockStr = match.getMatch().get().getGroup("oclock").value;
+            if (StringUtility.isNullOrEmpty(oclockStr)) {
+
+                String amStr = match.getMatch().get().getGroup(Constants.AmGroupName).value;
+                if (!StringUtility.isNullOrEmpty(amStr)) {
+                    if (hour >= Constants.HalfDayHourCount) {
+                        deltaHour = -Constants.HalfDayHourCount;
+                    } else {
+                        hasAm = true;
+                    }
+
+                }
+
+                String pmStr = match.getMatch().get().getGroup(Constants.PmGroupName).value;
+                if (!StringUtility.isNullOrEmpty(pmStr)) {
+                    if (hour < Constants.HalfDayHourCount) {
+                        deltaHour = Constants.HalfDayHourCount;
+                    }
+
+                    if (checkMatch(lunchRegex, pmStr)) {
+                        // for hour >= 10, < 12
+                        if (hour >= 10 && hour <= Constants.HalfDayHourCount) {
+                            deltaHour = 0;
+                            if (hour == Constants.HalfDayHourCount) {
+                                hasPm = true;
+                            } else {
+                                hasAm = true;
+                            }
+
+                        } else {
+                            hasPm = true;
+                        }
+
+                    } else if (checkMatch(nightRegex, pmStr)) {
+                        //For hour <= 3 or == 12, we treat it as am, for example 1 in the night (midnight) == 1am
+                        if (hour <= 3 || hour == Constants.HalfDayHourCount) {
+                            if (hour == Constants.HalfDayHourCount) {
+                                hour = 0;
+                            }
+
+                            deltaHour = 0;
+                            hasAm = true;
+                        } else {
+                            hasPm = true;
+                        }
+
+                    } else {
+                        hasPm = true;
+                    }
+                }
+            }
+        }
+
+        hour = (hour + deltaHour) % 24;
+
+        return new SuffixAdjustResult(hour, min, hasMin, hasAm, hasPm);
+    }
+
+    private boolean checkMatch(Pattern regex, String input) {
+        return RegExpUtility.getMatches(regex, input).length > 0;
+    }
+}
