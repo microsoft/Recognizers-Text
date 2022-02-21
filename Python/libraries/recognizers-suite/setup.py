a1fd6433d5e82143e9a53fcd499f970a37f34e31
Colm Dillon <cdillon@ip-192-168-0-108.ec2.internal>
2022-02-21 12:49:46 +0000

    bump to new alpha

--- Python/libraries/recognizers-suite/setup.py
+++ Python/libraries/recognizers-suite/setup.py
@@ -10,14 +10,14 @@ def read(fname):
 
 
 NAME = 'recognizers-text-suite-genesys'
-VERSION = '1.0.6a0'
+VERSION = '1.0.6a1'
 REQUIRES = [
-    'recognizers-text-genesys==1.0.6a0',
-    'recognizers-text-number-genesys==1.0.6a0',
-    'recognizers-text-number-with-unit-genesys==1.0.6a0',
-    'recognizers-text-date-time-genesys==1.0.6a0',
-    'recognizers-text-sequence-genesys==1.0.6a0',
-    'recognizers-text-choice-genesys==1.0.6a0'
+    'recognizers-text-genesys==1.0.6a1',
+    'recognizers-text-number-genesys==1.0.6a1',
+    'recognizers-text-number-with-unit-genesys==1.0.6a1',
+    'recognizers-text-date-time-genesys==1.0.6a1',
+    'recognizers-text-sequence-genesys==1.0.6a1',
+    'recognizers-text-choice-genesys==1.0.6a1'
 ]
 
 setup(
