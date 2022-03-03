365d66fd9bfc23c2bd14a253492997372c148854
colm-dillon <72518145+colm-dillon@users.noreply.github.com>
2022-03-03 11:35:43 +0000

    [Python] Enabling SpanishMexican culture in Number/Unit/DateTimveV2 (#2878)
    * Enabling SpanishMexican culture in Number/Unit/DateTimveV2
    
    * Update Spanish number specs according to review
    
    Co-authored-by: Colm Dillon <cdillon@ip-192-168-0-108.ec2.internal>

--- Python/libraries/recognizers-number/recognizers_number/number/german/parsers.py
+++ Python/libraries/recognizers-number/recognizers_number/number/german/parsers.py
@@ -80,6 +80,14 @@ class GermanNumberParserConfiguration(NumberParserConfiguration):
     def written_fraction_separator_texts(self) -> List[str]:
         return self._written_fraction_separator_texts
 
+    @property
+    def non_standard_separator_variants(self) -> List[str]:
+        return self._non_standard_separator_variants
+
+    @property
+    def is_multi_decimal_separator_culture(self) -> bool:
+        return self._is_multi_decimal_separator_culture
+
     def __init__(self, culture_info=None):
         if culture_info is None:
             culture_info = CultureInfo(Culture.German)
@@ -91,6 +99,8 @@ class GermanNumberParserConfiguration(NumberParserConfiguration):
         self._non_decimal_separator_char = GermanNumeric.NonDecimalSeparatorChar
         self._half_a_dozen_text = GermanNumeric.HalfADozenText
         self._word_separator_token = GermanNumeric.WordSeparatorToken
+        self._non_standard_separator_variants = []
+        self._is_multi_decimal_separator_culture = GermanNumeric.MultiDecimalSeparatorCulture
 
         self._written_decimal_separator_texts = GermanNumeric.WrittenDecimalSeparatorTexts
         self._written_group_separator_texts = GermanNumeric.WrittenGroupSeparatorTexts
