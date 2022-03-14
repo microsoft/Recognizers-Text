f5426e38a09d3974fc0979b7803a4cd17258ea62
Andreia Gaita <shana@spoiledcat.net>
2022-03-14 12:51:18 +0100

    [EN .NET] Workaround for TimexProperty.ToString() to not crash on DateTimeRanges (#2894)
    * Workaround for TimexProperty.ToString() to not crash on DateTimeRanges 
    
    * Add TODO for fixing the TimexProperty date range representation properly according to review

--- .NET/Microsoft.Recognizers.Text.DataTypes.DataDrivenTests/TestTimex.cs
+++ .NET/Microsoft.Recognizers.Text.DataTypes.DataDrivenTests/TestTimex.cs
@@ -138,6 +138,14 @@ namespace Microsoft.Recognizers.Text.DataTypes.TimexExpression.Tests
             Assert.AreEqual("13th March 2022", timex.ToString());
         }
 
+        [TestMethod]
+        public void DataTypes_Timex_FromDateTimeRange_ToString()
+        {
+            // TODO: This test documents a workaround to avoid exceptions when calling TimexProperty.ToString(). Proper fix for date range representation is needed.
+            var timex = new TimexProperty("(2022-03-15T16,2022-03-15T18,PT2H)");
+            Assert.AreEqual("15th March 2022 4PM", timex.ToString());
+        }
+
         private static void Roundtrip(string timex)
         {
             Assert.AreEqual(timex, new TimexProperty(timex).TimexValue);
