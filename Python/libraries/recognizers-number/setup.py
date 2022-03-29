a118e8a0eb3c1d8740584684f308bf771f3f971f
Oisin McNally <oisin.mcnally@genesys.com>
2022-03-29 14:30:22 +0100

    Update version numbers

--- Python/libraries/recognizers-number/setup.py
+++ Python/libraries/recognizers-number/setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 
 NAME = "recognizers-text-number-genesys"
-VERSION = "1.0.10a1"
+VERSION = "1.0.11a0"
 REQUIRES = ['recognizers-text-genesys', 'regex']
 
 setup(
--- Python/libraries/recognizers-suite/setup.py
+++ Python/libraries/recognizers-suite/setup.py
@@ -10,12 +10,12 @@ def read(fname):
 
 
 NAME = 'recognizers-text-suite-genesys'
-VERSION = '1.0.10a1'
+VERSION = '1.0.11a0'
 REQUIRES = [
     'recognizers-text-genesys==1.0.10a0',
-    'recognizers-text-number-genesys==1.0.10a1',
-    'recognizers-text-number-with-unit-genesys==1.0.10a1',
-    'recognizers-text-date-time-genesys==1.0.10a1',
+    'recognizers-text-number-genesys==1.0.11a0',
+    'recognizers-text-number-with-unit-genesys==1.0.11a0',
+    'recognizers-text-date-time-genesys==1.0.11a0',
     'recognizers-text-sequence-genesys==1.0.10a0',
     'recognizers-text-choice-genesys==1.0.10a0'
 ]
