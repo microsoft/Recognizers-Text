ca3af140c5911ec667d34c8f566d144ce2a6958e
Oisin McNally <oisin.mcnally@genesys.com>
2022-04-06 16:08:26 +0100

    Revert versions to master

--- Python/libraries/recognizers-suite/setup.py
+++ Python/libraries/recognizers-suite/setup.py
@@ -9,16 +9,10 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-NAME = 'recognizers-text-suite-genesys'
-VERSION = '1.0.11'
-REQUIRES = [
-    'recognizers-text-genesys==1.0.11',
-    'recognizers-text-number-genesys==1.0.11',
-    'recognizers-text-number-with-unit-genesys==1.0.11',
-    'recognizers-text-date-time-genesys==1.0.11',
-    'recognizers-text-sequence-genesys==1.0.11',
-    'recognizers-text-choice-genesys==1.0.11'
-]
+NAME = 'recognizers-text-suite'
+VERSION = '1.0.0.a0'
+REQUIRES = ['recognizers-text', 'recognizers-text-number', 'recognizers-text-number-with-unit',
+            'recognizers-text-date-time', 'recognizers-text-sequence', 'recognizers-text-choice']
 
 setup(
     name=NAME,
