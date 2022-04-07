ad7f195a2112fed6cc01fc103c6c0dd7d74694cc
Oisin McNally <oisin.mcnally@genesys.com>
2022-04-07 10:09:32 +0100

    increment lib numbers

--- Python/libraries/recognizers-choice/setup.py
+++ Python/libraries/recognizers-choice/setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 NAME = 'recognizers-text-choice-genesys'
-VERSION = '1.0.13'
+VERSION = '1.0.14'
 REQUIRES = ['recognizers-text-genesys', 'regex', 'grapheme']
 
 setup(
--- Python/libraries/recognizers-number/setup.py
+++ Python/libraries/recognizers-number/setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 
 NAME = "recognizers-text-number-genesys"
-VERSION = "1.0.13"
+VERSION = "1.0.14"
 REQUIRES = ['recognizers-text-genesys', 'regex']
 
 setup(
