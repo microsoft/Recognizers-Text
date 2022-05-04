8a258581cf218392dd206ed4466bafc4597fbf00
Oisin McNally <oisin.mcnally@genesys.com>
2022-05-04 13:58:49 +0100

    Release version

--- Python/libraries/datatypes-timex-expression/setup.py
+++ Python/libraries/datatypes-timex-expression/setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 NAME = 'datatypes_timex_expression_genesys'
 
-VERSION = '1.0.17a0'
+VERSION = '1.0.17'
 REQUIRES = []
 
 setup(
--- Python/libraries/recognizers-suite/setup.py
+++ Python/libraries/recognizers-suite/setup.py
@@ -11,14 +11,14 @@ def read(fname):
 
 NAME = 'recognizers-text-suite-genesys'
 
-VERSION = '1.0.17a0'
+VERSION = '1.0.17'
 REQUIRES = [
-    'recognizers-text-genesys==1.0.17a0',
-    'recognizers-text-number-genesys==1.0.17a0',
-    'recognizers-text-number-with-unit-genesys==1.0.17a0',
-    'recognizers-text-date-time-genesys==1.0.17a0',
-    'recognizers-text-sequence-genesys==1.0.17a0',
-    'recognizers-text-choice-genesys==1.0.17a0'
+    'recognizers-text-genesys==1.0.17',
+    'recognizers-text-number-genesys==1.0.17',
+    'recognizers-text-number-with-unit-genesys==1.0.17',
+    'recognizers-text-date-time-genesys==1.0.17',
+    'recognizers-text-sequence-genesys==1.0.17',
+    'recognizers-text-choice-genesys==1.0.17'
 
 ]
 
