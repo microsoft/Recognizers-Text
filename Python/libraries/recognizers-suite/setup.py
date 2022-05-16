76ba177fedb3dd1898f98a5b117049c1358fb9d3
Sam <sam.hickey@genesys.com>
2022-05-16 14:04:14 +0100

    Update package version

--- Python/libraries/recognizers-suite/setup.py
+++ Python/libraries/recognizers-suite/setup.py
@@ -11,14 +11,14 @@ def read(fname):
 
 NAME = 'recognizers-text-suite-genesys'
 
-VERSION = '1.0.17'
+VERSION = '1.0.18a0'
 REQUIRES = [
-    'recognizers-text-genesys==1.0.17',
-    'recognizers-text-number-genesys==1.0.17',
-    'recognizers-text-number-with-unit-genesys==1.0.17',
-    'recognizers-text-date-time-genesys==1.0.17',
-    'recognizers-text-sequence-genesys==1.0.17',
-    'recognizers-text-choice-genesys==1.0.17'
+    'recognizers-text-genesys==1.0.18a0',
+    'recognizers-text-number-genesys==1.0.18a0',
+    'recognizers-text-number-with-unit-genesys==1.0.18a0',
+    'recognizers-text-date-time-genesys==1.0.18a0',
+    'recognizers-text-sequence-genesys==1.0.18a0',
+    'recognizers-text-choice-genesys==1.0.18a0'
 
 ]
 
