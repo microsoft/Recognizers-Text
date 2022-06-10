901b9018e9be5fffc002331307f06bab226e58c6
kevinwalshgen <kevin.walsh@genesys.com>
2022-06-10 16:16:36 +0100

    NLU-2966: 1.0.20 Version number

--- Python/libraries/recognizers-suite/setup.py
+++ Python/libraries/recognizers-suite/setup.py
@@ -11,14 +11,14 @@ def read(fname):
 
 NAME = 'recognizers-text-suite-genesys'
 
-VERSION = '1.0.19'
+VERSION = '1.0.20'
 REQUIRES = [
-    'recognizers-text-genesys==1.0.19',
-    'recognizers-text-number-genesys==1.0.19',
-    'recognizers-text-number-with-unit-genesys==1.0.19',
-    'recognizers-text-date-time-genesys==1.0.19',
-    'recognizers-text-sequence-genesys==1.0.19',
-    'recognizers-text-choice-genesys==1.0.19'
+    'recognizers-text-genesys==1.0.20',
+    'recognizers-text-number-genesys==1.0.20',
+    'recognizers-text-number-with-unit-genesys==1.0.20',
+    'recognizers-text-date-time-genesys==1.0.20',
+    'recognizers-text-sequence-genesys==1.0.20',
+    'recognizers-text-choice-genesys==1.0.20'
 ]
 
 setup(
