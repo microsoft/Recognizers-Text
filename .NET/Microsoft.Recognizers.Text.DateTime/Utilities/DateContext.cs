0d63c025ccfa872abe167fe57bb18cce29d133ec
aitelint <Fabrizio.Sorba@telusinternational.com>
2022-06-23 09:47:47 +0000

    removed DateContext modifications

--- .NET/Microsoft.Recognizers.Text.DateTime/Utilities/DateContext.cs
+++ .NET/Microsoft.Recognizers.Text.DateTime/Utilities/DateContext.cs
@@ -109,19 +109,6 @@ namespace Microsoft.Recognizers.Text.DateTime
             return firstWeekday.AddDays(7 * (cardinal - 1));
         }
 
-        // This method is to ensure the month of end date is same with the begin date in patterns like "april 9th through seventeenth".
-        public (DateTimeParseResult pr1, DateTimeParseResult pr2) SyncMonth(DateTimeParseResult pr1, DateTimeParseResult pr2)
-        {
-            var pr1Value = (DateTimeResolutionResult)pr1.Value;
-            var pr2Value = (DateTimeResolutionResult)pr2.Value;
-            var timexList1 = pr1.TimexStr.Split(Constants.DateTimexConnector[0]);
-            var timexList2 = pr2.TimexStr.Split(Constants.DateTimexConnector[0]);
-            (pr1.TimexStr, pr1.Value) = SyncDateEntityResolutionMonth(pr1Value, pr2Value, timexList1, timexList2);
-            (pr2.TimexStr, pr2.Value) = SyncDateEntityResolutionMonth(pr2Value, pr1Value, timexList2, timexList1, false);
-
-            return (pr1, pr2);
-        }
-
         // This method is to ensure the year of begin date is same with the end date in no year situation.
         public (DateTimeParseResult pr1, DateTimeParseResult pr2) SyncYear(DateTimeParseResult pr1, DateTimeParseResult pr2)
         {
@@ -210,30 +197,6 @@ namespace Microsoft.Recognizers.Text.DateTime
             var endDate = SetDateWithContext(originalDateRange.Item2);
 
             return new Tuple<DateObject, DateObject>(startDate, endDate);
-        }
-
-        private (string, DateTimeResolutionResult) SyncDateEntityResolutionMonth(DateTimeResolutionResult value1, DateTimeResolutionResult value2, string[] timexList1, string[] timexList2, bool isFirstDate = true)
-        {
-            if (timexList1[1].Equals(Constants.TimexFuzzyMonth, StringComparison.Ordinal) && !timexList2[1].Equals(Constants.TimexFuzzyMonth, StringComparison.Ordinal))
-            {
-                var futureValue1 = (DateObject)value1.FutureValue;
-                var futureValue2 = (DateObject)value2.FutureValue;
-                var pastValue1 = (DateObject)value1.PastValue;
-                var pastValue2 = (DateObject)value2.PastValue;
-
-                // If the ordinal number is the first date and it represents a day larger than the second date (e.g. "from the twenty-seventh to the fourth of april"),
-                // it is resolved according to the reference date, otherwise it is resolved to the month of the other date.
-                if (!(isFirstDate && futureValue2.Day <= futureValue1.Day))
-                {
-                    int swift = isFirstDate || futureValue1.Day > futureValue2.Day ? 0 : 1;
-                    value1.FutureValue = DateObject.MinValue.SafeCreateFromValue(futureValue2.Year, futureValue2.Month, futureValue1.Day).AddMonths(swift);
-                    value1.PastValue = DateObject.MinValue.SafeCreateFromValue(pastValue2.Year, pastValue2.Month, pastValue1.Day).AddMonths(swift);
-                    timexList1[1] = ((DateObject)value1.FutureValue).Month.ToString("D2");
-                }
-            }
-
-            var timexStr = string.Join(Constants.DateTimexConnector, timexList1);
-            return (timexStr, value1);
         }
     }
 }
