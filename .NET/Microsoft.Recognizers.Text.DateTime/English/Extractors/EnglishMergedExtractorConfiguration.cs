d1a6acc081fc07818b6d431fa92f7d0230821791
Neelisha Saxena <nsaxena@microsoft.com>
2022-08-03 15:23:51 +0530

    deleting  code comment.

--- .NET/Microsoft.Recognizers.Text.DateTime/English/Extractors/EnglishMergedExtractorConfiguration.cs
+++ .NET/Microsoft.Recognizers.Text.DateTime/English/Extractors/EnglishMergedExtractorConfiguration.cs
@@ -67,38 +67,6 @@ namespace Microsoft.Recognizers.Text.DateTime.English
 
         };
 
-        /*
-        // Skip Regexes under TasksMode
-        public static readonly Regex[] TasksModeMentionFilters =
-        {
-            // supress cases like 1ampm
-            new Regex(DateTimeDefinitions.AmPmDescRegex, RegexFlags),
-
-            // supress cases like 1p--> 1pm
-            new Regex(DateTimeDefinitions.TasksModeSpecialDescRegex, RegexFlags),
-
-            // suppress holiday time reference whose celebration dates are region specific
-            new Regex(DateTimeDefinitions.TasksModeHolidayListSupression, RegexFlags),
-
-            // Remove decade regex ex seventies, twenties
-            new Regex(DateTimeDefinitions.DecadeRegex, RegexFlags),
-
-            // Remove decade regex ex 1990s, 9s
-            new Regex(DateTimeDefinitions.DecadeWithCenturyRegex, RegexFlags),
-
-            // "Q1 2018", "2nd quarter"
-            new Regex(DateTimeDefinitions.QuarterRegex, RegexFlags),
-
-            // "2016 Q1", "last year the 4th quarter"
-            new Regex(DateTimeDefinitions.QuarterRegexYearFront, RegexFlags),
-
-            // "2015 the H1", "H2 of 2016", "1st half 2018", "2nd half this year"
-            new Regex(DateTimeDefinitions.AllHalfYearRegex, RegexFlags),
-
-            // summer winter etc
-            new Regex(DateTimeDefinitions.SeasonRegex, RegexFlags),
-        };
-        */
         public static readonly StringMatcher SuperfluousWordMatcher = new StringMatcher();
 
         private const RegexOptions RegexFlags = RegexOptions.Singleline | RegexOptions.ExplicitCapture;
