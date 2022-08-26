e614fc6c53a45f3e6560c5aa923b9bf127d14a2f
Börje F. Karlsson <borjekar@microsoft.com>
2022-08-26 12:27:12 +0800

    Adding PT NumberRange resources and configs.

---
+++ .NET/Microsoft.Recognizers.Text.Number/Portuguese/Parsers/PortugueseNumberRangeParserConfiguration.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Globalization;
+using System.Text.RegularExpressions;
+using Microsoft.Recognizers.Definitions.Portuguese;
+
+namespace Microsoft.Recognizers.Text.Number.Portuguese
+{
+    public class PortugueseNumberRangeParserConfiguration : INumberRangeParserConfiguration
+    {
+
+        private const RegexOptions RegexFlags = RegexOptions.Singleline | RegexOptions.ExplicitCapture;
+
+        public PortugueseNumberRangeParserConfiguration(INumberOptionsConfiguration config)
+        {
+
+            CultureInfo = new CultureInfo(config.Culture);
+
+            var numConfig = new BaseNumberOptionsConfiguration(config.Culture, config.Options);
+
+            NumberExtractor = Spanish.NumberExtractor.GetInstance(numConfig);
+            OrdinalExtractor = Spanish.OrdinalExtractor.GetInstance(numConfig);
+
+            NumberParser = new BaseNumberParser(new PortugueseNumberParserConfiguration(config));
+
+            MoreOrEqual = new Regex(NumbersDefinitions.MoreOrEqual, RegexFlags);
+            LessOrEqual = new Regex(NumbersDefinitions.LessOrEqual, RegexFlags);
+            MoreOrEqualSuffix = new Regex(NumbersDefinitions.MoreOrEqualSuffix, RegexFlags);
+            LessOrEqualSuffix = new Regex(NumbersDefinitions.LessOrEqualSuffix, RegexFlags);
+            MoreOrEqualSeparate = new Regex(NumbersDefinitions.OneNumberRangeMoreSeparateRegex, RegexFlags);
+            LessOrEqualSeparate = new Regex(NumbersDefinitions.OneNumberRangeLessSeparateRegex, RegexFlags);
+        }
+
+        public CultureInfo CultureInfo { get; private set; }
+
+        public IExtractor NumberExtractor { get; private set; }
+
+        public IExtractor OrdinalExtractor { get; private set; }
+
+        public IParser NumberParser { get; private set; }
+
+        public Regex MoreOrEqual { get; private set; }
+
+        public Regex LessOrEqual { get; private set; }
+
+        public Regex MoreOrEqualSuffix { get; private set; }
+
+        public Regex LessOrEqualSuffix { get; private set; }
+
+        public Regex MoreOrEqualSeparate { get; private set; }
+
+        public Regex LessOrEqualSeparate { get; private set; }
+    }
+}
