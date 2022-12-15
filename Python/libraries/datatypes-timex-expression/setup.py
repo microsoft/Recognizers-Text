030d6f4213e8b7445d7528de7bd85c747a641f68
Conor <93664980+Conor-Keaney@users.noreply.github.com>
2022-12-15 17:19:26 +0000

    [NLU-3333] - Updating parser to merge joinable numbers (#52)
    * Adding merging joint numbers functionality to python
    
    * Unskipping Spanish, Portuguese and Japanese tests, updating version
    
    * Update to release version
    
    * Update package version
    
    * Update package version
    
    * Update package version to release

--- Python/libraries/datatypes-timex-expression/setup.py
+++ Python/libraries/datatypes-timex-expression/setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 NAME = 'datatypes_timex_expression_genesys'
 
-VERSION = '1.0.28'
+VERSION = '1.0.30'
 REQUIRES = []
 
 setup(
--- Python/libraries/recognizers-choice/setup.py
+++ Python/libraries/recognizers-choice/setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 NAME = 'recognizers-text-choice-genesys'
 
-VERSION = '1.0.28'
+VERSION = '1.0.30'
 
 REQUIRES = ['recognizers-text-genesys', 'regex', 'grapheme']
 
