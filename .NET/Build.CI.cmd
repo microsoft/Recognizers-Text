27371dfb9d7483782b66d11bde2319adbce67a12
FARHAD SHAKERIN <fxs130430@utdallas.edu>
2023-03-28 16:32:15 -0700

    - msbuild

--- .NET/Build.CI.cmd
+++ .NET/Build.CI.cmd
@@ -53,18 +53,5 @@ IF %ERRORLEVEL% NEQ 0 (
 
 ECHO.
 ECHO # Restoring NuGet dependencies
-CALL "buildtools\nuget" restore
+CALL %MSBuild% Microsoft.Recognizers.Text.sln -t:restore /p:RestorePackagesConfig=true
 
-set configuration=Release
-ECHO.
-ECHO # Generate resources
-CALL !MSBuild! Microsoft.Recognizers.Definitions.Common\Microsoft.Recognizers.Definitions.Common.csproj /t:Clean,Build /p:Configuration=%configuration%
-
-ECHO # Building .NET solution (%configuration%)
-CALL !MSBuild! Microsoft.Recognizers.Text.sln /t:Restore,Clean,Build /p:Configuration=%configuration%
-IF %ERRORLEVEL% NEQ 0 (
-	ECHO # Failed to build .NET Project.
-	EXIT /b %ERRORLEVEL%
-)
-
-ECHO ============================== .NET BUILD END ==============================
