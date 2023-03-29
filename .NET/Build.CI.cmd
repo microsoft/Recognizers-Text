2e83fedeb07d20057d8708c603ffbde8c1eec180
FARHAD SHAKERIN <fxs130430@utdallas.edu>
2023-03-28 17:50:52 -0700

    - fixing typo in validation project

--- .NET/Build.CI.cmd
+++ .NET/Build.CI.cmd
@@ -53,8 +53,9 @@ IF %ERRORLEVEL% NEQ 0 (
 
 ECHO.
 ECHO # Restoring NuGet dependencies
-CALL "buildtools\nuget"
-CALL "buildtools\nuget" restore
+REM CALL !MSBuild! Microsoft.Recognizers.Definitions.Common\Microsoft.Recognizers.Definitions.Common.csproj /t:Restore
+REM CALL !MSBuild! Microsoft.Recognizers.Definitions\Microsoft.Recognizers.Definitions.csproj /t:Restore
+CALL !MSBuild! Microsoft.Recognizers.Text.sln /t:Restore
 
 set configuration=Release
 ECHO.
