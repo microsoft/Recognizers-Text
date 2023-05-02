51dfd2899090ed7d787c31bb642839558f25d705
andrew-gradinari <93652511+andrew-gradinari@users.noreply.github.com>
2023-05-02 15:25:57 +0100

    [NLU-3534] Fix for case like 'March one 2020' (#73)

--- Python/libraries/recognizers-choice/setup.py
+++ Python/libraries/recognizers-choice/setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 NAME = 'recognizers-text-choice-genesys'
-VERSION = '1.0.47'
+VERSION = '1.0.50'
 REQUIRES = ['recognizers-text-genesys', 'regex', 'grapheme']
 
 setup(
--- Python/libraries/recognizers-number/setup.py
+++ Python/libraries/recognizers-number/setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 
 NAME = "recognizers-text-number-genesys"
-VERSION = "1.0.47"
+VERSION = "1.0.50"
 REQUIRES = ['recognizers-text-genesys', 'regex']
 
 setup(
