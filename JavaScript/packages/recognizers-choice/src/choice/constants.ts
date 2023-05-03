51233ee503bba59cf17e46c446a10bb12d4bc865
FARHAD SHAKERIN <fxs130430@utdallas.edu>
2023-05-03 11:31:22 -0700

    Delete JavaScript directory

--- JavaScript/packages/recognizers-choice/src/choice/constants.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-export class Constants {
-    static readonly SYS_BOOLEAN: string = "boolean";
-    static readonly SYS_BOOLEAN_TRUE: string = "boolean-true";
-    static readonly SYS_BOOLEAN_FALSE: string = "boolean-false";
-}
