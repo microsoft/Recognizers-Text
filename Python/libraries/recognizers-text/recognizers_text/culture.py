b32b5c972716235b741f29fa2b07d2af20479ed9
Sam Hickey <sam.hickey@genesys.com>
2023-09-20 14:27:53 +0100

    Add arabic date and time support

--- Python/libraries/recognizers-text/recognizers_text/culture.py
+++ Python/libraries/recognizers-text/recognizers_text/culture.py
@@ -15,6 +15,7 @@ class Culture:
     SpanishMexican: str = 'es-mx'
     Turkish: str = 'tr-tr'
     German: str = 'de-de'
+    Arabic: str = 'ar-ae'
 
     @staticmethod
     def _get_supported_culture_codes():
@@ -31,7 +32,8 @@ class Culture:
             Culture.Spanish,
             Culture.SpanishMexican,
             Culture.Turkish,
-            Culture.German
+            Culture.German,
+            Culture.Arabic
         ]
 
     @staticmethod
