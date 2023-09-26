189ccb5daa31c76c11b99a5be7b95ecf587cdf96
Sam Hickey <sam.hickey@genesys.com>
2023-09-26 16:21:10 +0100

    Update package version

--- Python/libraries/recognizers-suite/setup.py
+++ Python/libraries/recognizers-suite/setup.py
@@ -10,15 +10,15 @@ def read(fname):
 
 
 NAME = 'recognizers-text-suite-genesys'
-VERSION = '1.0.66a0'
+VERSION = '1.0.66a1'
 REQUIRES = [
-    'recognizers-text-genesys==1.0.66a0',
-    'recognizers-text-number-genesys==1.0.66a0',
-    'recognizers-text-number-with-unit-genesys==1.0.66a0',
-    'recognizers-text-date-time-genesys==1.0.66a0',
-    'recognizers-text-sequence-genesys==1.0.66a0',
-    'recognizers-text-choice-genesys==1.0.66a0',
-    'datatypes_timex_expression_genesys==1.0.66a0'
+    'recognizers-text-genesys==1.0.66a1',
+    'recognizers-text-number-genesys==1.0.66a1',
+    'recognizers-text-number-with-unit-genesys==1.0.66a1',
+    'recognizers-text-date-time-genesys==1.0.66a1',
+    'recognizers-text-sequence-genesys==1.0.66a1',
+    'recognizers-text-choice-genesys==1.0.66a1',
+    'datatypes_timex_expression_genesys==1.0.66a1'
 ]
 
 setup(
