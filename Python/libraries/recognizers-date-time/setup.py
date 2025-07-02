84e394e4ae9559bf8e25323ef67673c53210db68
Daniel Vaughan <daniel.vaughan@genesys.com>
2025-07-02 10:54:09 +0100

    undo revert

--- Python/libraries/recognizers-date-time/setup.py
+++ Python/libraries/recognizers-date-time/setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 NAME = 'recognizers-text-date-time-genesys'
-VERSION = '1.1.43a2'
+VERSION = '1.1.43a3'
 REQUIRES = [
     'recognizers-text-genesys',
     'recognizers-text-number-genesys',
--- Python/libraries/recognizers-number-with-unit/setup.py
+++ Python/libraries/recognizers-number-with-unit/setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 NAME = "recognizers-text-number-with-unit-genesys"
-VERSION = "1.1.43a2"
+VERSION = "1.1.43a3"
 REQUIRES = ['recognizers-text-genesys', 'recognizers-text-number-genesys', 'regex']
 
 setup(
