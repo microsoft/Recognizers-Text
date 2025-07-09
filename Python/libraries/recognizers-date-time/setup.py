b42facf1fce2734122a62a94278c53effb696418
Daniel Vaughan <daniel.vaughan@genesys.com>
2025-07-09 10:42:24 +0100

    date -> datetime

--- Python/libraries/recognizers-date-time/setup.py
+++ Python/libraries/recognizers-date-time/setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 NAME = 'recognizers-text-date-time-genesys'
-VERSION = '1.1.43b1'
+VERSION = '1.1.43b2'
 REQUIRES = [
     'recognizers-text-genesys',
     'recognizers-text-number-genesys',
--- Python/libraries/recognizers-text/setup.py
+++ Python/libraries/recognizers-text/setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 
 NAME = "recognizers-text-genesys"
-VERSION = "1.1.43b1"
+VERSION = "1.1.43b2"
 REQUIRES = ['emoji==1.1.0', 'multipledispatch']
 
 setup(
